a05d2d57825eeee77a81a1db6c5d87f72008fd52
Jens Bendisposto <jens@bendisposto.de>
2011-12-29 17:18:13 +0100

    added missing import

--- de.prob.core.kernel/src/main/java/de/prob/Shell.java
+++ de.prob.core.kernel/src/main/java/de/prob/Shell.java
@@ -3,6 +3,7 @@ package de.prob;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.PrintStream;
 import java.util.List;
 
 import javax.script.ScriptEngine;
