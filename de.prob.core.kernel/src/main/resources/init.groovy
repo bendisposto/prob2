0045eb677b1619a77c9d21afd6d305412b9a5630
Jens Bendisposto <jens@bendisposto.de>
2012-02-23 10:29:53 +0100

    changed init script

--- de.prob.core.kernel/src/main/resources/init.groovy
+++ de.prob.core.kernel/src/main/resources/init.groovy
@@ -1,11 +1,4 @@
-println "Hello from Groovy!!!" 
-
-classLoader = ClassLoader.systemClassLoader
-
-while (classLoader.parent) {  classLoader = classLoader.parent }
-
-console_reader.addCompletor(new org.codehaus.groovy.tools.shell.util.ClassNameCompletor(new GroovyClassLoader()));
-
+println "Hello from Groovy!!!"
 s = api.b_def()
 
 explore = s.&explore
