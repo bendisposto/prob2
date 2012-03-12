ee770f67942558fc4bcfcbd491e77b38e61696f4
Philipp Koerner <p.koerner@uni-duesseldorf.de>
2012-03-12 18:06:46 +0100

    improved tests

--- de.prob.core.kernel/src/test/java/de/prob/animator/command/SpecializedBooleanCheckersCommandTest.java
+++ de.prob.core.kernel/src/test/java/de/prob/animator/command/SpecializedBooleanCheckersCommandTest.java
@@ -2,8 +2,6 @@ package de.prob.animator.command;
 
 import static org.junit.Assert.*;
 
-import java.util.Random;
-
 import org.junit.Test;
 
 import test.TestHelper;
@@ -12,11 +10,22 @@ import de.prob.parser.ISimplifiedROMap;
 import de.prob.prolog.term.PrologTerm;
 
 public class SpecializedBooleanCheckersCommandTest {
-	Random r = new Random();
+
 
 	@Test
-	public void testCheckInitialisationStatusCommand() throws ProBException {
-		boolean b = r.nextBoolean();
+	public void testCheckInitialisationStatusCommandTrue() throws ProBException {
+		boolean b = true;
+		ISimplifiedROMap<String, PrologTerm> map = TestHelper.mkAtomMock(
+				"PropResult", "" + b);
+		CheckInitialisationStatusCommand command = new CheckInitialisationStatusCommand(
+				"root");
+		command.processResult(map);
+		assertEquals(b, command.isInitialized());
+	}
+	
+	@Test
+	public void testCheckInitialisationStatusCommandFalse() throws ProBException {
+		boolean b = false;
 		ISimplifiedROMap<String, PrologTerm> map = TestHelper.mkAtomMock(
 				"PropResult", "" + b);
 		CheckInitialisationStatusCommand command = new CheckInitialisationStatusCommand(
@@ -26,8 +35,19 @@ public class SpecializedBooleanCheckersCommandTest {
 	}
 
 	@Test
-	public void testCheckInvariantStatusCommand() throws ProBException {
-		boolean b = r.nextBoolean();
+	public void testCheckInvariantStatusCommandTrue() throws ProBException {
+		boolean b = true;
+		ISimplifiedROMap<String, PrologTerm> map = TestHelper.mkAtomMock(
+				"PropResult", "" + b);
+		CheckInvariantStatusCommand command = new CheckInvariantStatusCommand(
+				"root");
+		command.processResult(map);
+		assertEquals(b, command.isInvariantViolated());
+	}
+	
+	@Test
+	public void testCheckInvariantStatusCommandFalse() throws ProBException {
+		boolean b = false;
 		ISimplifiedROMap<String, PrologTerm> map = TestHelper.mkAtomMock(
 				"PropResult", "" + b);
 		CheckInvariantStatusCommand command = new CheckInvariantStatusCommand(
@@ -37,9 +57,21 @@ public class SpecializedBooleanCheckersCommandTest {
 	}
 
 	@Test
-	public void testCheckMaxOperationReachedStatusCommand()
+	public void testCheckMaxOperationReachedStatusCommandTrue()
 			throws ProBException {
-		boolean b = r.nextBoolean();
+		boolean b = true;
+		ISimplifiedROMap<String, PrologTerm> map = TestHelper.mkAtomMock(
+				"PropResult", "" + b);
+		CheckMaxOperationReachedStatusCommand command = new CheckMaxOperationReachedStatusCommand(
+				"root");
+		command.processResult(map);
+		assertEquals(b, command.maxOperationReached());
+	}
+	
+	@Test
+	public void testCheckMaxOperationReachedStatusCommandFalse()
+			throws ProBException {
+		boolean b = false;
 		ISimplifiedROMap<String, PrologTerm> map = TestHelper.mkAtomMock(
 				"PropResult", "" + b);
 		CheckMaxOperationReachedStatusCommand command = new CheckMaxOperationReachedStatusCommand(
@@ -49,8 +81,8 @@ public class SpecializedBooleanCheckersCommandTest {
 	}
 
 	@Test
-	public void testCheckTimeoutStatusCommand() throws ProBException {
-		boolean b = r.nextBoolean();
+	public void testCheckTimeoutStatusCommandTrue() throws ProBException {
+		boolean b = true;
 		ISimplifiedROMap<String, PrologTerm> map = TestHelper.mkAtomMock(
 				"PropResult", "" + b);
 		CheckTimeoutStatusCommand command = new CheckTimeoutStatusCommand(
@@ -59,4 +91,14 @@ public class SpecializedBooleanCheckersCommandTest {
 		assertEquals(b, command.isTimeout());
 	}
 
+	@Test
+	public void testCheckTimeoutStatusCommandFalse() throws ProBException {
+		boolean b = false;
+		ISimplifiedROMap<String, PrologTerm> map = TestHelper.mkAtomMock(
+				"PropResult", "" + b);
+		CheckTimeoutStatusCommand command = new CheckTimeoutStatusCommand(
+				"root");
+		command.processResult(map);
+		assertEquals(b, command.isTimeout());
+	}
 }
