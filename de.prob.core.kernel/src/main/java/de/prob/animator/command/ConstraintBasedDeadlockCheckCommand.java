717249154209d5d1e8676f4d4217782466d8a228
Philipp Koerner <p.koerner@uni-duesseldorf.de>
2012-03-22 17:39:30 +0100

    closes #14

--- de.prob.core.kernel/src/main/java/de/prob/animator/command/ConstraintBasedDeadlockCheckCommand.java
+++ de.prob.core.kernel/src/main/java/de/prob/animator/command/ConstraintBasedDeadlockCheckCommand.java
@@ -99,8 +99,7 @@ public class ConstraintBasedDeadlockCheckCommand implements ICommand {
 			} catch (ResultParserException e) {
 				logger.error("Result from Prolog was not as expected.", e);
 				throw new ProBException();
-			}
-			
+			}	
 		} else {
 			logger.error("unexpected result from deadlock check: " + resultTerm);
 			throw new ProBException();
