0fa442915145664a200c62bc982da4b2e9910fe5
Joy Clark <joyclark2004@msn.com>
2012-04-11 10:31:22 +0200

    Added evaluate command #64 prolog side needs to be adapted for vars that don't exist

--- de.prob.core.kernel/src/test/java/de/prob/animator/command/EvaluateRawExpressionsCommandTest.java
+++ de.prob.core.kernel/src/test/java/de/prob/animator/command/EvaluateRawExpressionsCommandTest.java
@@ -1,7 +1,10 @@
 package de.prob.animator.command;
 
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 import java.util.Collection;
