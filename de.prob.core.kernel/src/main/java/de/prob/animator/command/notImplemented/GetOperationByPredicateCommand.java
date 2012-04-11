06f93d97386d88efbbe841d246f87f1a3b959fac
Joy Clark <joyclark2004@msn.com>
2012-04-11 11:02:46 +0200

    worked on #45 implemented GetOperationFromPredicate command

--- de.prob.core.kernel/src/main/java/de/prob/animator/command/notImplemented/GetOperationByPredicateCommand.java
+++ de.prob.core.kernel/src/main/java/de/prob/animator/command/notImplemented/GetOperationByPredicateCommand.java
@@ -6,27 +6,24 @@
 
 package de.prob.animator.command.notImplemented;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import de.be4.classicalb.core.parser.analysis.prolog.ASTProlog;
+import de.be4.classicalb.core.parser.exceptions.BException;
 import de.prob.ProBException;
 import de.prob.animator.command.ICommand;
+import de.prob.animator.domainobjects.ClassicalBEvalElement;
+import de.prob.animator.domainobjects.OpInfo;
+import de.prob.parser.BindingGenerator;
 import de.prob.parser.ISimplifiedROMap;
+import de.prob.parser.ResultParserException;
 import de.prob.prolog.output.IPrologTermOutput;
+import de.prob.prolog.term.ListPrologTerm;
 import de.prob.prolog.term.PrologTerm;
-
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//import de.be4.classicalb.core.parser.analysis.prolog.ASTProlog;
-//import de.be4.classicalb.core.parser.exceptions.BException;
-//import de.prob.core.Animator;
-//import de.prob.core.ProblemHandler;
-//import de.prob.core.domainobjects.Operation;
-//import de.prob.core.domainobjects.eval.PredicateEvalElement;
-//import de.prob.exceptions.ProBException;
-//import de.prob.parser.ISimplifiedROMap;
-//import de.prob.prolog.output.IPrologTermOutput;
-//import de.prob.prolog.term.CompoundPrologTerm;
-//import de.prob.prolog.term.ListPrologTerm;
-//import de.prob.prolog.term.PrologTerm;
 
 /**
  * Command to execute an event that has not been enumerated by ProB, for further
@@ -37,148 +34,93 @@ import de.prob.prolog.term.PrologTerm;
  */
 public final class GetOperationByPredicateCommand implements ICommand {
 
-	@Override
-	public void writeCommand(IPrologTermOutput pto) throws ProBException {
-		// TODO Auto-generated method stub
+	Logger logger = LoggerFactory
+			.getLogger(GetOperationByPredicateCommand.class);
+	private static final String NEW_STATE_ID_VARIABLE = "NewStateID";
+	private final ClassicalBEvalElement evalElement;
+	private final String stateId;
+	private final String name;
+	private List<OpInfo> operation;
+	private final int nrOfSolutions;
 
+	private GetOperationByPredicateCommand() {
+		throw new UnsupportedOperationException("Do not call this constructor");
 	}
 
+	private GetOperationByPredicateCommand(final String stateId,
+			final String name, final String predicate, final int nrOfSolutions) {
+		this.stateId = stateId;
+		this.name = name;
+		this.nrOfSolutions = nrOfSolutions;
+		this.evalElement = new ClassicalBEvalElement(predicate);
+	}
+
+	/**
+	 * This method is called when the command is prepared for sending. The
+	 * method is called by the Animator class, most likely it is not interesting
+	 * for other classes.
+	 * 
+	 * @throws ProBException
+	 * 
+	 * @see de.prob.core.command.IComposableCommand#writeCommand(de.prob.prolog.output.IPrologTermOutput)
+	 */
 	@Override
-	public void processResult(ISimplifiedROMap<String, PrologTerm> bindings)
+	public void writeCommand(final IPrologTermOutput pto) throws ProBException {
+		pto.openTerm("execute_custom_operations").printAtomOrNumber(stateId)
+				.printAtom(name);
+		try {
+			final ASTProlog prolog = new ASTProlog(pto, null);
+			evalElement.parse().apply(prolog);
+		} catch (BException e) {
+			logger.error("Parse error", e);
+			throw new ProBException();
+		} finally {
+			pto.printNumber(nrOfSolutions);
+			pto.printVariable(NEW_STATE_ID_VARIABLE);
+			pto.printVariable("Errors").closeTerm();
+		}
+	}
+
+	/**
+	 * This method is called to extract relevant information from ProB's answer.
+	 * The method is called by the Animator class, most likely it is not
+	 * interesting for other classes.
+	 * 
+	 * @throws ProBException
+	 * 
+	 * @see de.prob.core.command.IComposableCommand#writeCommand(de.prob.prolog.output.IPrologTermOutput)
+	 */
+	@Override
+	public void processResult(
+			final ISimplifiedROMap<String, PrologTerm> bindings)
 			throws ProBException {
-		// TODO Auto-generated method stub
 
+		try {
+			ListPrologTerm list = BindingGenerator.getList(bindings
+					.get(NEW_STATE_ID_VARIABLE));
+
+			if (list.isEmpty()) {
+				operation = null;
+			} else {
+				ArrayList<OpInfo> result = new ArrayList<OpInfo>();
+				for (PrologTerm prologTerm : list) {
+					String id = prologTerm.getArgument(1).getFunctor();
+					String name = prologTerm.getArgument(2).getFunctor();
+					String src = prologTerm.getArgument(3).getFunctor();
+					String dest = prologTerm.getArgument(4).getFunctor();
+					String args = prologTerm.getArgument(6).toString();
+					result.add(new OpInfo(id, name, src, dest, args));
+				}
+				operation = result;
+			}
+		} catch (ResultParserException e) {
+			logger.error("Result from Prolog was not as expected.", e);
+			throw new ProBException();
+		}
 	}
-	//
-	// FIXME: REFACTOR THIS CLASS
-	// private static final String NEW_STATE_ID_VARIABLE = "NewStateID";
-	// private final PredicateEvalElement evalElement;
-	// private final String stateId;
-	// private final String name;
-	// private List<OpInfo> operation;
-	// private final int nrOfSolutions;
-	//
-	// private GetOperationByPredicateCommand() {
-	// throw new UnsupportedOperationException("Do not call this constructor");
-	// }
-	//
-	// private GetOperationByPredicateCommand(final String stateId,
-	// final String name, final String predicate, final int nrOfSolutions) {
-	// this.stateId = stateId;
-	// this.name = name;
-	// this.nrOfSolutions = nrOfSolutions;
-	// PredicateEvalElement parsedEvalElement = null;
-	// try {
-	// parsedEvalElement = PredicateEvalElement.create(predicate);
-	// } catch (BException e) {
-	// String message = "Fatal error when trying to parse " + predicate
-	// + ". Execution of operation " + name + " aborted.";
-	// ProblemHandler.raiseCommandException(message);
-	// parsedEvalElement = null;
-	// } finally {
-	// evalElement = parsedEvalElement;
-	// }
-	// }
-	//
-	// /**
-	// * Works like @see getOperations but returns a single solution
-	// */
-	// public static Operation getOperation(final Animator a,
-	// final String stateId, final String name, final String predicate)
-	// throws ProBException, BException {
-	//
-	// List<Operation> operations = getOperations(a, stateId, name, predicate,
-	// 1);
-	//
-	// return operations == null ? null : operations.get(0);
-	// }
-	//
-	// /**
-	// * Tries to find a valid transition from the state <em>stateId</em>
-	// * satisfying a B <em>predicate</em> . Returns either null if no event
-	// * satisfying <em></em> was found by ProB or an Operation object that can
-	// be
-	// * executed using ExecuteOperationCommand.
-	// *
-	// * @param animator
-	// * - Animator Instance
-	// * @param stateId
-	// * - The state in which the event should be fired
-	// * @param name
-	// * - The event's name
-	// * @param predicate
-	// * - Additional guarding predicate
-	// * @param nrOfSolutions
-	// * - maximum number of solutions
-	// * @return an Operation or null
-	// * @throws BException
-	// * - if the B predicate contains errors
-	// * @throws ProBException
-	// * - if something terrible happens ;-)
-	// */
-	// public static List<Operation> getOperations(final Animator a,
-	// final String stateId, final String name, final String predicate,
-	// final int nrOfSolutions) throws ProBException, BException {
-	//
-	// GetOperationByPredicateCommand executeOperationCommand = new
-	// GetOperationByPredicateCommand(
-	// stateId, name, predicate, nrOfSolutions);
-	// if (executeOperationCommand.evalElement != null) {
-	// a.execute(executeOperationCommand);
-	// }
-	// return executeOperationCommand.getOperation();
-	// }
-	//
-	// /**
-	// * This method is called when the command is prepared for sending. The
-	// * method is called by the Animator class, most likely it is not
-	// interesting
-	// * for other classes.
-	// *
-	// * @see
-	// de.prob.core.command.IComposableCommand#writeCommand(de.prob.prolog.output.IPrologTermOutput)
-	// */
-	// public void writeCommand(final IPrologTermOutput pto) {
-	// pto.openTerm("execute_custom_operations").printAtomOrNumber(stateId)
-	// .printAtom(name);
-	// final ASTProlog prolog = new ASTProlog(pto, null);
-	// evalElement.getPrologAst().apply(prolog);
-	// pto.printNumber(nrOfSolutions);
-	// pto.printVariable(NEW_STATE_ID_VARIABLE);
-	// pto.printVariable("Errors").closeTerm();
-	// }
-	//
-	// /**
-	// * This method is called to extract relevant information from ProB's
-	// answer.
-	// * The method is called by the Animator class, most likely it is not
-	// * interesting for other classes.
-	// *
-	// * @see
-	// de.prob.core.command.IComposableCommand#writeCommand(de.prob.prolog.output.IPrologTermOutput)
-	// */
-	// public void processResult(
-	// final ISimplifiedROMap<String, PrologTerm> bindings)
-	// throws CommandException {
-	//
-	// ListPrologTerm list = (ListPrologTerm) bindings
-	// .get(NEW_STATE_ID_VARIABLE);
-	//
-	// if (list.isEmpty()) {
-	// operation = null;
-	// } else {
-	// ArrayList<Operation> result = new ArrayList<Operation>();
-	// for (PrologTerm prologTerm : list) {
-	// Operation op = Operation
-	// .fromPrologTerm((CompoundPrologTerm) prologTerm);
-	// result.add(op);
-	// }
-	// operation = result;
-	// }
-	// }
-	//
-	// private List<Operation> getOperation() {
-	// return operation;
-	// }
+
+	public List<OpInfo> getOperation() {
+		return operation;
+	}
 
 }
