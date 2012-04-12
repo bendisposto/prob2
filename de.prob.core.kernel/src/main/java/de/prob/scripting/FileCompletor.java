23187d942dcf5f03dee79d58c129981924ddc35e
Jens Bendisposto <jens@bendisposto.de>
2012-04-12 08:21:15 +0200

    fixed findbugs warnings

--- de.prob.core.kernel/src/main/java/de/prob/scripting/FileCompletor.java
+++ de.prob.core.kernel/src/main/java/de/prob/scripting/FileCompletor.java
@@ -1,6 +1,5 @@
 package de.prob.scripting;
 
-import java.io.File;
 import java.util.List;
 
 import jline.Completor;
@@ -9,11 +8,11 @@ public class FileCompletor implements Completor {
 
 	@Override
 	public int complete(final String buf, final int cursor,
-			final List candidates) {
-		String buffer = (buf == null) ? "" : buf;
-		File file = new File(buffer);
+			@SuppressWarnings("rawtypes") final List candidates) {
+		// String buffer = (buf == null) ? "" : buf;
+		// File file = new File(buffer);
 		// if (file.exists())
-		return 0;
+		return -1;
 	}
 
 }
