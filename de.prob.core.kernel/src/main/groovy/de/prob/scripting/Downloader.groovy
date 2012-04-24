daab73afbdf04324f2657b61d756acf55b462897
Philipp Koerner <p.koerner@uni-duesseldorf.de>
2012-04-24 10:08:49 +0200

    working on #102, getting versioninfos from server, using latest version of cli at the moment

--- de.prob.core.kernel/src/main/groovy/de/prob/scripting/Downloader.groovy
+++ de.prob.core.kernel/src/main/groovy/de/prob/scripting/Downloader.groovy
@@ -36,9 +36,22 @@ class Downloader {
 		}
 
 
+		// download config
+		def configurl = "http://nightly.cobra.cs.uni-duesseldorf.de/tmp/config.groovy"
+		def file = probhome + "config.groovy"
+		download(configurl, file)
+		File g = new File(file)
+
+		// get location of desired cli version
+		def config = new ConfigSlurper().parse(g.toURL())
+		def versionurl = config."latest".url
+		//def versionurl = config."1.3.4-test".url
+		g.delete();
+
+
 		// Use operating system to download the correct zip file
 		def targetzip = probhome+"probcli_${os}.zip"
-		def url = "http://nightly.cobra.cs.uni-duesseldorf.de/cli/probcli_${os}.zip"
+		def url = versionurl + "probcli_${os}.zip"
 		download(url,targetzip)
 
 		// Unzip file to correct directory (probhome)
