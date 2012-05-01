b5483187211289fd9fd1b21763f433cc20deda36
Jens Bendisposto <jens@bendisposto.de>
2012-05-01 17:56:53 +0200

    working on PROBCORE-13

--- de.prob.core.kernel/src/main/java/de/prob/model/classicalb/ClassicalBDependencyType.java
+++
@@ -1,5 +0,0 @@
-package de.prob.model.classicalb;
-
-public enum ClassicalBDependencyType {
-	REFINES, SEES, USES, INCLUDES;
-}
--- de.prob.core.kernel/src/main/java/de/prob/model/classicalb/ClassicalBModel.java
+++ de.prob.core.kernel/src/main/java/de/prob/model/classicalb/ClassicalBModel.java
@@ -1,42 +1,44 @@
 package de.prob.model.classicalb;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 import com.google.inject.Inject;
 
-import de.be4.classicalb.core.parser.analysis.prolog.NodeIdAssignment;
-import de.be4.classicalb.core.parser.analysis.prolog.RecursiveMachineLoader;
 import de.be4.classicalb.core.parser.node.Start;
 import de.prob.ProBException;
 import de.prob.statespace.StateSpace;
-import edu.uci.ics.jung.graph.DirectedGraph;
 import edu.uci.ics.jung.graph.DirectedSparseMultigraph;
-import edu.uci.ics.jung.graph.MultiGraph;
 
-public class ClassicalBModel extends ClassicalBModelGraph implements
-		DirectedGraph<ClassicalBMachine, ClassicalBDependencyType>,
-		MultiGraph<ClassicalBMachine, ClassicalBDependencyType> {
+public class ClassicalBModel {
 
 	private final StateSpace statespace;
 	private ClassicalBMachine mainMachine = null;
+	Map<String, ClassicalBMachine> content = new HashMap<String, ClassicalBMachine>();
 
 	@Inject
-	public ClassicalBModel(
-			final DirectedSparseMultigraph<ClassicalBMachine, ClassicalBDependencyType> graph,
-			StateSpace statespace) {
-		super(graph);
+	public ClassicalBModel(StateSpace statespace) {
 		this.statespace = statespace;
 	}
 
-	public void initialize(Start ast,File f) throws ProBException {
+	public DirectedSparseMultigraph<String, RefType> initialize(
+			Start ast, File f) throws ProBException {
 
-//		NodeIdAssignment nodeIdMapping = ast.getNodeIdMapping();
-//		ClassicalBMachine classicalBMachine = new ClassicalBMachine(
-//				nodeIdMapping);
-//
-//		addVertex(classicalBMachine);
-//		mainMachine = classicalBMachine;
+		DirectedSparseMultigraph<String, RefType> graph = new DirectedSparseMultigraph<String, RefType>();
 
+		mainMachine = new ClassicalBMachine(null);
+		DomBuilder d = new DomBuilder(mainMachine);
+		d.build(ast);
+
+		String name = mainMachine.getName();
+		graph.addVertex(name);
+		content.put(name, mainMachine);
+
+		ast.apply(new DependencyWalker(name,f.getParentFile(), graph));
+		return graph;
 	}
 
 	public StateSpace getStatespace() {
