564e0900a276283a4208e812bdaacaf532d3b843
Joy Clark <joyclark2004@msn.com>
2012-05-31 18:08:51 +0200

    PROBCORE-51 work in progress

--- de.prob.core.kernel/src/main/java/de/prob/visualization/MachineRenderer.java
+++ de.prob.core.kernel/src/main/java/de/prob/visualization/MachineRenderer.java
@@ -1,52 +1,153 @@
 package de.prob.visualization;
 
-import java.awt.Color;
 import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
 
-import javax.swing.JButton;
-import javax.swing.JTable;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 import org.jgraph.JGraph;
 import org.jgraph.graph.CellView;
 import org.jgraph.graph.DefaultGraphCell;
-import org.jgraph.graph.GraphConstants;
+import org.jgraph.graph.GraphLayoutCache;
 import org.jgraph.graph.VertexRenderer;
 import org.jgraph.graph.VertexView;
 
 import de.prob.model.classicalb.ClassicalBEntity;
 import de.prob.model.classicalb.ClassicalBMachine;
-import de.prob.model.classicalb.ClassicalBModel;
 import de.prob.model.representation.Operation;
 
 @SuppressWarnings("serial")
 public class MachineRenderer extends VertexRenderer {
 
 	@Override
-	public Component getRendererComponent(JGraph graph, CellView view,
-			boolean sel, boolean focus, boolean preview) {
+	public Component getRendererComponent(final JGraph graph,
+			final CellView view, final boolean sel, final boolean focus,
+			final boolean preview) {
 		this.view = (VertexView) view;
 		DefaultGraphCell cell = (DefaultGraphCell) this.view.getCell();
 		ClassicalBMachine m = (ClassicalBMachine) cell.getUserObject();
 
-		String[][] data = new String[1 + m.constants().size()
-				+ m.variables().size() + m.operations().size()][1];
+		// String[][] data = new String[1 + m.constants().size()
+		// + m.variables().size() + m.operations().size()][1];
+		//
+		// data[0][0] = m.name();
+		// int row = 1;
+		// for (ClassicalBEntity e : m.constants()) {
+		// data[row++][0] = e.getIdentifier();
+		// }
+		// for (ClassicalBEntity e : m.variables()) {
+		// data[row++][0] = e.getIdentifier();
+		// }
+		// for (Operation e : m.operations()) {
+		// data[row++][0] = e.toString();
+		// }
+		//
+		// JTable t = new JTable(data, new String[] { "M" });
+		// t.setOpaque(true);
+		// t.setBackground(Color.ORANGE);
+		// t.setForeground(Color.BLACK);
+		// t.setGridColor(Color.WHITE);
 
-		data[0][0] = m.name();
-		int row = 1;
-		for (ClassicalBEntity e : m.constants()) {
-			data[row++][0] = e.getIdentifier();
-		}
-		for (ClassicalBEntity e : m.variables()) {
-			data[row++][0] = e.getIdentifier();
-		}
-		for (Operation e : m.operations()) {
-			data[row++][0] = e.toString();
-		}
+		JPanel box = createVertexRep(m);
 
-		JTable t = new JTable(data, new String[] { "M" });
-		t.setBackground(Color.WHITE);
-		t.setForeground(Color.black);
-		return t;
-
+		Rectangle2D oldBounds = this.view.getBounds();
+		oldBounds.setRect(oldBounds.getX(), oldBounds.getY(), box.getWidth(),
+				box.getHeight());
+		this.view.setBounds(oldBounds);
+		System.out.println(this.view.getBounds());
+		return box;
 	}
+
+	public JPanel createVertexRep(final ClassicalBMachine m) {
+		JPanel box = new JPanel(new GridLayout(calcNumOfRows(m), 1));
+		JLabel name = new JLabel(m.name());
+		box.add(name);
+		if (!m.variables().isEmpty()) {
+			JPanel variables = new JPanel(new FlowLayout());
+			variables.add(new JLabel("Variables"));
+			for (ClassicalBEntity var : m.variables()) {
+				variables.add(new JLabel(" " + var.getIdentifier()));
+			}
+			box.add(variables);
+		}
+		if (!m.constants().isEmpty()) {
+			JPanel constants = new JPanel(new GridLayout(1 + m.constants()
+					.size(), 1));
+			constants.add(new JLabel("Constants"));
+			for (ClassicalBEntity constant : m.constants()) {
+				constants.add(new JLabel(" " + constant.getIdentifier()));
+			}
+			box.add(constants);
+		}
+		if (!m.operations().isEmpty()) {
+			JPanel operations = new JPanel(new GridLayout(1 + m.operations()
+					.size(), 1));
+			operations.add(new JLabel("Operations"));
+			for (Operation op : m.operations()) {
+				operations.add(new JLabel(" " + op.toString()));
+			}
+		}
+		box.setSize(new Dimension(100, (1 + m.variables().size()
+				+ m.constants().size() + m.operations().size()) * 25));
+		return box;
+	}
+
+	public int calcNumOfRows(final ClassicalBMachine m) {
+		int nr = 1;
+		if (!m.variables().isEmpty()) {
+			nr++;
+		}
+		if (!m.constants().isEmpty()) {
+			nr++;
+		}
+		if (!m.operations().isEmpty()) {
+			nr++;
+		}
+		return nr;
+	}
+
+	@Override
+	public Point2D getPerimeterPoint(final VertexView view,
+			final Point2D source, final Point2D p) {
+		// TODO Auto-generated method stub
+		Rectangle2D bounds = view.getBounds();
+		double x = bounds.getX();
+		double y = bounds.getY();
+		double width = bounds.getWidth();
+		double height = bounds.getHeight();
+		double xCenter = x + width / 2;
+		double yCenter = y + height / 2;
+		double dx = p.getX() - xCenter; // Compute Angle
+		double dy = p.getY() - yCenter;
+		double alpha = Math.atan2(dy, dx);
+		double xout = 0, yout = 0;
+		double pi = Math.PI;
+		double pi2 = Math.PI / 2.0;
+		double beta = pi2 - alpha;
+		double t = Math.atan2(height, width);
+		if (alpha < -pi + t || alpha > pi - t) { // Left edge
+			xout = x;
+			yout = yCenter - width * Math.tan(alpha) / 2;
+		} else if (alpha < -t) { // Top Edge
+			yout = y;
+			xout = xCenter - height * Math.tan(beta) / 2;
+		} else if (alpha < t) { // Right Edge
+			xout = x + width;
+			yout = yCenter + width * Math.tan(alpha) / 2;
+		} else { // Bottom Edge
+			yout = y + height;
+			xout = xCenter + height * Math.tan(beta) / 2;
+		}
+		return new Point2D.Double(xout, yout);
+	}
+
+	public void update(final GraphLayoutCache cache) {
+		this.view.update(cache);
+	}
+
 }
--- de.prob.core.kernel/src/main/java/de/prob/visualization/Visualization.java
+++ de.prob.core.kernel/src/main/java/de/prob/visualization/Visualization.java
@@ -5,6 +5,8 @@ import java.util.Map;
 import javax.swing.JFrame;
 
 import org.jgraph.JGraph;
+import org.jgraph.graph.CellView;
+import org.jgraph.graph.DefaultGraphCell;
 import org.jgraph.graph.GraphLayoutCache;
 import org.jgraph.graph.VertexView;
 import org.jgrapht.ext.JGraphModelAdapter;
@@ -33,9 +35,8 @@ public class Visualization {
 		JFrame jf = new JFrame();
 
 		JGraph jgraph = new JGraph(modelAdapter);
-
+		jgraph.setGridSize(20);
 		VertexView.renderer = new MachineRenderer();
-
 		jf.setSize(600, 600);
 
 		ClassicalBMachine mainMachine = model.getMainMachine();
@@ -43,12 +44,38 @@ public class Visualization {
 		JGraphFacade f = new JGraphFacade(jgraph,
 				new ClassicalBMachine[] { model.getMainMachine() });
 		layout.run(f);
+		System.out.println(f.getAttributes());
 
 		Map nested = f.createNestedMap(true, true);
+		System.out.println(nested);
 		GraphLayoutCache glc = jgraph.getGraphLayoutCache();
+		change(nested, glc);
 		glc.edit(nested);
 		jf.getContentPane().add(jgraph);
 		jf.pack();
 		jf.setVisible(true);
+	}
+
+	public void change(final Map nested, final GraphLayoutCache glc) {
+		CellView[] allViews = glc.getCellViews();
+		for (CellView cellView : allViews) {
+			if (cellView instanceof VertexView) {
+				DefaultGraphCell cell = (DefaultGraphCell) cellView.getCell();
+				if (cell.getUserObject() instanceof ClassicalBMachine) {
+					ClassicalBMachine m = (ClassicalBMachine) cell
+							.getUserObject();
+					int width = 100;
+					int height = 1 + m.variables().size()
+							+ m.constants().size() + m.operations().size();
+					Object object = nested.get(cell);
+					System.out.println(((java.util.Hashtable) object)
+							.contains("bounds"));
+					System.out.println(object.getClass());
+					System.out.println(m.name());
+					System.out.println(nested.containsKey(cell));
+					System.out.println(nested.keySet());
+				}
+			}
+		}
 	}
 }
