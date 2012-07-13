08a1d0d562073262067cbe640eb09da0f3f3edf8
Philipp Koerner <p.koerner@uni-duesseldorf.de>
2012-07-13 16:36:10 +0200

    working on refactoring of history - still to do: worry about guice and the evaulation part, tests

--- de.prob.core.kernel/src/main/java/de/prob/statespace/History.java
+++ de.prob.core.kernel/src/main/java/de/prob/statespace/History.java
@@ -1,12 +1,39 @@
 package de.prob.statespace;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Random;
+import java.util.Set;
+
+import de.be4.classicalb.core.parser.exceptions.BException;
 
 public class History {
 
-	private final List<HistoryElement> history = new ArrayList<HistoryElement>();
-	private int current = -1;
+	private final Random randomGenerator;
+	private final List<HistoryElement> history;
+	private int current;
+
+	private final StateSpace s;
+
+	// TODO: worry about injection
+	public History(final StateSpace s, final Random random) {
+		this.s = s;
+		this.current = -1;
+		this.history = new ArrayList<HistoryElement>();
+		this.randomGenerator = random;
+	}
+
+	public History(final StateSpace s, final List<HistoryElement> history,
+			final Random random) {
+		this.s = s;
+		this.history = history;
+		this.randomGenerator = random;
+		current = history.size() - 1;
+	}
 
 	/**
 	 * Adds a new transition (History Element) to the History object. Takes the
@@ -45,28 +72,54 @@ public class History {
 		}
 	}
 
-	// goToPos
 	public void goToPos(final int pos) {
 		if (pos >= -1 && pos < history.size()) {
 			current = pos;
 		}
 	}
 
-	// back
+	/**
+	 * Moves one step back in the animation if this is possible.
+	 */
 	public void back() {
 		if (canGoBack()) {
 			current--;
+
+			StateId oldState = getCurrentStateId();
+			String opId = getCurrentTransition();
+
+			s.evaluateFormulas(getCurrentStateId());
+
+			if (opId != null) {
+				OperationId op = new OperationId(opId);
+				s.notifyAnimationChange(s.getEdgeSource(op),
+						s.getEdgeTarget(op), op);
+			} else {
+				s.notifyAnimationChange(oldState, getCurrentStateId(), null);
+			}
 		}
 	}
 
-	// forward
+	/**
+	 * Moves one step forward in the animation if this is possible
+	 */
 	public void forward() {
 		if (canGoForward()) {
 			current++;
+			StateId oldState = getCurrentStateId();
+			s.evaluateFormulas(getCurrentStateId());
+
+			String opId = getCurrentTransition();
+			if (opId != null) {
+				OperationId op = new OperationId(opId);
+				s.notifyAnimationChange(s.getEdgeSource(op),
+						s.getEdgeTarget(op), op);
+			} else {
+				s.notifyAnimationChange(oldState, getCurrentStateId(), null);
+			}
 		}
 	}
 
-	// getCurrentTransition
 	public String getCurrentTransition() {
 		if (current == -1)
 			return null;
@@ -77,7 +130,6 @@ public class History {
 		if (current <= 0)
 			return false;
 
-		// String currentOp = history.get(current).getOp();
 		String currentOp = getCurrentTransition();
 		if (id == null)
 			return currentOp == null;
@@ -127,4 +179,147 @@ public class History {
 			return "root";
 		return history.get(current).getDest();
 	}
+
+	public StateId getCurrentStateId() {
+		final Map<String, StateId> states = s.getStates();
+		return states.get(getCurrentState());
+	}
+
+	/**
+	 * Carries out one step in the animation with the id from an Operation. If
+	 * the opId is contained in the outgoing edges (it is enabled) explore it
+	 * (if not explored) and add state to history
+	 * 
+	 * @param opId
+	 */
+	public void step(final String opId) {
+
+		OperationId op = new OperationId(opId);
+		if (!s.outgoingEdgesOf(getCurrentStateId()).contains(op))
+			throw new IllegalArgumentException(opId
+					+ " is not a valid operation on this state");
+
+		StateId newState = s.getEdgeTarget(op);
+		add(newState.getId(), opId);
+		s.evaluateFormulas(getCurrentStateId());
+		s.notifyAnimationChange(s.getEdgeSource(op), s.getEdgeTarget(op), op);
+	}
+
+	public void step(final int i) {
+		String opId = String.valueOf(i);
+		step(opId);
+	}
+
+	/**
+	 * Finds one Operation that satisfies the operation name and predicate at
+	 * the current state
+	 * 
+	 * @param opName
+	 * @param predicate
+	 * @return one operations that meets the specifications @ * @throws
+	 *         BException
+	 */
+	public Operation findOneOp(final String opName, final String predicate)
+			throws BException {
+		return s.opFromPredicate(getCurrentStateId(), opName, predicate, 1)
+				.get(0);
+	}
+
+	/**
+	 * Finds an Operation with opName and predicate and carries out one
+	 * animation step with that Operation
+	 * 
+	 * @param opName
+	 * @param predicate
+	 * @throws BException
+	 */
+	public void stepWithOp(final String opName, final String predicate)
+			throws BException {
+		Operation op = findOneOp(opName, predicate);
+		step(op.getId());
+	}
+
+	/**
+	 * Moves from the current state to the state at stateId.
+	 * 
+	 * @param stateId
+	 */
+	public void goToState(final StateId stateId) {
+		if (!s.containsVertex(stateId))
+			throw new IllegalArgumentException("state does not exist");
+		if (!s.isExplored(stateId)) {
+			s.explore(stateId);
+		}
+
+		add(stateId.getId(), null);
+		s.evaluateFormulas(getCurrentStateId());
+		s.notifyAnimationChange(getCurrentStateId(), stateId, null);
+	}
+
+	public void goToState(final String id) {
+		goToState(s.getStates().get(id));
+	}
+
+	public void goToState(final int id) {
+		goToState(String.valueOf(id));
+	}
+
+	public void randomAnim(final int steps) {
+		if (steps <= 0)
+			return;
+
+		final StateId state = getCurrentStateId();
+
+		boolean deadlock = true;
+		deadlock = s.isDeadlock(state);
+
+		if (deadlock)
+			return;
+
+		final Set<OperationId> operations = s.outgoingEdgesOf(state);
+		int size = operations.size();
+		OperationId[] op = operations.toArray(new OperationId[size]);
+		int thresh = randomGenerator.nextInt(size);
+		OperationId nextOp = op[thresh];
+
+		final boolean invariantPreserved = s.getInfo().getInvariantOk()
+				.get(state);
+
+		if (!invariantPreserved)
+			return;
+
+		step(nextOp.getId());
+
+		randomAnim(steps - 1);
+	}
+
+	public String printOps() {
+		StringBuilder sb = new StringBuilder();
+		StateId current = getCurrentStateId();
+		Collection<OperationId> opIds = s.outgoingEdgesOf(current);
+		sb.append("Operations: \n");
+		for (OperationId opId : opIds) {
+			Operation op = s.getInfo().getOp(opId);
+			sb.append("  " + op.getId() + ": " + op.toString() + "\n");
+		}
+		return sb.toString();
+	}
+
+	public String printState() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("Current State Id: " + getCurrentState() + "\n");
+		HashMap<String, String> currentState = s.getInfo().getState(
+				getCurrentStateId());
+		// FIXME: Find a way to get the names of the variables so that they can
+		// be retrieved from the map
+		if (currentState != null) {
+			Set<Entry<String, String>> entrySet = currentState.entrySet();
+			for (Entry<String, String> entry : entrySet) {
+				sb.append("  " + entry.getKey() + " -> " + entry.getValue()
+						+ "\n");
+			}
+		}
+		return sb.toString();
+	}
+
 }
--- de.prob.core.kernel/src/main/java/de/prob/statespace/StateSpace.java
+++ de.prob.core.kernel/src/main/java/de/prob/statespace/StateSpace.java
@@ -1,13 +1,9 @@
 package de.prob.statespace;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map.Entry;
-import java.util.Random;
-import java.util.Set;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -52,7 +48,6 @@ public class StateSpace extends StateSpaceGraph implements IAnimator {
 
 	private transient final IAnimator animator;
 	private final HashSet<StateId> explored = new HashSet<StateId>();
-	private final History history;
 	private final StateSpaceInfo info;
 
 	private final List<IEvalElement> formulas = new ArrayList<IEvalElement>();
@@ -61,17 +56,12 @@ public class StateSpace extends StateSpaceGraph implements IAnimator {
 
 	private final HashMap<String, StateId> states = new HashMap<String, StateId>();
 
-	private final Random randomGenerator;
-
 	@Inject
 	public StateSpace(final IAnimator animator,
 			final DirectedMultigraphProvider graphProvider,
-			final Random randomGenerator, final History history,
 			final StateSpaceInfo info) {
 		super(graphProvider.get());
 		this.animator = animator;
-		this.randomGenerator = randomGenerator;
-		this.history = history;
 		this.info = info;
 		StateId root = new StateId("root", "1", this);
 		addVertex(root);
@@ -99,7 +89,7 @@ public class StateSpace extends StateSpaceGraph implements IAnimator {
 			Operation op = new Operation(operations.id, operations.name,
 					operations.params);
 			if (!containsEdge(new OperationId(op.getId()))) {
-				info.add(operations.id, op);
+				getInfo().add(operations.id, op);
 				notifyStateSpaceChange(operations.id,
 						containsVertex(getVertex(operations.dest)));
 				StateId newState = new StateId(operations.dest,
@@ -112,7 +102,7 @@ public class StateSpace extends StateSpaceGraph implements IAnimator {
 			}
 		}
 
-		info.add(state, command);
+		getInfo().add(state, command);
 	}
 
 	public void explore(final String state) {
@@ -150,7 +140,7 @@ public class StateSpace extends StateSpaceGraph implements IAnimator {
 			Operation op = new Operation(operations.id, operations.name,
 					operations.params);
 			if (!containsEdge(new OperationId(op.getId()))) {
-				info.add(operations.id, op);
+				getInfo().add(operations.id, op);
 				notifyStateSpaceChange(operations.id,
 						containsVertex(getVertex(operations.dest)));
 				addEdge(getVertex(operations.src), getVertex(operations.dest),
@@ -159,20 +149,6 @@ public class StateSpace extends StateSpaceGraph implements IAnimator {
 			ops.add(op);
 		}
 		return ops;
-	}
-
-	/**
-	 * Finds one Operation that satisfies the operation name and predicate at
-	 * the current state
-	 * 
-	 * @param opName
-	 * @param predicate
-	 * @return one operations that meets the specifications @ * @throws
-	 *         BException
-	 */
-	public Operation findOneOp(final String opName, final String predicate)
-			throws BException {
-		return opFromPredicate(getCurrentState(), opName, predicate, 1).get(0);
 	}
 
 	/**
@@ -198,130 +174,10 @@ public class StateSpace extends StateSpaceGraph implements IAnimator {
 	 * @param state
 	 * @return returns if a specific state is explored
 	 */
-	private boolean isExplored(final StateId state) {
+	public boolean isExplored(final StateId state) {
 		if (!containsVertex(state))
 			throw new IllegalArgumentException("Unknown State id");
 		return explored.contains(state);
-	}
-
-	// MOVE WITHIN STATESPACE
-
-	/**
-	 * Finds an Operation with opName and predicate and carries out one
-	 * animation step with that Operation
-	 * 
-	 * @param opName
-	 * @param predicate
-	 * @throws BException
-	 */
-	public void stepWithOp(final String opName, final String predicate)
-			throws BException {
-		Operation op = findOneOp(opName, predicate);
-		step(op.getId());
-	}
-
-	public void goToState(final String id) {
-		goToState(states.get(id));
-	}
-
-	public void goToState(final int id) {
-		goToState(String.valueOf(id));
-	}
-
-	/**
-	 * Moves from the current state to the state at stateId.
-	 * 
-	 * @param stateId
-	 */
-	public void goToState(final StateId stateId) {
-		if (!containsVertex(stateId))
-			throw new IllegalArgumentException("state does not exist");
-		if (!isExplored(stateId)) {
-			explore(stateId);
-		}
-
-		history.add(stateId.getId(), null);
-		evaluateFormulas();
-		notifyAnimationChange(getCurrentState(), stateId, null);
-	}
-
-	/**
-	 * Carries out one step in the animation with the id from an Operation. If
-	 * the opId is contained in the outgoing edges (it is enabled) explore it
-	 * (if not explored) and add state to history
-	 * 
-	 * @param opId
-	 */
-	public void step(final String opId) {
-		OperationId op = new OperationId(opId);
-		if (!outgoingEdgesOf(getCurrentState()).contains(op))
-			throw new IllegalArgumentException(opId
-					+ " is not a valid operation on this state");
-
-		StateId newState = getEdgeTarget(op);
-		if (!isExplored(newState)) {
-			explore(newState.getId());
-		}
-		history.add(newState.getId(), opId);
-		evaluateFormulas();
-		notifyAnimationChange(getEdgeSource(op), getEdgeTarget(op), op);
-	}
-
-	public void step(final int i) {
-		String opId = String.valueOf(i);
-		step(opId);
-	}
-
-	/**
-	 * Moves one step back in the animation if this is possible.
-	 */
-	public void back() {
-		if (history.canGoBack()) {
-			StateId oldState = getCurrentState();
-			String opId = history.getCurrentTransition();
-
-			history.back();
-			evaluateFormulas();
-
-			if (opId != null) {
-				OperationId op = new OperationId(opId);
-				notifyAnimationChange(getEdgeSource(op), getEdgeTarget(op), op);
-			} else {
-				notifyAnimationChange(oldState, getCurrentState(), null);
-			}
-		}
-	}
-
-	/**
-	 * Moves one step forward in the animation if this is possible
-	 */
-	public void forward() {
-		if (history.canGoForward()) {
-			StateId oldState = getCurrentState();
-
-			history.forward();
-			evaluateFormulas();
-
-			String opId = history.getCurrentTransition();
-			if (opId != null) {
-				OperationId op = new OperationId(opId);
-				notifyAnimationChange(getEdgeSource(op), getEdgeTarget(op), op);
-			} else {
-				notifyAnimationChange(oldState, getCurrentState(), null);
-			}
-		}
-	}
-
-	/**
-	 * returns the state id of the current state in the animation
-	 * 
-	 * @return returns the current state from the animation
-	 */
-
-	public StateId getCurrentState() {
-		String currentState = history.getCurrentState();
-		StateId stateId = states.get(currentState);
-		return stateId;
 	}
 
 	// AUTOMATED ANIMATION IN STATESPACE
@@ -330,33 +186,6 @@ public class StateSpace extends StateSpaceGraph implements IAnimator {
 	 * 
 	 * @param steps
 	 */
-	public void randomAnim(final int steps) {
-		if (steps <= 0)
-			return;
-
-		final StateId state = getCurrentState();
-
-		boolean deadlock = true;
-		deadlock = isDeadlock(state);
-
-		if (deadlock)
-			return;
-
-		final Set<OperationId> operations = outgoingEdgesOf(state);
-		int size = operations.size();
-		OperationId[] op = operations.toArray(new OperationId[size]);
-		int thresh = randomGenerator.nextInt(size);
-		OperationId nextOp = op[thresh];
-
-		final boolean invariantPreserved = info.getInvariantOk().get(state);
-
-		if (!invariantPreserved)
-			return;
-
-		step(nextOp.getId());
-
-		randomAnim(steps - 1);
-	}
 
 	// EVALUATE PART OF STATESPACE
 
@@ -368,59 +197,15 @@ public class StateSpace extends StateSpaceGraph implements IAnimator {
 	 * @param formula
 	 * @throws BException
 	 */
-	public void addUserFormula(final String formula) throws BException {
-		formulas.add(new ClassicalBEvalElement(formula));
-		List<EvaluationResult> result = evaluate(formulas);
-		HashMap<String, String> varsAtState = info.getState(getCurrentState());
-		for (EvaluationResult evaluationResult : result) {
-			varsAtState.put(evaluationResult.code, evaluationResult.value);
-		}
-	}
-
-	/**
-	 * Evaluates all the user formulas for the current state and updates the map
-	 * of variables in the info object accordingly
-	 */
-	public void evaluateFormulas() {
-		try {
-			List<EvaluationResult> evaluate = evaluate(formulas);
-			HashMap<String, String> varsAtCurrentState = info
-					.getState(getCurrentState());
-			for (EvaluationResult result : evaluate) {
-				varsAtCurrentState.put(result.code, result.value);
-			}
-		} catch (BException e) {
-			logger.error("Parse Exception in formula");
-		}
-	}
-
-	/**
-	 * Evaluates a single formula or an array of formulas (represented as
-	 * strings) and returns a list of EvaluationResults for the current state.
-	 * 
-	 * @param code
-	 * @return returns a list of evalutation results @ * @throws BException
-	 */
-	public List<EvaluationResult> evaluate(final List<IEvalElement> code)
-			throws BException {
-		return eval(getCurrentState().getId(), code);
-	}
-
-	public List<EvaluationResult> evaluate(final IEvalElement code)
-			throws BException {
-		List<IEvalElement> list = new ArrayList<IEvalElement>();
-		list.add(code);
-		return evaluate(list);
-	}
-
-	public List<EvaluationResult> evaluate(final String... code)
-			throws BException {
-		List<IEvalElement> list = new ArrayList<IEvalElement>();
-		for (String c : code) {
-			list.add(new ClassicalBEvalElement(c));
-		}
-		return evaluate(list);
-	}
+	// public void addUserFormula(final String formula) throws BException {
+	// formulas.add(new ClassicalBEvalElement(formula));
+	// List<EvaluationResult> result = evaluate(formulas);
+	// HashMap<String, String> varsAtState = getInfo().getState(
+	// getCurrentState());
+	// for (EvaluationResult evaluationResult : result) {
+	// varsAtState.put(evaluationResult.code, evaluationResult.value);
+	// }
+	// }
 
 	/**
 	 * Evaluates a single formula or an array of formulas (represented as
@@ -443,6 +228,18 @@ public class StateSpace extends StateSpaceGraph implements IAnimator {
 		execute(command);
 
 		return command.getValues();
+	}
+
+	public void evaluateFormulas(final StateId state) {
+		try {
+			List<EvaluationResult> evaluate = eval(state.getId(), formulas);
+			HashMap<String, String> varsAtCurrentState = info.getState(state);
+			for (EvaluationResult result : evaluate) {
+				varsAtCurrentState.put(result.code, result.value);
+			}
+		} catch (BException e) {
+			logger.error("Parse Exception in formula");
+		}
 	}
 
 	public List<EvaluationResult> eval(final String state, final String... code)
@@ -492,7 +289,7 @@ public class StateSpace extends StateSpaceGraph implements IAnimator {
 		stateSpaceListeners.add(l);
 	}
 
-	private void notifyAnimationChange(final StateId stateId,
+	public void notifyAnimationChange(final StateId stateId,
 			final StateId fromState, final OperationId withOp) {
 		for (IAnimationListener listener : animationListeners) {
 			listener.currentStateChanged(fromState, stateId, withOp);
@@ -510,42 +307,12 @@ public class StateSpace extends StateSpaceGraph implements IAnimator {
 	@Override
 	public String toString() {
 		String result = "";
-		result += printState();
-		result += printOps();
 		result += super.toString();
 		return result;
 	}
 
-	public String printState() {
-		StringBuilder sb = new StringBuilder();
-		sb.append("Current State Id: " + getCurrentState() + "\n");
-		HashMap<String, String> currentState = info.getState(getCurrentState());
-		// FIXME: Find a way to get the names of the variables so that they can
-		// be retrieved from the map
-		if (currentState != null) {
-			Set<Entry<String, String>> entrySet = currentState.entrySet();
-			for (Entry<String, String> entry : entrySet) {
-				sb.append("  " + entry.getKey() + " -> " + entry.getValue()
-						+ "\n");
-			}
-		}
-		return sb.toString();
-	}
-
-	public String printOps() {
-		StringBuilder sb = new StringBuilder();
-		StateId current = getCurrentState();
-		Collection<OperationId> opIds = outgoingEdgesOf(current);
-		sb.append("Operations: \n");
-		for (OperationId opId : opIds) {
-			Operation op = info.getOp(opId);
-			sb.append("  " + op.getId() + ": " + op.toString() + "\n");
-		}
-		return sb.toString();
-	}
-
 	public String printInfo() {
-		return info.toString();
+		return getInfo().toString();
 	}
 
 	// assert !s.getOutEdges(s.getCurrentState()).contains(new OperationId("1"))
@@ -555,6 +322,14 @@ public class StateSpace extends StateSpaceGraph implements IAnimator {
 
 	public boolean isOutEdge(final String stateId, final String opId) {
 		return isOutEdge(getVertex(stateId), new OperationId(opId));
+	}
+
+	public HashMap<String, StateId> getStates() {
+		return states;
+	}
+
+	public StateSpaceInfo getInfo() {
+		return info;
 	}
 
 }
