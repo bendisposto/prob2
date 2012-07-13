314abc4f3f3b5d8f030ac66b413950f59845d37a
Jens Bendisposto <jens@bendisposto.de>
2012-07-13 14:17:20 +0200

    look for a free port (PROBCORE-68)

--- de.prob.core.kernel/src/main/java/de/prob/webconsole/WebConsole.java
+++ de.prob.core.kernel/src/main/java/de/prob/webconsole/WebConsole.java
@@ -1,20 +1,73 @@
 package de.prob.webconsole;
 
 import java.awt.Desktop;
+import java.io.IOException;
+import java.net.DatagramSocket;
+import java.net.ServerSocket;
 import java.net.URI;
 import java.security.ProtectionDomain;
 
+import org.eclipse.jetty.server.Connector;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.nio.SelectChannelConnector;
 import org.eclipse.jetty.webapp.WebAppContext;
 
+/**
+ * @author bendisposto
+ *
+ */
 public class WebConsole {
+
+	
+	
+	/**
+	 * Taken from Apache MINA 
+	 * @param port
+	 * @return
+	 */
+	public static boolean available(int port) {
+		if (port < 8080 || port > 8180) {
+			throw new IllegalArgumentException("Invalid start port: " + port);
+		}
+
+		ServerSocket ss = null;
+		DatagramSocket ds = null;
+		try {
+			ss = new ServerSocket(port);
+			ss.setReuseAddress(true);
+			ds = new DatagramSocket(port);
+			ds.setReuseAddress(true);
+			return true;
+		} catch (IOException e) {
+		} finally {
+			if (ds != null) {
+				ds.close();
+			}
+
+			if (ss != null) {
+				try {
+					ss.close();
+				} catch (IOException e) {
+					/* should not be thrown */
+				}
+			}
+		}
+
+		return false;
+	}
 
 	public static void run() throws Exception {
 
 		System.setProperty("org.eclipse.jetty.util.log.class", "");
 
-		Server server = new Server(8080);
+		int port = 8080;
+
+		while (!available(port)) {
+			port++;
+		}
+
+		Server server = new Server(port);
 
 		ProtectionDomain protectionDomain = WebConsole.class
 				.getProtectionDomain();
@@ -30,9 +83,8 @@ public class WebConsole {
 		server.setHandler(handlers);
 
 		server.start();
-		Desktop.getDesktop().browse(new URI("http://localhost:8080"));
+		Desktop.getDesktop().browse(new URI("http://localhost:" + port));
 		server.join();
 
 	}
-
 }
