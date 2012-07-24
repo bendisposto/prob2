d5ebd3495de285ec9c2bd307f7ba910a270b11b9
Joy Clark <joyclark2004@msn.com>
2012-07-24 15:48:42 +0200

    Deleted the following commands because they are no longer relevant to the new core

--- de.prob.core.kernel/src/main/java/de/prob/animator/command/notImplemented/ISimpleTextCommand.java
+++
@@ -1,7 +0,0 @@
-package de.prob.animator.command.notImplemented;
-
-import de.prob.animator.command.ICommand;
-
-public interface ISimpleTextCommand extends ICommand{
-	String getResultingText();
-}
