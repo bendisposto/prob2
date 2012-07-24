e49d8277ce2f4c4ce81d61e553e1471ea5067898
Joy Clark <joyclark2004@msn.com>
2012-07-24 12:21:21 +0200

    Added integration test(PROBCORE-5) for implementation of PROBCORE-65

---
+++ de.prob.core.kernel/groovyTests/syncronizedLift.groovy
@@ -0,0 +1,19 @@
+s = api.b_load(dir+"/machines/Lift.mch").getStatespace()
+x = api.b_load(dir+"/machines/Lift.mch").getStatespace()
+import de.prob.statespace.*
+h = new SyncHistory([new History(s),new History(x)],["inc"])
+assert h.toString() == "0: [] Current Transition is: null\n1: [] Current Transition is: null\n"
+h = h.add("\$initialise_machine",["4"],0)
+assert h.toString() == "0: [0] Current Transition is: 0\n1: [] Current Transition is: null\n"
+h = h.add("\$initialise_machine",["4"],1)
+assert h.toString() == "0: [0] Current Transition is: 0\n1: [0] Current Transition is: 0\n"
+h = h.add("inc",[])
+assert h.toString() == "0: [0, 1] Current Transition is: 1\n1: [0, 1] Current Transition is: 1\n"
+h = h.add("inc",[])
+assert h.toString() == "0: [0, 1, 4] Current Transition is: 4\n1: [0, 1, 4] Current Transition is: 4\n"
+h = h.add("dec",[],1)
+assert h.toString() == "0: [0, 1, 4] Current Transition is: 4\n1: [0, 1, 4, 8] Current Transition is: 8\n"
+h = h.back()
+assert h.toString() == "0: [0, 1, 4] Current Transition is: 4\n1: [0, 1, 4] Current Transition is: 4\n"
+h = h.forward()
+assert h.toString() == "0: [0, 1, 4] Current Transition is: 4\n1: [0, 1, 4, 8] Current Transition is: 8\n"
