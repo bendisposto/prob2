eababb7f55d0831afd0487406fb53085b2a42a0b
Jens Bendisposto <jens@bendisposto.de>
2012-09-11 15:32:26 +0200

    Load Model (configuration only, implementation is missing)

---
+++ de.prob.ui/src/main/java/de/prob/ui/eventb/StartAnimationHandler.java
@@ -0,0 +1,18 @@
+package de.prob.ui.eventb;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.IHandler;
+import org.eclipse.core.commands.IHandlerListener;
+
+public class StartAnimationHandler extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		return null;
+	}
+
+
+
+}
