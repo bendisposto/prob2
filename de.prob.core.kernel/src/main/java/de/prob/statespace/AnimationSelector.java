c378849b25f3246e5327c08bd9c97e8ea7a833c4
Joy Clark <joyclark2004@msn.com>
2012-09-20 14:40:51 +0200

    PROBCORE-96 Updated AnimationSelector so that it is possible to identify the model for a given history

--- de.prob.core.kernel/src/main/java/de/prob/statespace/AnimationSelector.java
+++ de.prob.core.kernel/src/main/java/de/prob/statespace/AnimationSelector.java
@@ -1,9 +1,13 @@
 package de.prob.statespace;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.google.inject.Singleton;
+
+import de.prob.model.representation.AbstractModel;
 
 @Singleton
 public class AnimationSelector implements IAnimationListener {
@@ -11,38 +15,42 @@ public class AnimationSelector implements IAnimationListener {
 	List<IHistoryChangeListener> listeners = new ArrayList<IHistoryChangeListener>();
 	List<History> histories = new ArrayList<History>();
 	History currentHistory = null;
+	Map<History, AbstractModel> models = new HashMap<History, AbstractModel>();
 	
 	public void registerHistoryChangeListener(IHistoryChangeListener listener) {
 		listeners.add(listener);
 		if( currentHistory != null ) {
-			listener.historyChange(currentHistory);
+			notifyHistoryChange(currentHistory,models.get(currentHistory));
 		}
 	}
 	
 	@Override
 	public void currentStateChanged(History oldHistory, History newHistory) {
 		if(oldHistory.equals(currentHistory)) {
-			notifyHistoryChange(newHistory);
+			notifyHistoryChange(newHistory,models.get(oldHistory));
 		}
 		histories.set(histories.indexOf(oldHistory), newHistory);
+		models.put(newHistory, models.get(oldHistory));
+		models.remove(oldHistory);
 		currentHistory = newHistory;
 	}
 
 	public void changeCurrentHistory(History history) {
 		currentHistory = history;
-		notifyHistoryChange(history);
+		notifyHistoryChange(history, models.get(history));
 	}
 	
-	public void addNewHistory(History history) {
+	public void addNewHistory(History history, AbstractModel model) {
 		histories.add(history);
+		models.put(history, model);
 		history.registerAnimationListener(this);
 		currentHistory = history;
-		notifyHistoryChange(history);
+		notifyHistoryChange(history, model);
 	}
 	
-	public void notifyHistoryChange(History history) {
+	public void notifyHistoryChange(History history, AbstractModel model) {
 		for (IHistoryChangeListener listener : listeners) {
-			listener.historyChange(history);
+			listener.historyChange(history,model);
 		}
 	}
 	
--- de.prob.core.kernel/src/main/java/de/prob/statespace/IHistoryChangeListener.java
+++ de.prob.core.kernel/src/main/java/de/prob/statespace/IHistoryChangeListener.java
@@ -1,5 +1,7 @@
 package de.prob.statespace;
 
+import de.prob.model.representation.AbstractModel;
+
 public interface IHistoryChangeListener {
-	public void historyChange(History history);
+	public void historyChange(History history, AbstractModel model);
 }
