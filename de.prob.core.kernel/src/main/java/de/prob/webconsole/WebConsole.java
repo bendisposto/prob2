093f6727be86ae7b53e2c0a7fe47c88bd99cf87f
Jens Bendisposto <jens@bendisposto.de>
2012-09-28 15:46:04 +0200

    fixed PROBCORE-117

--- de.prob.core.kernel/src/main/java/de/prob/webconsole/WebConsole.java
+++ de.prob.core.kernel/src/main/java/de/prob/webconsole/WebConsole.java
@@ -2,6 +2,7 @@ package de.prob.webconsole;
 
 import java.awt.Desktop;
 import java.io.IOException;
+import java.net.BindException;
 import java.net.DatagramSocket;
 import java.net.ServerSocket;
 import java.net.URI;
@@ -20,43 +21,7 @@ import org.eclipse.jetty.webapp.WebAppContext;
  */
 public class WebConsole {
 
-	private final static int PORT = findPort(8080);
-
-	/**
-	 * Taken from Apache MINA
-	 * 
-	 * @param port
-	 * @return next available port
-	 */
-	public static boolean available(final int port) {
-		if (port < 8080 || port > 8180)
-			throw new IllegalArgumentException("Invalid start port: " + port);
-
-		ServerSocket ss = null;
-		DatagramSocket ds = null;
-		try {
-			ss = new ServerSocket(port);
-			ss.setReuseAddress(true);
-			ds = new DatagramSocket(port);
-			ds.setReuseAddress(true);
-			return true;
-		} catch (IOException e) {
-		} finally {
-			if (ds != null) {
-				ds.close();
-			}
-
-			if (ss != null) {
-				try {
-					ss.close();
-				} catch (IOException e) {
-					/* should not be thrown */
-				}
-			}
-		}
-
-		return false;
-	}
+	private static int PORT;
 
 	public static void run() throws Exception {
 		run(new Runnable() {
@@ -76,19 +41,10 @@ public class WebConsole {
 	}
 
 	public static void run(final Runnable openBrowser) throws Exception {
-		
+
 		System.setProperty("org.eclipse.jetty.util.log.class", "");
-		
-				
+
 		Server server = new Server();
-
-		Connector connector = new SelectChannelConnector();
-		connector.setPort(PORT);
-		connector.setServer(server);
-		String hostname = System.getProperty("prob.host", "127.0.0.1");
-		connector.setHost(hostname);
-
-		server.setConnectors(new Connector[] { connector });
 
 		ProtectionDomain protectionDomain = WebConsole.class
 				.getProtectionDomain();
@@ -106,23 +62,31 @@ public class WebConsole {
 		handlers.addHandler(context);
 		server.setHandler(handlers);
 
-		server.start();
+		int port = 8080;
+		boolean found = false;
+		do {
+			try {
+				Connector connector = new SelectChannelConnector();
+				connector.setServer(server);
+				String hostname = System.getProperty("prob.host", "127.0.0.1");
+				connector.setHost(hostname);
+				server.setConnectors(new Connector[] { connector });
+				connector.setPort(port);
+				server.start();
+				found = true;
+			} catch (BindException ex) {
+				port++;
+			}
+		} while (!found && port < 8180);
+
+		if (!found) {
+			throw new BindException("No free port found between 8080 and 8179");
+		}
+
+		PORT = port;
+
 		openBrowser.run();
 		server.join();
-
-	}
-
-	private static int findPort(int port) {
-		boolean found = false;
-		int p = port;
-		do {
-			found = available(p);
-			if (found)
-				return p;
-		} while (port < 8180);
-		throw new IllegalStateException(
-				"Cannot find an open port in the range between 8080 and 8179");
-
 	}
 
 	public static int getPort() {
