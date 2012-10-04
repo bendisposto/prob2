d84d18ddf25f1f5892138c817c190b3bdfb985f9
Jens Bendisposto <jens@bendisposto.de>
2012-10-05 00:15:45 +0200

    Working on a better integration of the console within eclipse

---
+++ de.prob.core.kernel/src/main/resources/javascripts/console.js
@@ -0,0 +1,95 @@
+var controller;
+var loglevel = "ERROR";
+var bindingTable;
+
+function onValidate(line) {
+	return true;
+};
+
+function onComplete(line,column, perform) {
+	var result;
+	$.getJSON("complete", {
+		input : line,
+		col: column
+	}, function(json) {
+		perform(json);
+	});
+};
+
+function updateImports() {
+	var impdiv = $("#imports")[0];
+	$.getJSON("imports", {}, function(data) {
+		impdiv.value = data;
+	});
+}
+
+// function onHandle(line, report) {
+// $.getJSON("evaluate", {
+// input : line
+// }, function(data) {
+// bindingTable.fnReloadAjax()
+// updateImports()
+// if (!data.continued) {
+// controller.lePrompt = false;
+// report([ {
+// msg : data.output,
+// className : "jquery-console-message-value"
+// } ]);
+//
+// } else {
+// controller.lePrompt = true;
+// report();
+// }
+// });
+// };
+
+function onHandle(line, report) {
+	$.getJSON("evaluate", {
+		input : line
+	}, function(data) {
+		if (!data.continued) {
+			controller.lePrompt = false;
+			report([ {
+				msg : data.output,
+				className : "jquery-console-message-value"
+			} ]);
+
+		} else {
+			controller.lePrompt = true;
+			report();
+		}
+	});
+};
+
+function switchLogLevel() {
+	var level = "ERROR";
+	var levela = $("#loglevel")[0];
+	if (loglevel == "ERROR") {
+		level = "TRACE";
+	}
+	$.getJSON("loglevel", {
+		input : level
+	}, function(data) {
+		levela.innerHTML = data.output;
+		loglevel = data.output;
+	});
+
+}
+
+
+function initialize() {
+	lePrompt = "ProB> ";
+	var levela = $("#loglevel")[0];
+ 	controller = $("#console").console({
+		welcomeMessage : 'ProB 2.0 console',
+		promptLabel : 'ProB> ',
+		continuedPromptLabel : '----| ',
+		commandValidate : onValidate,
+		commandHandle : onHandle,
+		completionHandle : onComplete,
+		autofocus : true,
+		animateScroll : true,
+		promptHistory : true
+	});
+
+}
