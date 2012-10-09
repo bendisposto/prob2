88646017615714345135d0b2ef77c43e274a4399
Jens Bendisposto <jens@bendisposto.de>
2012-10-09 09:26:02 +0200

    Fixed PROBCORE-130

--- de.prob.core.kernel/src/main/java/de/prob/webconsole/shellcommands/LoadCommand.java
+++ de.prob.core.kernel/src/main/java/de/prob/webconsole/shellcommands/LoadCommand.java
@@ -8,6 +8,9 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import jline.FileNameCompletor;
 
 import com.google.common.base.Joiner;
@@ -16,17 +19,35 @@ import de.prob.webconsole.GroovyExecution;
 
 public class LoadCommand extends AbstractShellCommand {
 
+	private final static Logger LOGGER = LoggerFactory
+			.getLogger(LoadCommand.class);
+
 	@Override
 	public Object perform(List<String> m, GroovyExecution exec)
 			throws IOException {
-		assert m.size() > 1;
+
+		if (m.size() != 2) {
+			String msg = "Load commend takes exactly one parameter, a filename.";
+			LOGGER.error(msg);
+			return "error: " + msg;
+		}
+
 		String filename = m.get(1);
 
 		if (filename.startsWith("~")) {
-			filename = filename.replaceFirst("~", System.getProperty("user.home"));
+			filename = filename.replaceFirst("~",
+					System.getProperty("user.home"));
 		}
 
 		int lastDot = filename.lastIndexOf('.');
+
+		File f = new File(filename);
+		if (!f.exists()) {
+			String msg = "File '" + filename + "' does not exist.";
+			LOGGER.error(msg);
+			return "error: " + msg;
+		}
+
 		String extension = filename.substring(lastDot, filename.length());
 
 		if (extension.equals(".mch") || extension.equals(".ref")
@@ -48,8 +69,9 @@ public class LoadCommand extends AbstractShellCommand {
 			}
 			return model;
 		}
-		System.out.println("Unknown filetype: " + extension);
-		return null;
+		String msg = "Unknown filetype: " + extension;
+		LOGGER.error(msg);
+		return "error: " + msg;
 	}
 
 	private synchronized String freshVar(GroovyExecution exec, String string) {
