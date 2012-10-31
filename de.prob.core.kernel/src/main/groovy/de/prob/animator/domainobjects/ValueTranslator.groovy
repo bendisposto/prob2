93b6baa10284f23eedd9e395e275fcd2c71afbe7
Jens Bendisposto <jens@bendisposto.de>
2012-10-31 15:33:29 +0100

    more work on value translation PROBCORE-107

--- de.prob.core.kernel/src/main/groovy/de/prob/animator/domainobjects/ValueTranslator.groovy
+++ de.prob.core.kernel/src/main/groovy/de/prob/animator/domainobjects/ValueTranslator.groovy
@@ -3,14 +3,17 @@ package de.prob.animator.domainobjects
 import groovy.transform.TupleConstructor
 
 import com.google.common.base.Joiner
+import com.sun.tools.jdi.LinkedHashMap;
 
+import de.prob.prolog.term.CompoundPrologTerm;
+import de.prob.prolog.term.IntegerPrologTerm;
 import de.prob.prolog.term.ListPrologTerm
 import de.prob.prolog.term.PrologTerm
 
 @TupleConstructor class  Tuple {
-  def first
-  def second   
-  @Override
+	def first
+	def second
+	@Override
 	public String toString() {
 		return "(${first},${second})";
 	}
@@ -27,7 +30,7 @@ class ValueTranslator {
 			makeSet(node.getArgument(5),left)
 		}
 	}
-	
+
 	def makeSetFromList(ListPrologTerm list) {
 		def res = new HashSet()
 		for (PrologTerm t : list) {
@@ -35,8 +38,9 @@ class ValueTranslator {
 		}
 		res
 	}
-	
-	def makeRecord(ListPrologTerm fields) { // field(Name1,Val)
+
+	def makeRecord(ListPrologTerm fields) {
+		// field(Name1,Val)
 		def res = new LinkedHashMap()
 		for (PrologTerm prologTerm : fields) {
 			res.put(prologTerm.getArgument(1).functor, translate(prologTerm.getArgument(2)))
@@ -44,7 +48,32 @@ class ValueTranslator {
 		res
 	}
 
-	def translate(PrologTerm term) {
+	def PrologTerm toProlog(object) {
+		if (object instanceof BigInteger) {
+			return new CompoundPrologTerm("int", new IntegerPrologTerm(object))
+		}
+		if (object instanceof Tuple) {
+			return new CompoundPrologTerm("','", toProlog(object.first), toProlog(object.second))
+		}
+		if (object instanceof Boolean) {
+			return object ? new CompoundPrologTerm("pred_true") : new CompoundPrologTerm("pred_false")
+		}
+		if (object instanceof HashSet) {
+			return new ListPrologTerm( object.collect { toProlog(it) }.toArray(new PrologTerm[object.size()]));
+		}
+		if (object instanceof LinkedHashMap) {
+			return new CompoundPrologTerm('rec', 
+				new ListPrologTerm(object.collect { new CompoundPrologTerm("field", it.key,toProlog(it.value)) }.toArray(new PrologTerm[object.size()])));
+		}
+		if (object instanceof ArrayList) {
+			return new CompoundPrologTerm("fd",new CompoundPrologTerm(object[1]),new IntegerPrologTerm(object[0]));
+		}
+		if (object instanceof String) {
+			return new CompoundPrologTerm("string", new CompoundPrologTerm(object));
+		}
+	}
+
+	def toGroovy(PrologTerm term) {
 		if (term instanceof ListPrologTerm) {
 			return makeSetFromList(term);
 		}
@@ -58,33 +87,31 @@ class ValueTranslator {
 			break
 			case "avl_set": makeSet(term.getArgument(1),new HashSet())
 			break
-			case ",": new Tuple(translate(term.getArgument(1)), translate(term.getArgument(2))) 
+			case ",": new Tuple(translate(term.getArgument(1)), translate(term.getArgument(2)))
 			break
-			case "fd":  [term.getArgument(2).functor,term.getArgument(1)] 
+			case "fd":  [term.getArgument(2).functor,term.getArgument(1)]
 			break
 			case "string":  term.getArgument(1).functor
-			break
-			case "rec": makeRecord(term.getArgument(1))
 			break
 			case "rec": makeRecord(term.getArgument(1))
 			break
 			default: throw new IllegalArgumentException("Don't know how to translate "+term)
 		}
 	}
-	
+
 
 
 	def String asString(groovy_state) {
 		switch (groovy_state) {
 			case HashSet: "{"+ Joiner.on(",").join(groovy_state.collect {asString(it)}) +"}"
-				break
+			break
 			case LinkedHashMap: "rec("+Joiner.on(",").join(groovy_state.collect {it.key+":"+asString(it.value)})+")"
-				break
-			case ArrayList: if (groovy_state.isEmpty()) "{}" else  asString(groovy_state.get(0))+asString(groovy_state.get(1))
-			    break
+			break
+			case ArrayList:  asString(groovy_state.get(0))+asString(groovy_state.get(1))
+			break
 			default: groovy_state.toString();
 		}
 	}
-	
-	
+
+
 }
