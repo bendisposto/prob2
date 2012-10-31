2ab07a527683d7e96e75e3c5c00f0466a5775975
Jens Bendisposto <jens@bendisposto.de>
2012-10-31 14:16:11 +0100

    Support records & Tuples PROBCORE-107

--- de.prob.core.kernel/src/main/groovy/de/prob/animator/domainobjects/ValueTranslator.groovy
+++ de.prob.core.kernel/src/main/groovy/de/prob/animator/domainobjects/ValueTranslator.groovy
@@ -1,9 +1,20 @@
 package de.prob.animator.domainobjects
 
-import com.google.common.base.Joiner;
+import groovy.transform.TupleConstructor
 
-import de.prob.prolog.term.IntegerPrologTerm
+import com.google.common.base.Joiner
+
+import de.prob.prolog.term.ListPrologTerm
 import de.prob.prolog.term.PrologTerm
+
+@TupleConstructor class  Tuple {
+  def first
+  def second   
+  @Override
+	public String toString() {
+		return "(${first},${second})";
+	}
+}
 
 class ValueTranslator {
 
@@ -16,26 +27,49 @@ class ValueTranslator {
 			makeSet(node.getArgument(5),left)
 		}
 	}
+	
+	def makeSetFromList(ListPrologTerm list) {
+		def res = new HashSet()
+		for (PrologTerm t : list) {
+			res.add(translate(t))
+		}
+		res
+	}
+	
+	def makeRecord(ListPrologTerm fields) { // field(Name1,Val)
+		def res = new LinkedHashMap()
+		for (PrologTerm prologTerm : fields) {
+			res.put(prologTerm.getArgument(1).functor, translate(prologTerm.getArgument(2)))
+		}
+		res
+	}
 
 	def translate(PrologTerm term) {
-		if (term instanceof IntegerPrologTerm) 
-			term.getValue() else  {
+		if (term instanceof ListPrologTerm) {
+			return makeSetFromList(term);
+		}
 		def termFunctor = term.functor
 		switch (termFunctor) {
 			case "int": term.getArgument(1).getValue()
-			 break
+			break
 			case "pred_true": true
 			break
 			case "pred_false": false
 			break
 			case "avl_set": makeSet(term.getArgument(1),new HashSet())
 			break
-			case ",": [1:translate(term.getArgument(1)),2:translate(term.getArgument(2))]
+			case ",": new Tuple(translate(term.getArgument(1)), translate(term.getArgument(2))) 
 			break
 			case "fd":  [term.getArgument(2).functor,term.getArgument(1)] 
 			break
-			default: []
-		}}
+			case "string":  term.getArgument(1).functor
+			break
+			case "rec": makeRecord(term.getArgument(1))
+			break
+			case "rec": makeRecord(term.getArgument(1))
+			break
+			default: throw new IllegalArgumentException("Don't know how to translate "+term)
+		}
 	}
 	
 
@@ -44,11 +78,13 @@ class ValueTranslator {
 		switch (groovy_state) {
 			case HashSet: "{"+ Joiner.on(",").join(groovy_state.collect {asString(it)}) +"}"
 				break
-			case LinkedHashMap: "("+asString(groovy_state.get(1))+","+asString(groovy_state.get(2))+")"
+			case LinkedHashMap: "rec("+Joiner.on(",").join(groovy_state.collect {it.key+":"+asString(it.value)})+")"
 				break
 			case ArrayList: if (groovy_state.isEmpty()) "{}" else  asString(groovy_state.get(0))+asString(groovy_state.get(1))
 			    break
 			default: groovy_state.toString();
 		}
 	}
+	
+	
 }
