69784e40a3c6fbcb7090d6f4fff18a5d4d3e9abc
Joy Clark <joyclark2004@msn.com>
2012-11-06 18:28:57 +0100

    PROBCORE-152 remove unused objects

--- de.prob.core.kernel/src/main/java/de/prob/model/eventb/newdom/EventBSet.java
+++
@@ -1,9 +0,0 @@
-package de.prob.model.eventb.newdom;
-
-import de.prob.model.representation.newdom.Set;
-
-public class EventBSet extends Set {
-	public EventBSet(final String name) {
-		super(name);
-	}
-}
