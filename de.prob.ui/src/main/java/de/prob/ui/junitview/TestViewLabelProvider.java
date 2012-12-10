aae414cc78852a4d48d57cc5c8f425959378a42f
Joy Clark <joyclark2004@msn.com>
2012-12-10 16:50:23 +0100

    PROBCORE-176 Fixed the label provider so that the suite icons and test icons now appear.

--- de.prob.ui/src/main/java/de/prob/ui/junitview/TestViewLabelProvider.java
+++ de.prob.ui/src/main/java/de/prob/ui/junitview/TestViewLabelProvider.java
@@ -1,30 +1,52 @@
 package de.prob.ui.junitview;
 
+import java.util.Set;
+
 import junit.framework.Test;
+import junit.framework.TestCase;
 import junit.framework.TestFailure;
+import junit.framework.TestSuite;
 
 import org.eclipse.jface.viewers.ITableLabelProvider;
 import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.swt.graphics.Image;
-import org.eclipse.ui.ISharedImages;
-import org.eclipse.ui.PlatformUI;
+
+import com.google.common.collect.SetMultimap;
 
 import de.prob.ui.Activator;
 
 public class TestViewLabelProvider extends LabelProvider implements
 		ITableLabelProvider {
 	private final Image suite_err = Activator.getDefault().getImageRegistry()
-			.getDescriptor(Activator.JUNIT_TSUITE_OK).createImage();
+			.getDescriptor(Activator.JUNIT_TSUITE_ERROR).createImage();
 	private final Image suite_fail = Activator.getDefault().getImageRegistry()
 			.getDescriptor(Activator.JUNIT_TSUITE_FAIL).createImage();
 	private final Image suite_ok = Activator.getDefault().getImageRegistry()
-			.getDescriptor(Activator.JUNIT_TSUITE_ERROR).createImage();
+			.getDescriptor(Activator.JUNIT_TSUITE_OK).createImage();
 	private final Image test_err = Activator.getDefault().getImageRegistry()
 			.getDescriptor(Activator.JUNIT_TEST_ERR).createImage();
 	private final Image test_fail = Activator.getDefault().getImageRegistry()
 			.getDescriptor(Activator.JUNIT_TEST_FAIL).createImage();
 	private final Image test_ok = Activator.getDefault().getImageRegistry()
 			.getDescriptor(Activator.JUNIT_TEST_OK).createImage();
+	private SetMultimap<Class<TestSuite>, Object> map;
+
+	@Override
+	public String getText(final Object element) {
+		if (element instanceof Class<?>) {
+			return ((Class) element).getSimpleName();
+		}
+		if (element instanceof TestCase) {
+			return ((TestCase) element).getName();
+		}
+		if (element instanceof TestFailure) {
+			Test failedTest = ((TestFailure) element).failedTest();
+			if (failedTest instanceof TestCase) {
+				return ((TestCase) failedTest).getName();
+			}
+		}
+		return element.toString();
+	}
 
 	@Override
 	public String getColumnText(final Object obj, final int index) {
@@ -47,7 +69,40 @@ public class TestViewLabelProvider extends LabelProvider implements
 
 	@Override
 	public Image getImage(final Object obj) {
-		return PlatformUI.getWorkbench().getSharedImages()
-				.getImage(ISharedImages.IMG_OBJ_ELEMENT);
+		if (obj instanceof Test) {
+			return test_ok;
+		} else if (obj instanceof TestFailure) {
+			if (((TestFailure) obj).isFailure()) {
+				return test_fail;
+			} else {
+				return test_err;
+			}
+		} else if (obj instanceof Class<?>) {
+			return calculateClassCoverate(obj);
+		}
+		return suite_err;
+	}
+
+	@SuppressWarnings("unchecked")
+	private Image calculateClassCoverate(final Object obj) {
+		Set<Object> set = map.get((Class<TestSuite>) obj);
+		boolean isFailure = false;
+		for (Object object : set) {
+			if (object instanceof TestFailure) {
+				if (!((TestFailure) object).isFailure()) {
+					return suite_err;
+				} else {
+					isFailure = true;
+				}
+			}
+		}
+		if (isFailure) {
+			return suite_fail;
+		}
+		return suite_ok;
+	}
+
+	public void update(final SetMultimap<Class<TestSuite>, Object> map) {
+		this.map = map;
 	}
 }
