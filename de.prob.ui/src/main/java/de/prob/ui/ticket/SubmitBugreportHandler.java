d91846adea6f370e1d4d34d4b0ec18d8cb88508c
Joy Clark <joyclark2004@msn.com>
2012-12-14 15:27:04 +0100

    PROBCORE-185 Copied all of the files from ProB 1.0 into ProB 2.0. Submitting bug report opens to Jira website.

---
+++ de.prob.ui/src/main/java/de/prob/ui/ticket/SubmitBugreportHandler.java
@@ -0,0 +1,39 @@
+package de.prob.ui.ticket;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.IHandler;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.browser.IWebBrowser;
+
+public class SubmitBugreportHandler extends AbstractHandler implements IHandler {
+
+	@Override
+	public Object execute(final ExecutionEvent event) throws ExecutionException {
+		// IWorkbenchWindow window = HandlerUtil
+		// .getActiveWorkbenchWindowChecked(event);
+		//
+		// BugReportWizard wizard = new BugReportWizard();
+		// WizardDialog dialog = new WizardDialog(window.getShell(), wizard);
+		// dialog.open();
+		// return null;
+
+		IWebBrowser browser;
+		try {
+			browser = PlatformUI.getWorkbench().getBrowserSupport()
+					.createBrowser("jira");
+			browser.openURL(new URL("http://jira.cobra.cs.uni-duesseldorf.de/"));
+		} catch (PartInitException e) {
+			e.printStackTrace();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+}
