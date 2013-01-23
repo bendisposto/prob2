16edc6c787cd13566f25530832684626aeaa4a56
Daniel Plagge <plagge@cs.uni-duesseldorf.de>
2013-01-23 12:18:36 +0100

    added new commands for the evalstore

---
+++ de.prob.core.kernel/src/main/java/de/prob/animator/command/EvalstoreCreateByStateCommand.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package de.prob.animator.command;
+
+import de.prob.parser.ISimplifiedROMap;
+import de.prob.prolog.output.IPrologTermOutput;
+import de.prob.prolog.term.IntegerPrologTerm;
+import de.prob.prolog.term.PrologTerm;
+
+/**
+ * Creates an evaluation store based on a state from the state space.
+ * 
+ * An evaluation store can be used to evaluate formulas on it and to 
+ * add new identifiers with new values to create a new evaluation store.
+ * 
+ * @author plagge
+ */
+public class EvalstoreCreateByStateCommand implements ICommand {
+
+	private static final String STORE_ID_VAR = "ID";
+	private final String stateId;
+	private long evalstoreId;
+	
+	public EvalstoreCreateByStateCommand(String stateId) {
+		this.stateId = stateId;
+	}
+
+	@Override
+	public void writeCommand(IPrologTermOutput pto) {
+		pto.openTerm("es_copy_from_statespace");
+		pto.printAtomOrNumber(stateId);
+		pto.printVariable(STORE_ID_VAR);
+		pto.closeTerm();
+	}
+
+	@Override
+	public void processResult(ISimplifiedROMap<String, PrologTerm> bindings) {
+		// TODO[DP, 22.01.2013]: Handle errors?
+		final IntegerPrologTerm result = (IntegerPrologTerm) bindings.get(STORE_ID_VAR);
+		evalstoreId = result.getValue().longValue();
+	}
+	
+	public long getEvalstoreId() {
+		return evalstoreId;
+	}
+
+}
