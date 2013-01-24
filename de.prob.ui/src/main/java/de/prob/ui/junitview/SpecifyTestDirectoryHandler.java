b56ce0b76b04550b41443b659a20eaae6cebde5e
Joy Clark <joyclark2004@msn.com>
2013-01-24 13:20:01 +0100

    PROBCORE-176 created dialog to specify the directory in which the test scripts should be found

--- de.prob.ui/src/main/java/de/prob/ui/junitview/SpecifyTestDirectoryHandler.java
+++ de.prob.ui/src/main/java/de/prob/ui/junitview/SpecifyTestDirectoryHandler.java
@@ -4,35 +4,25 @@ import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.IHandler;
-import org.eclipse.jface.dialogs.IInputValidator;
-import org.eclipse.jface.dialogs.InputDialog;
+import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.handlers.HandlerUtil;
 
 import de.prob.testing.TestRegistry;
 import de.prob.webconsole.ServletContextListener;
 
-public class SpecifyTestDirectoryHandler extends AbstractHandler implements IHandler {
+public class SpecifyTestDirectoryHandler extends AbstractHandler implements
+		IHandler {
 
 	@Override
 	public Object execute(final ExecutionEvent event) throws ExecutionException {
 		Shell shell = HandlerUtil.getActiveShell(event);
 
-		InputDialog inputDialog = new InputDialog(
-				shell,
-				"Open Test Directory",
-				"Please specify the directory where you have saved your groovy tests:",
-				null, new IInputValidator() {
-					String errormsg = "That is not a valid directory";
+		FileDialog dialog = new FileDialog(shell);
+		dialog.open();
+		String answer = dialog.getFilterPath();
 
-					@Override
-					public String isValid(final String newText) {
-						return null;
-					}
-				});
-		inputDialog.open();
-		String answer = inputDialog.getValue();
-		if (answer != null) {
+		if (answer != null && answer != "") {
 			ServletContextListener.INJECTOR.getInstance(TestRegistry.class)
 					.loadTests(answer);
 			return null;
