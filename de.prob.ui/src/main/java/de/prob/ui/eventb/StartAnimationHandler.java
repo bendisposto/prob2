58d48f390838c493c525a24f95d319c3c2657fb0
Lukas Ladenberger <lukas.ladenberger@googlemail.com>
2013-02-22 16:29:53 +0100

    reverted start animation handler

--- de.prob.ui/src/main/java/de/prob/ui/eventb/StartAnimationHandler.java
+++ de.prob.ui/src/main/java/de/prob/ui/eventb/StartAnimationHandler.java
@@ -1,22 +1,40 @@
 package de.prob.ui.eventb;
 
+import groovy.lang.Binding;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.handlers.HandlerUtil;
 import org.eventb.core.IEventBRoot;
 import org.rodinp.core.IRodinFile;
 import org.rodinp.core.RodinCore;
 
+import com.google.inject.Injector;
+
+import de.prob.animator.command.LoadEventBCommand;
+import de.prob.animator.command.StartAnimationCommand;
+import de.prob.model.eventb.EventBModel;
+import de.prob.rodin.translate.EventBTranslator;
+import de.prob.scripting.EventBFactory;
+import de.prob.statespace.AnimationSelector;
+import de.prob.statespace.History;
+import de.prob.statespace.StateSpace;
+import de.prob.ui.eventb.internal.TranslatorFactory;
+import de.prob.webconsole.GroovyExecution;
+import de.prob.webconsole.ServletContextListener;
+
 public class StartAnimationHandler extends AbstractHandler {
 
 	private ISelection fSelection;
-
-	private StartAnimationProgressBar animationProgressBar;
 
 	@Override
 	public Object execute(final ExecutionEvent event) throws ExecutionException {
@@ -25,15 +43,52 @@ public class StartAnimationHandler extends AbstractHandler {
 
 		final IEventBRoot rootElement = getRootElement();
 
-		if (animationProgressBar != null)
-			animationProgressBar.kill();
-		animationProgressBar = new StartAnimationProgressBar(Display
-				.getDefault().getActiveShell(), rootElement);
-		animationProgressBar.initGuage();
-		animationProgressBar.open();
+		EventBTranslator eventBTranslator = new EventBTranslator(rootElement);
+
+		Injector injector = ServletContextListener.INJECTOR;
+
+		final EventBFactory instance = injector
+				.getInstance(EventBFactory.class);
+
+		EventBModel model = instance.load(eventBTranslator.getMainComponent(),
+				eventBTranslator.getMachines(), eventBTranslator.getContexts());
+
+		StringWriter writer = new StringWriter();
+		PrintWriter pto = new PrintWriter(writer);
+		try {
+			TranslatorFactory.translate(rootElement, pto);
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		}
+
+		StateSpace s = model.getStatespace();
+
+		Pattern p2 = Pattern.compile("^package\\((.*?)\\)\\.");
+		Matcher m2 = p2.matcher(writer.toString());
+		m2.find();
+		String cmd = m2.group(1);
+
+		s.execute(new LoadEventBCommand(cmd));
+		s.execute(new StartAnimationCommand());
+
+		History h = new History(s);
+		AnimationSelector selector = injector
+				.getInstance(AnimationSelector.class);
+		selector.addNewHistory(h);
+		final GroovyExecution ge = injector.getInstance(GroovyExecution.class);
+		Binding bindings = ge.getBindings();
+		try {
+			bindings.setVariable(ge.freshVar("space_"), s);
+			s.registerStateSpaceListener(ge);
+		} catch (Error t) {
+			t.printStackTrace();
+		} finally {
+			ge.notifyListerners();
+		}
+
+		System.gc();
 
 		return null;
-
 	}
 
 	private IEventBRoot getRootElement() {
