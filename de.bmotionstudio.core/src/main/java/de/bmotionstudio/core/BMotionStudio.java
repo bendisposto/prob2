f8b61818432d0f430054f18dcf88bfb1d7dba3a9
Lukas Ladenberger <lukas.ladenberger@googlemail.com>
2013-02-26 13:05:44 +0100

    perspective support refactoring

--- de.bmotionstudio.core/src/main/java/de/bmotionstudio/core/BMotionStudio.java
+++ de.bmotionstudio.core/src/main/java/de/bmotionstudio/core/BMotionStudio.java
@@ -1,19 +1,43 @@
 package de.bmotionstudio.core;
 
+import java.io.File;
+
+import org.eclipse.ui.IPerspectiveDescriptor;
+
 public class BMotionStudio {
 
 	private static String imagePath;
-	
+
+	private static IPerspectiveDescriptor currentPerspective;
+
+	private static File currentModelFile;
+
 	public static void setImagePath(String path) {
 		imagePath = path;
 	}
-	
+
 	public static String getImagePath() {
 		return imagePath;
 	}
 
 	public static void reset() {
 		BMotionStudio.setImagePath(null);
+	}
+
+	public static IPerspectiveDescriptor getCurrentPerspective() {
+		return currentPerspective;
+	}
+
+	public static void setCurrentPerspective(IPerspectiveDescriptor perspective) {
+		currentPerspective = perspective;
+	}
+
+	public static File getCurrentModelFile() {
+		return currentModelFile;
+	}
+
+	public static void setCurrentModelFile(File modelFile) {
+		currentModelFile = modelFile;
 	}
 	
 }
--- de.bmotionstudio.core/src/main/java/de/bmotionstudio/core/editor/ProBStartup.java
+++ de.bmotionstudio.core/src/main/java/de/bmotionstudio/core/editor/ProBStartup.java
@@ -1,12 +1,14 @@
-package de.prob.ui;
+package de.bmotionstudio.core.editor;
 
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.IPerspectiveDescriptor;
 import org.eclipse.ui.IPerspectiveRegistry;
 import org.eclipse.ui.IStartup;
+import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.PlatformUI;
 
-import de.prob.ui.util.PerspectiveUtil;
+import de.bmotionstudio.core.util.PerspectiveUtil;
+import de.prob.ui.PerspectiveFactory;
 
 public class ProBStartup implements IStartup {
 
@@ -15,16 +17,27 @@ public class ProBStartup implements IStartup {
 		Display.getDefault().asyncExec(new Runnable() {
 			@Override
 			public void run() {
-				IPerspectiveRegistry perspectiveRegistry = PlatformUI
-						.getWorkbench().getPerspectiveRegistry();
+				boolean deleted = false;
+				IWorkbench workbench = PlatformUI.getWorkbench();
+				IPerspectiveDescriptor currentPerspective = workbench
+						.getActiveWorkbenchWindow().getActivePage()
+						.getPerspective();
+				IPerspectiveRegistry perspectiveRegistry = workbench
+						.getPerspectiveRegistry();
 				IPerspectiveDescriptor[] perspectives = perspectiveRegistry
 						.getPerspectives();
 				for (IPerspectiveDescriptor p : perspectives) {
 					if (p.getId().replace("<", "").startsWith("ProB_")) {
 						PerspectiveUtil.closePerspective(p);
 						PerspectiveUtil.deletePerspective(p);
+						deleted = true;
 					}
 				}
+				if (deleted
+						&& currentPerspective.getId().replace("<", "")
+								.startsWith("ProB_"))
+					PerspectiveUtil
+							.switchPerspective(PerspectiveFactory.PROB_PERSPECTIVE);
 			}
 		});
 	}
