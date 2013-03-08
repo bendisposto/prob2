c66e8f513681cbcc8e35ea2064148a06cbbab39d
Joy Clark <joyclark2004@msn.com>
2013-03-08 15:02:59 +0100

    PROBCORE-301 fix failing test

--- de.prob.core.kernel/src/test/java/de/prob/animator/domainobjects/EvaluationResultTest.java
+++ de.prob.core.kernel/src/test/java/de/prob/animator/domainobjects/EvaluationResultTest.java
@@ -10,10 +10,10 @@ public class EvaluationResultTest {
 	public void testToString() {
 		EvaluationResult a = new EvaluationResult("0", "c1", "yay", "", "",
 				"exists", new String[0], false);
-		assertEquals("0: yay", a.toString());
+		assertEquals("yay", a.toString());
 		EvaluationResult b = new EvaluationResult("1", "c2", "the", "is 4", "",
 				"exists", new String[0], false);
-		assertEquals("1: the Solution: is 4", b.toString());
+		assertEquals("the Solution: is 4", b.toString());
 		EvaluationResult c = new EvaluationResult("3", "c3", "", "",
 				"does not work", "exists", new String[0], false);
 		assertEquals("'Errors: does not work'", c.toString());
