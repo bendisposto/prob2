08ddb998bf2bffa11c6d0882e177520cc247e479
Joy Clark <joyclark2004@msn.com>
2013-04-05 12:07:47 +0200

    PROBCORE-307 Working on predicate visualization

---
+++ de.prob.core.kernel/src/main/resources/javascripts/predicate.js
@@ -0,0 +1,144 @@
+function doIt(dataset) {
+    buildTree(dataset)
+}
+
+function visit(parent, visitFn, childrenFn)
+{
+    if (!parent) return;
+
+    visitFn(parent);
+
+    var children = childrenFn(parent);
+    if (children) {
+        var count = children.length;
+        for (var i = 0; i < count; i++) {
+            visit(children[i], visitFn, childrenFn);
+        }
+    }
+}
+
+var m = [20, 120, 20, 120],
+    w = 1280 - m[1] - m[3],
+    h = 800 - m[0] - m[2],
+    i = 0,
+    root;
+
+var tree = d3.layout.tree()
+    .size([h, w]);
+
+var diagonal = d3.svg.diagonal()
+    .projection(function(d) { return [d.y, d.x]; });
+
+var vis = d3.select("#body").append("svg:svg")
+    .attr("width", w + m[1] + m[3])
+    .attr("height", h + m[0] + m[2])
+  .append("svg:g")
+    .attr("transform", "translate(" + m[3] + "," + m[0] + ")");  
+
+function buildTree(treeData)
+{
+    root = treeData
+    root.x0 = h / 2;
+    root.y0 = 0;
+ 
+    //STOPPED HERE
+
+    // Calculate total nodes, max label length
+    var totalNodes = 0;
+    var maxLabelLength = 0;
+    visit(treeData, function(d)
+    {
+        totalNodes++;
+        maxLabelLength = Math.max(d.name.length, maxLabelLength);
+    }, function(d)
+    {
+        return d.contents && d.contents.length > 0 ? d.contents : null;
+    });
+
+    // size of the diagram
+    var size = { width:$(containerName).outerWidth(), height: totalNodes * 15};
+
+    var tree = d3.layout.tree()
+        .sort(null)
+        .size([size.height, size.width - maxLabelLength*options.fontSize])
+        .children(function(d)
+        {
+            return (!d.contents || d.contents.length === 0) ? null : d.contents;
+        });
+
+    var nodes = tree.nodes(treeData);
+    var links = tree.links(nodes);
+
+    
+    /*
+        <svg>
+            <g class="container" />
+        </svg>
+     */
+    var layoutRoot = d3.select("svg")
+        .append("svg:svg").attr("width", size.width).attr("height", size.height)
+        .append("svg:g")
+        .attr("class", "container")
+        .attr("transform", "translate(" + maxLabelLength + ",0)");
+
+
+    // Edges between nodes as a <path class="link" />
+    var link = d3.svg.diagonal()
+        .projection(function(d)
+        {
+            return [d.y, d.x];
+        });
+
+    layoutRoot.selectAll("path.link")
+        .data(links)
+        .enter()
+        .append("svg:path")
+        .attr("class", "link")
+        .attr("d", link);
+
+
+    /*
+        Nodes as
+        <g class="node">
+            <circle class="node-dot" />
+            <text />
+        </g>
+     */
+    var nodeGroup = layoutRoot.selectAll("g.node")
+        .data(nodes)
+        .enter()
+        .append("svg:g")
+        .attr("class", "node")
+        .attr("transform", function(d)
+        {
+            return "translate(" + d.y + "," + d.x + ")";
+        });
+
+    nodeGroup.append("svg:circle")
+        .attr("class", "node-dot")
+        .attr("r", options.nodeRadius);
+
+    nodeGroup.append("svg:text")
+        .attr("text-anchor", function(d)
+        {
+            return d.children ? "end" : "start";
+        })
+        .attr("dx", function(d)
+        {
+            var gap = 2 * options.nodeRadius;
+            return d.children ? -gap : gap;
+        })
+        .attr("dy", 3)
+        .text(function(d)
+        {
+            return d.name;
+        });
+
+}
+
+function displayFormula() {
+    var line = $("#formula")[0].value;
+	$.getJSON("formula", {
+		formula : line
+	}, doIt);
+}
