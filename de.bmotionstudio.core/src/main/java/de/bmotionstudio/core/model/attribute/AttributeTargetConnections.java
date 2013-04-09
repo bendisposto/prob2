6bf50905d03d22504cdef4b713abb0169e45672e
Lukas Ladenberger <lukas.ladenberger@googlemail.com>
2013-04-09 10:46:55 +0200

    better handling of connections (via properties) PROBCORE-319

---
+++ de.bmotionstudio.core/src/main/java/de/bmotionstudio/core/model/attribute/AttributeTargetConnections.java
@@ -0,0 +1,27 @@
+/** 
+ * (c) 2009 Lehrstuhl fuer Softwaretechnik und Programmiersprachen, 
+ * Heinrich Heine Universitaet Duesseldorf
+ * This software is licenced under EPL 1.0 (http://www.eclipse.org/org/documents/epl-v10.html) 
+ * */
+
+package de.bmotionstudio.core.model.attribute;
+
+import org.eclipse.ui.views.properties.PropertyDescriptor;
+
+public class AttributeTargetConnections extends AbstractAttribute {
+
+	public AttributeTargetConnections(Object value) {
+		super(value);
+	}
+
+	@Override
+	public PropertyDescriptor preparePropertyDescriptor() {
+		return null;
+	}
+
+	@Override
+	public String getName() {
+		return "Target Connections";
+	}
+
+}
