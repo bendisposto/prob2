319eb926d3c4fb139edb88e8af1be81a3467b78c
Sebastian Krings <sebastian@krin.gs>
2013-06-26 15:47:00 +0200

    add files missing from previous commit

---
+++ de.prob.core.kernel/src/main/java/de/prob/animator/command/ModelCheckingCommand.java
@@ -0,0 +1,79 @@
+/**
+ * (c) 2009 Lehrstuhl fuer Softwaretechnik und Programmiersprachen, Heinrich
+ * Heine Universitaet Duesseldorf This software is licenced under EPL 1.0
+ * (http://www.eclipse.org/org/documents/epl-v10.html)
+ * */
+
+package de.prob.animator.command;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import de.prob.check.ModelCheckingResult;
+import de.prob.parser.BindingGenerator;
+import de.prob.parser.ISimplifiedROMap;
+import de.prob.prolog.output.IPrologTermOutput;
+import de.prob.prolog.term.CompoundPrologTerm;
+import de.prob.prolog.term.ListPrologTerm;
+import de.prob.prolog.term.PrologTerm;
+import de.prob.statespace.OpInfo;
+
+public class ModelCheckingCommand extends AbstractCommand {
+	private final int time;
+	private final List<String> options;
+	private ModelCheckingResult result;
+	private final String RESULT = "Result";
+	private final String OPS = "Ops";
+
+	Logger logger = LoggerFactory.getLogger(ModelCheckingCommand.class);
+	private final long last;
+	private final List<OpInfo> newOps = new ArrayList<OpInfo>();
+
+	public ModelCheckingCommand(final int time,
+			final List<String> options, final long last) {
+		this.time = time;
+		this.options = options;
+		this.last = last;
+	}
+
+	public ModelCheckingResult getResult() {
+		return result;
+	}
+
+	@Override
+	public void processResult(
+			final ISimplifiedROMap<String, PrologTerm> bindings) {
+		PrologTerm pt = bindings.get(RESULT);
+		CompoundPrologTerm term = BindingGenerator.getCompoundTerm(pt,
+				pt.getArity());
+		ListPrologTerm list = BindingGenerator.getList(bindings.get(OPS));
+		result = new ModelCheckingResult(term);
+
+		for (PrologTerm prologTerm : list) {
+			CompoundPrologTerm op = BindingGenerator.getCompoundTerm(
+					prologTerm, 3);
+			String id = OpInfo.getIdFromPrologTerm(op.getArgument(1));
+			String src = OpInfo.getIdFromPrologTerm(op.getArgument(2));
+			String dest = OpInfo.getIdFromPrologTerm(op.getArgument(3));
+			newOps.add(new OpInfo(id, src, dest));
+		}
+
+	}
+
+	@Override
+	public void writeCommand(final IPrologTermOutput pto) {
+		pto.openTerm("prob2_modelcheck").printNumber(time).openList();
+		for (String o : options) {
+			pto.printAtom(o);
+		}
+		pto.closeList().printNumber(last).printVariable(OPS)
+				.printVariable(RESULT).closeTerm();
+	}
+
+	public List<OpInfo> getNewOps() {
+		return newOps;
+	}
+}
---
+++ de.prob.core.kernel/src/main/java/de/prob/check/ModelCheckingSearchOption.java
@@ -0,0 +1,72 @@
+/** 
+ * (c) 2009 Lehrstuhl fuer Softwaretechnik und Programmiersprachen, 
+ * Heinrich Heine Universitaet Duesseldorf
+ * This software is licenced under EPL 1.0 (http://www.eclipse.org/org/documents/epl-v10.html) 
+ * */
+
+/**
+ * 
+ */
+package de.prob.check;
+
+import java.util.ArrayList;
+import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public enum ModelCheckingSearchOption {
+	breadth_first_search(0, "Breadth First Search", false), find_deadlocks(1,
+			"Find Deadlocks", true), find_invariant_violations(2,
+			"Find Invariant Violations", true), find_assertion_violations(3,
+			"Find Theorem Violations", false), inspect_existing_nodes(4,
+			"Recheck Existing States", false),
+			stop_at_full_coverage(5, "Stop when all Events are Covered", false);
+
+	private final String text;
+	private final int pos;
+	private final boolean enabledByDefault;
+
+	private ModelCheckingSearchOption(final int pos, final String text,
+			final boolean enabledByDefault) {
+		this.pos = pos;
+		this.text = text;
+		this.enabledByDefault = enabledByDefault;
+	}
+
+	public final String getDescription() {
+		return text;
+	}
+
+	private static final Map<Integer, ModelCheckingSearchOption> lookup = new HashMap<Integer, ModelCheckingSearchOption>();
+
+	static {
+		for (ModelCheckingSearchOption s : EnumSet
+				.allOf(ModelCheckingSearchOption.class)) {
+			lookup.put(s.getPos(), s);
+		}
+	}
+
+	public final int getPos() {
+		return pos;
+	}
+
+	public final boolean isEnabledByDefault() {
+		return enabledByDefault;
+	}
+
+	public final static List<String> getDefaultOptions() {
+		List<String> options = new ArrayList<String>();
+		for (ModelCheckingSearchOption option : lookup.values()) {
+			if (option.isEnabledByDefault()) {
+				options.add(option.name());
+			}
+		}
+		return options;
+	}
+
+	public final static ModelCheckingSearchOption get(final int code) {
+		return lookup.get(code);
+	}
+
+}
