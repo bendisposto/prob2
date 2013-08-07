51a813e94b08505abbfd551bdce75e7c2cb6ebb2
Jens Bendisposto <jens@bendisposto.de>
2013-08-07 10:01:01 +0200

    refactoring to improve testability

--- de.prob.core.kernel/src/main/resources/ui/valueOverTime/ValueOverTime.js
+++ de.prob.core.kernel/src/main/resources/ui/valueOverTime/ValueOverTime.js
@@ -1,44 +1,51 @@
 ValueOverTime = (function() {
-    var extern = {}
-    var session = Session();
-    var svg = VizUtils.createCanvas("#visualization", $("#left-col")[0].clientWidth, VizUtils.calculateHeight());
+	var extern = {}
+	var session = Session();
+	var svg = VizUtils.createCanvas("#visualization",
+			$("#left-col")[0].clientWidth, VizUtils.calculateHeight());
 
-    $(document).ready(function() {
-    });
+	$(document).ready(function() {
+	});
 
-    $(".form-control").keyup(function(e) {
-        session.sendCmd("parse", {
-            "formula" : e.target.value,
-            "id" : e.target.id
-        })
-    });
+	$(".form-control").keyup(function(e) {
+		session.sendCmd("parse", {
+			"formula" : e.target.value,
+			"id" : e.target.id
+		})
+	});
 
-    $(".add_expr").click(function(e) {
-        e.preventDefault();
-        session.sendCmd("addFormula", {
-            "time" : $("#inputTime").val(),
-            "formula": $("#inputFormula1").val(),
-            "client" : extern.client
-        })
-    })
+	$(".add_expr").click(function(e) {
+		e.preventDefault();
+		session.sendCmd("addFormula", {
+			"time" : $("#inputTime").val(),
+			"formula" : $("#inputFormula1").val(),
+			"client" : extern.client
+		})
+	})
 
-    function draw(data) {
-        //TO IMPLEMENT
-    }
+	function draw(whatever) {
+		// TO IMPLEMENT
+	}
 
-    function parseOk(data) {
-        $("#"+data.id).parent().removeClass("has-error")
-    }
+	function parseOk(id) {
+		$("#" + id).parent().removeClass("has-error")
+	}
 
-    function parseError(data) {
-        $("#"+data.id).parent().addClass("has-error")
-    }    
-    
-    extern.draw = draw
-    extern.client = ""
-    extern.init = session.init
-    extern.parseError = parseError
-    extern.parseOk = parseOk
+	function parseError(id) {
+		$("#" + id).parent().addClass("has-error")
+	}
 
-    return extern;
+	extern.draw = function(data) {
+		draw(data.whatever);
+	}
+	extern.client = ""
+	extern.init = session.init
+	extern.parseError = function(data) {
+		parseError(data.id);
+	}
+	extern.parseOk = function(data) {
+		parseOk(data.id);
+	}
+
+	return extern;
 }())
