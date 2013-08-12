c7b98801663afc94ff666240a20a0c6984934a8d
Jens Bendisposto <jens@bendisposto.de>
2013-08-12 10:40:48 +0200

    Added convenience method for Objects that can directly be translated to Json

--- de.prob.core.kernel/src/main/java/de/prob/web/WebUtils.java
+++ de.prob.core.kernel/src/main/java/de/prob/web/WebUtils.java
@@ -27,6 +27,17 @@ public class WebUtils {
 		return sw.toString();
 	}
 
+	public static Map<String, String> wrap(Object... content) {
+		if (content.length % 2 != 0)
+			throw new IllegalArgumentException(
+					"wrap requires an even number of arguments");
+		HashMap<String, String> m = new HashMap<String, String>();
+		for (int i = 0; i < content.length; i = i + 2) {
+			m.put(content[i].toString(), content[i + 1].toString());
+		}
+		return m;
+	}
+
 	public static Map<String, String> wrap(String... content) {
 		if (content.length % 2 != 0)
 			throw new IllegalArgumentException(
