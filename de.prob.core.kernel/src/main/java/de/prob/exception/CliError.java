d8f16d4004d1dc6400870cc3a3848fc2e46a934a
Philipp Koerner <p.koerner@uni-duesseldorf.de>
2013-08-20 13:45:52 +0200

    added constructor

--- de.prob.core.kernel/src/main/java/de/prob/exception/CliError.java
+++ de.prob.core.kernel/src/main/java/de/prob/exception/CliError.java
@@ -2,6 +2,8 @@ package de.prob.exception;
 
 import java.io.IOException;
 
+import de.prob.prolog.term.PrologTerm;
+
 @SuppressWarnings("serial")
 public class CliError extends RuntimeException {
 
@@ -11,6 +13,11 @@ public class CliError extends RuntimeException {
 
 	public CliError(String message, IOException e) {
 		super(message, e);
+	}
+	
+	public CliError(PrologTerm pt) {
+		super(pt.toString());
+		
 	}
 
 }
