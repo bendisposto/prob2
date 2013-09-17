234497ca873137bf3912601da6a78e79a6b8962d
Joy Clark <joyclark2004@msn.com>
2013-09-17 18:12:10 +0200

    PROBCORE-425 Updated CurrentTraceView so that it uses the correct port

--- de.prob.ui/src/main/java/de/prob/ui/historyview/CurrentTraceView.java
+++ de.prob.ui/src/main/java/de/prob/ui/historyview/CurrentTraceView.java
@@ -5,6 +5,8 @@ import org.eclipse.swt.browser.Browser;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.part.ViewPart;
 
+import de.prob.webconsole.WebConsole;
+
 /**
  * This sample class demonstrates how to plug-in a new workbench view. The view
  * shows data obtained from the model. The sample creates a dummy model on the
@@ -26,7 +28,12 @@ public class CurrentTraceView extends ViewPart {
 	 * The ID of the view as specified by the extension.
 	 */
 	public static final String ID = "de.prob.ui.historyview.CurrentTraceView";
+	private final int port;
 	private Browser browser;
+
+	public CurrentTraceView() {
+		port = WebConsole.getPort();
+	}
 
 	/**
 	 * This is a callback that will allow us to create the viewer and initialize
@@ -35,7 +42,7 @@ public class CurrentTraceView extends ViewPart {
 	@Override
 	public void createPartControl(final Composite parent) {
 		browser = new Browser(parent, SWT.NONE);
-		browser.setUrl("http://localhost:8080/sessions/CurrentTrace");
+		browser.setUrl("http://localhost:" + port + "/sessions/CurrentTrace");
 	}
 
 	/**
