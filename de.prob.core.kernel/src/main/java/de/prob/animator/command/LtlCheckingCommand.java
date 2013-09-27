2a38970d4cd6be919aba2f4621681c586827d6db
Philipp Koerner <p.koerner@uni-duesseldorf.de>
2013-09-27 13:38:52 +0200

    implemented ltl commands and ltl result, they implement the new interfaces

--- de.prob.core.kernel/src/main/java/de/prob/animator/command/LtlCheckingCommand.java
+++ de.prob.core.kernel/src/main/java/de/prob/animator/command/LtlCheckingCommand.java
@@ -6,7 +6,11 @@
 
 package de.prob.animator.command;
 
+import java.util.List;
+
 import de.prob.animator.IAnimator;
+import de.prob.animator.domainobjects.IEvalElement;
+import de.prob.animator.domainobjects.LtlCheckingResult;
 import de.prob.parser.ISimplifiedROMap;
 import de.prob.prolog.output.IPrologTermOutput;
 import de.prob.prolog.term.CompoundPrologTerm;
@@ -14,18 +18,24 @@ import de.prob.prolog.term.IntegerPrologTerm;
 import de.prob.prolog.term.ListPrologTerm;
 import de.prob.prolog.term.PrologTerm;
 import de.prob.statespace.OpInfo;
+import de.prob.statespace.StateId;
 
 /**
  * @ Andriy: Das ist jetzt deine Baustelle :)
  */
-public final class LtlCheckingCommand extends AbstractCommand {
+public final class LtlCheckingCommand extends EvaluationCommand {
 
 	private static final String VARIABLE_NAME_ATOMICS = "A";
 	private static final String VARIABLE_NAME_STRUCTURE = "S";
 	private static final String VARIABLE_NAME_RESULT = "R";
 
 	public static enum StartMode {
-		init, starthere, checkhere
+		init, // checks formula in initialisation state(s)
+		starthere, // checks formula in current state
+		checkhere /*
+				 * start in initialisation state(s) and check the formula in
+				 * current state
+				 */
 	};
 
 	public static enum Status {
@@ -46,143 +56,31 @@ public final class LtlCheckingCommand extends AbstractCommand {
 		INFINITE, FINITE, REDUCED
 	};
 
-	public static class Result {
-		private final Status status;
-		private final ListPrologTerm atomics;
-		private final PrologTerm structure;
-		private final ListPrologTerm counterexample;
-		private final PathType pathType;
-		private final int loopEntry;
-		private final OpInfo[] initPathOps;
-
-		public Result(final Status status, final ListPrologTerm atomics,
-				final PrologTerm structure,
-				final ListPrologTerm counterexample, final PathType pathType,
-				final int loopEntry, final OpInfo[] initPathOps) {
-			this.status = status;
-			this.atomics = atomics;
-			this.structure = structure;
-			this.counterexample = counterexample;
-			this.pathType = pathType;
-			this.loopEntry = loopEntry;
-			this.initPathOps = initPathOps;
-		}
-
-		/**
-		 * @return the basic outcome of the model-checking, never
-		 *         <code>null</code>
-		 */
-		public Status getStatus() {
-			return status;
-		}
-
-		/**
-		 * @return if the model-checking is finished after the call.
-		 */
-		public boolean isAbort() {
-			return status.isAbort();
-		}
-
-		/**
-		 * Returns list of pretty-printed sub-formulas, each of the form
-		 * ap(Text) or tp(Text).
-		 * 
-		 * E.g. for G({x=0} U [start]), the list contains ap('{x=0}') and
-		 * tp('[start]').
-		 * 
-		 * @return A list of atomic formulas, never <code>null</code>
-		 */
-		public ListPrologTerm getAtomics() {
-			return atomics;
-		}
-
-		/**
-		 * A tree structure representing the formula. Atomic propositions or
-		 * transition propositions are represented by integers, referring to the
-		 * index of the corresponding formula in {@link #atomics}
-		 * 
-		 * Lets assume that {@link #getAtomics()} returns the list
-		 * [ap('{x=0}'),ap('{y=1}'),tp('[start]')] for the LTL formula
-		 * 
-		 * G({y=1} => ({x=0} U [start])).
-		 * 
-		 * The corresponding structure would be: globally(implies(1,until(0,2)))
-		 * 
-		 * @return the structure, <code>null</code> in case of a typecheck-error
-		 */
-		public PrologTerm getStructure() {
-			return structure;
-		}
-
-		/**
-		 * Returns the list of atoms of the counter-example. Each atom is a term
-		 * of the form
-		 * atom(StateId,EvalList,NextOperationId,NextOperationString) where
-		 * StateId is the ID of the corresponding state and NextOperationId is
-		 * the ID of the operation that leads to the next atom.
-		 * NextOperationString is a pretty-printed string of that operation.
-		 * EvalList is a list of 0s and 1s, it has the same length of the list
-		 * returned by {@link #getAtomics()}. Each number represents the
-		 * evaluation of the corresponding atomic formula (in
-		 * {@link #getAtomics()}) in the current atom. 0=false and 1=true.
-		 * 
-		 * @return A list of atoms, <code>null</code> if no counter-example is
-		 *         found.
-		 */
-		public ListPrologTerm getCounterexample() {
-			return counterexample;
-		}
-
-		/**
-		 * @return the path type, <code>null</code> if there is no
-		 *         counter-example
-		 */
-		public PathType getPathType() {
-			return pathType;
-		}
-
-		/**
-		 * This value is only defined if {@link #getPathType()} returns LOOP.
-		 * 
-		 * @return the index in the counter-example where the loop of the
-		 *         lasso-form starts, -1 if there is no counterexample or the
-		 *         example is not in lasso form.
-		 */
-		public int getLoopEntry() {
-			return loopEntry;
-		}
-
-		public OpInfo[] getInitPathOps() {
-			return initPathOps;
-		}
-	}
-
-	private final PrologTerm ltlFormula;
 	private final int max;
 	private final StartMode mode;
-	private Result result;
+	private LtlCheckingResult result;
+	private final StateId stateid;
 
-	public LtlCheckingCommand() {
-		this.ltlFormula = parseLTLFormula("ignored");
-		this.max = 10;
-		this.mode = StartMode.init;
-	}
-
-	public LtlCheckingCommand(final PrologTerm ltlFormula, final int max,
-			final StartMode mode) {
-		this.ltlFormula = ltlFormula;
+	public LtlCheckingCommand(final List<IEvalElement> ltlFormula,
+			final int max,
+			final StartMode mode, final StateId stateid) {
+		super(ltlFormula, stateid.getId());
 		this.max = max;
 		this.mode = mode;
+		this.stateid = stateid;
 	}
 
-	public static Result modelCheck(final IAnimator a, final PrologTerm fomula,
-			final int max, final StartMode mode) {
-		LtlCheckingCommand command = new LtlCheckingCommand(fomula, max, mode);
+	public static LtlCheckingResult modelCheck(final IAnimator a,
+			final List<IEvalElement> fomula,
+ final int max, final StartMode mode,
+			final StateId stateid) {
+		LtlCheckingCommand command = new LtlCheckingCommand(fomula, max, mode,
+				stateid);
 		a.execute(command);
 		return command.getResult();
 	}
 
-	public Result getResult() {
+	public LtlCheckingResult getResult() {
 		return result;
 	}
 
@@ -243,14 +141,21 @@ public final class LtlCheckingCommand extends AbstractCommand {
 		final boolean noStructure = (structure instanceof ListPrologTerm)
 				&& ((ListPrologTerm) structure).isEmpty();
 
-		result = new Result(status, atomics, noStructure ? null : structure,
-				counterexample, pathType, loopEntry, initPath);
-}
+		result = new LtlCheckingResult(status, atomics, noStructure ? null : structure,
+ counterexample, pathType, loopEntry, initPath,
+				stateid);
+		values.add(result);
+	}
 
 	@Override
 	public void writeCommand(final IPrologTermOutput pto) {
+		// set the state first
+		// TODO: refactor prolog code, we want to use the stateID as a parameter
+		pto.openTerm("setCurrentState").printAtomOrNumber(stateid.getId())
+				.closeTerm();
+		// then call the ltl modelcheck predicate
 		pto.openTerm("do_ltl_modelcheck");
-		ltlFormula.toTermOutput(pto);
+		evalElements.get(0).printProlog(pto);
 		pto.printNumber(max);
 		pto.printAtom(mode.toString());
 		pto.printVariable(VARIABLE_NAME_ATOMICS);
@@ -259,26 +164,4 @@ public final class LtlCheckingCommand extends AbstractCommand {
 		pto.closeTerm();
 	}
 
-	private PrologTerm parseLTLFormula(String ignored) {
-		CompoundPrologTerm cpt = new CompoundPrologTerm("globally",
-				new CompoundPrologTerm("finally", new CompoundPrologTerm(
-						"action",
-				new CompoundPrologTerm("btrans", new CompoundPrologTerm(
-"event",
-										new CompoundPrologTerm("new"))))));
-
-		// final Animator animator = Animator.getAnimator();
-		// PrologTerm parsedFormula = null;
-		// final LtlParser parser = new LtlParser(
-		// animator.getLanguageDependendPart());
-		//
-		// try {
-		// parsedFormula = parser.generatePrologTerm(formula, "root");
-		// } catch (LtlParseException e) { // see Task#163
-		// MessageDialog.openError(getShell(), "Syntax error",
-		// e.getLocalizedMessage());
-		// }
-
-		return cpt;
-	}
 }
--- de.prob.core.kernel/src/main/java/de/prob/web/views/LtlFormula.java
+++ de.prob.core.kernel/src/main/java/de/prob/web/views/LtlFormula.java
@@ -11,15 +11,11 @@ import org.slf4j.LoggerFactory;
 
 import com.google.inject.Inject;
 
-import de.be4.classicalb.core.parser.ClassicalBParser;
 import de.be4.ltl.core.parser.LtlParseException;
-import de.be4.ltl.core.parser.LtlParser;
-import de.prob.animator.command.LtlCheckingCommand;
-import de.prob.animator.command.LtlCheckingCommand.StartMode;
-import de.prob.animator.command.LtlCheckingCommand.Status;
-import de.prob.prolog.term.CompoundPrologTerm;
-import de.prob.prolog.term.PrologTerm;
+import de.prob.animator.command.EvaluationCommand;
+import de.prob.animator.domainobjects.LTL;
 import de.prob.statespace.AnimationSelector;
+import de.prob.statespace.StateId;
 import de.prob.web.AbstractSession;
 import de.prob.web.WebUtils;
 
@@ -27,44 +23,36 @@ public class LtlFormula extends AbstractSession {
 
 	private final Logger logger = LoggerFactory.getLogger(CurrentTrace.class);
 
-	private final List<PrologTerm> formulas = new ArrayList<PrologTerm>();
-	private final List<Status> stati = new ArrayList<Status>();
+	private final List<LTL> formulas = new ArrayList<LTL>();
+	private final List<String> stati = new ArrayList<String>();
 
 	private final AnimationSelector animations;
-	private final LtlParser ltlParser;
+
 
 	@Inject
-	public LtlFormula(final AnimationSelector animations) {
+	public LtlFormula(final AnimationSelector animations)
+			throws LtlParseException {
 		this.animations = animations;
-		/* TODO: worry about the language specific parser */
-		ltlParser = new LtlParser(new ClassicalBParser());
 
-		CompoundPrologTerm cpt1 = new CompoundPrologTerm("globally",
-				new CompoundPrologTerm("finally", new CompoundPrologTerm(
-						"action", new CompoundPrologTerm("btrans",
-								new CompoundPrologTerm("event",
-										new CompoundPrologTerm("new"))))));
-		CompoundPrologTerm cpt2 = new CompoundPrologTerm("finally",
-				new CompoundPrologTerm("action", new CompoundPrologTerm(
-						"btrans", new CompoundPrologTerm("event",
-								new CompoundPrologTerm("new")))));
-		CompoundPrologTerm cpt3 = new CompoundPrologTerm("true");
-		formulas.add(cpt1);
-		formulas.add(cpt2);
-		formulas.add(cpt3);
-		stati.add(null);
-		stati.add(null);
-		stati.add(null);
+		LTL ltl1 = new LTL("GF[new]");
+		LTL ltl2 = new LTL("F[new]");
+		LTL ltl3 = new LTL("true");
+
+		formulas.add(ltl1);
+		formulas.add(ltl2);
+		formulas.add(ltl3);
+		stati.add("unchecked");
+		stati.add("unchecked");
+		stati.add("unchecked");
 	}
 
 	public void submit_formulas() {
 		final int len = formulas.size();
 		Object[] res = new Object[len];
 		for (int i = 0; i < len; i++) {
-			final Status x = stati.get(i);
-			final String statusString = x == null ? "unchecked" : x.toString();
+			final String statusString = stati.get(i);
 			res[i] = WebUtils.wrap("id", String.valueOf(i), "formulaText",
-					formulas.get(i), "status", statusString);
+					formulas.get(i).getCode(), "status", statusString);
 		}
 		Map<String, String> wrap = WebUtils.wrap("cmd",
 				"LtlFormula.setFormulas", "formulas", WebUtils.toJson(res));
@@ -74,17 +62,17 @@ public class LtlFormula extends AbstractSession {
 
 	public Object checkNthFormula(Map<String, String[]> params) {
 		int pos = Integer.parseInt(get(params, "pos"));
-		if (stati.get(pos) != null) {
-			return null;
-		}
 
-		PrologTerm formula = formulas.get(pos);
-		LtlCheckingCommand lcc = new LtlCheckingCommand(formula, 500,
-				StartMode.init);
+
+		LTL formula = formulas.get(pos);
+
+		// TODO: check formula in current state, show if outdated, etc.
+		StateId stateid = animations.getCurrentTrace().getCurrentState();
+		EvaluationCommand lcc = formula.getCommand(stateid);
 		animations.getCurrentTrace().getStateSpace().execute(lcc);
 
 		stati.remove(pos);
-		stati.add(pos, lcc.getResult().getStatus());
+		stati.add(pos, lcc.getValues().get(0).getValue());
 
 		submit_formulas();
 		return null;
@@ -105,11 +93,10 @@ public class LtlFormula extends AbstractSession {
 			return null;
 		}
 
+		LTL ltl = new LTL(formula);
 
-		PrologTerm compoundPrologTerm = ltlParser.generatePrologTerm(
-				formula, "root");
-		formulas.add(compoundPrologTerm);
-		stati.add(null);
+		formulas.add(ltl);
+		stati.add("unchecked");
 		logger.trace(params.toString());
 		submit_formulas();
 		return null;
