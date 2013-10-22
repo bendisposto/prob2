68118c542e980962867a5cc01bc22f425b942957
Philipp Koerner <p.koerner@uni-duesseldorf.de>
2013-10-22 11:46:53 +0200

    the LTL formula view now reacts if the state is changed; refactoring: use a tuple of an LTL object and a status-String instead of keeping two seperate lists

--- de.prob.core.kernel/src/main/java/de/prob/web/views/LtlFormula.java
+++ de.prob.core.kernel/src/main/java/de/prob/web/views/LtlFormula.java
@@ -15,16 +15,18 @@ import de.be4.ltl.core.parser.LtlParseException;
 import de.prob.animator.command.EvaluationCommand;
 import de.prob.animator.domainobjects.LTL;
 import de.prob.statespace.AnimationSelector;
+import de.prob.statespace.IAnimationChangeListener;
 import de.prob.statespace.StateId;
+import de.prob.statespace.Trace;
 import de.prob.web.AbstractSession;
 import de.prob.web.WebUtils;
 
-public class LtlFormula extends AbstractSession {
+public class LtlFormula extends AbstractSession implements
+		IAnimationChangeListener {
 
 	private final Logger logger = LoggerFactory.getLogger(CurrentTrace.class);
 
-	private final List<LTL> formulas = new ArrayList<LTL>();
-	private final List<String> stati = new ArrayList<String>();
+	private final List<LTLFormulaTuple> formulas = new ArrayList<LTLFormulaTuple>();
 
 	private final AnimationSelector animations;
 
@@ -33,26 +35,25 @@ public class LtlFormula extends AbstractSession {
 	public LtlFormula(final AnimationSelector animations)
 			throws LtlParseException {
 		this.animations = animations;
+		animations.registerAnimationChangeListener(this);
 
 		LTL ltl1 = new LTL("GF[new]");
 		LTL ltl2 = new LTL("F[new]");
 		LTL ltl3 = new LTL("true");
 
-		formulas.add(ltl1);
-		formulas.add(ltl2);
-		formulas.add(ltl3);
-		stati.add("unchecked");
-		stati.add("unchecked");
-		stati.add("unchecked");
+		formulas.add(new LTLFormulaTuple(ltl1));
+		formulas.add(new LTLFormulaTuple(ltl2));
+		formulas.add(new LTLFormulaTuple(ltl3));
 	}
 
-	public void submit_formulas() {
+	public void submitFormulas() {
 		final int len = formulas.size();
 		Object[] res = new Object[len];
 		for (int i = 0; i < len; i++) {
-			final String statusString = stati.get(i);
+			LTLFormulaTuple tuple = formulas.get(i);
+			final String statusString = tuple.getStatus();
 			res[i] = WebUtils.wrap("id", String.valueOf(i), "formulaText",
-					formulas.get(i).getCode(), "status", statusString);
+					tuple.getFormula().getCode(), "status", statusString);
 		}
 		Map<String, String> wrap = WebUtils.wrap("cmd",
 				"LtlFormula.setFormulas", "formulas", WebUtils.toJson(res));
@@ -62,27 +63,25 @@ public class LtlFormula extends AbstractSession {
 
 	public Object checkNthFormula(Map<String, String[]> params) {
 		int pos = Integer.parseInt(get(params, "pos"));
+		LTLFormulaTuple tuple = formulas.get(pos);
 
-
-		LTL formula = formulas.get(pos);
+		LTL formula = tuple.getFormula();
 
 		// TODO: check formula in current state, show if outdated, etc.
 		StateId stateid = animations.getCurrentTrace().getCurrentState();
 		EvaluationCommand lcc = formula.getCommand(stateid);
 		animations.getCurrentTrace().getStateSpace().execute(lcc);
 
-		stati.remove(pos);
-		stati.add(pos, lcc.getValues().get(0).getValue());
+		tuple.setStatus(lcc.getValues().get(0).getValue());
 
-		submit_formulas();
+		submitFormulas();
 		return null;
 	}
 
 	public Object removeFormula(Map<String, String[]> params) {
 		int pos = Integer.parseInt(get(params, "pos"));
 		formulas.remove(pos);
-		stati.remove(pos);
-		submit_formulas();
+		submitFormulas();
 		return null;
 	}
 
@@ -95,10 +94,9 @@ public class LtlFormula extends AbstractSession {
 
 		LTL ltl = new LTL(formula);
 
-		formulas.add(ltl);
-		stati.add("unchecked");
+		formulas.add(new LTLFormulaTuple(ltl));
 		logger.trace(params.toString());
-		submit_formulas();
+		submitFormulas();
 		return null;
 	}
 
@@ -110,7 +108,48 @@ public class LtlFormula extends AbstractSession {
 	@Override
 	public void reload(String client, int lastinfo, AsyncContext context) {
 		super.reload(client, lastinfo, context);
-		submit_formulas();
+		submitFormulas();
+	}
+
+	@Override
+	public void traceChange(Trace trace) {
+		for (LTLFormulaTuple tuple : formulas) {
+			tuple.resetStatus();
+		}
+		submitFormulas();
+	}
+
+	private class LTLFormulaTuple {
+
+		private final LTL formula;
+		private String status;
+
+		public LTLFormulaTuple(LTL f) {
+			formula = f;
+			this.setStatus("unchecked");
+		}
+
+		public LTLFormulaTuple(LTL f, String status) {
+			formula = f;
+			this.setStatus(status);
+		}
+
+		public LTL getFormula() {
+			return formula;
+		}
+
+		public String getStatus() {
+			return status;
+		}
+
+		public void setStatus(String status) {
+			this.status = status;
+		}
+
+		public void resetStatus() {
+			status = "unchecked";
+		}
+
 	}
 
 }
