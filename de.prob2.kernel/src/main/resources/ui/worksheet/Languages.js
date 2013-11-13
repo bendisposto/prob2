bbd2ed259a3732dcb5fbf45e79caa9365867b0b5
Rene Goebbbels <Rene.Goebbbels@hhu.de>
2013-11-13 11:28:50 +0100

    Completed Trace constructor method to support trace selection

--- de.prob2.kernel/src/main/resources/ui/worksheet/Languages.js
+++ de.prob2.kernel/src/main/resources/ui/worksheet/Languages.js
@@ -39,6 +39,28 @@ Languages = (function() {
 				theme : "default",
 				viewportMargin : Infinity,
 			},
+			construct : function(id,editor_data){
+				if(editor_data.args!=null){
+					
+					var templateArgs={
+						"withDropdown":(editor_data.args.traces!=null && editor_data.args.traces.length>1),
+						"items":editor_data.args.traces,
+						"type":"trace"
+					};
+					var menu=$(Worksheet.getSession().render("/ui/worksheet/box-menu.html",templateArgs));
+					$("#box"+id+" .heading-bar").append(menu);
+					menu.find(".selectee").click(function(e){
+							$(e.target).closest(".box-menu").find(".selectee").removeClass("selected");
+							$(e.target).addClass("selected");
+							$(e.target).closest(".box-menu").find(".trace-selected").html(e.target.id);
+						});
+					if(editor_data.args.traces!=null && editor_data.args.traces.length>0){ 
+						menu.find(".trace-selected").html(editor_data.args.traces[0]);
+					}else{
+						menu.find(".trace-selected").html("No Traces in Worksheet");
+					}
+				}				
+			},
 			no_vars : true,
 			has_source : true
 		},
