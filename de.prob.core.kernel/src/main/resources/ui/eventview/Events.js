5454ffa7f3facb19db822276707b3d1eb878ad07
Lukas Ladenberger <lukas.ladenberger@googlemail.com>
2013-11-22 15:51:31 +0100

    added e.preventDefault() method to event call (working on PROBCORE-456)

--- de.prob.core.kernel/src/main/resources/ui/eventview/Events.js
+++ de.prob.core.kernel/src/main/resources/ui/eventview/Events.js
@@ -122,9 +122,9 @@ Events = (function() {
             }
         }, 250));
 
-        $("#random1").click(function(e) {random(1)})
-        $("#random5").click(function(e) {random(5)})
-        $("#random10").click(function(e) {random(10)})
+        $("#random1").click(function(e) {e.preventDefault();random(1)})
+        $("#random5").click(function(e) {e.preventDefault();random(5)})
+        $("#random10").click(function(e) {e.preventDefault();random(10)})
         $("#randomX").click(function(e) {
             e.preventDefault();
             if(!$("#randomX").prop("disabled")) {
@@ -201,6 +201,7 @@ Events = (function() {
             e.append(session.render("/ui/eventview/operation.html", v))
         }
         $(".enabled").click(function(e) {
+        	e.preventDefault();
             var id = e.currentTarget.id
             id = id.substring(2,id.length)
 //            console.log(id)
