b118b38b8da28cb9ac027f440bf9385555110f04
Joy Clark <joyclark2004@msn.com>
2013-12-02 18:06:03 +0100

    PROBCORE-453 Removed deprecated/unnecessary commands

--- de.prob.core.kernel/src/main/java/de/prob/animator/command/AbstractCommand.java
+++ de.prob.core.kernel/src/main/java/de/prob/animator/command/AbstractCommand.java
@@ -9,17 +9,12 @@ import de.prob.prolog.output.IPrologTermOutput;
 import de.prob.prolog.term.PrologTerm;
 
 /**
- * The ICommand interface is used to implement composable interactions with the
- * ProB core. It defines two callback methods that are being called by the
- * Animator when the command is being performed.
+ * The {@link AbstractCommand} class is used to implement composable
+ * interactions with the ProB core. It defines two callback methods that are
+ * being called by the Animator when the command is being performed. It also
+ * provides a {@link #getSubcommands()} method to break down the command into
+ * separate commands to improve debugging.
  * 
- * @author bendisposto
- */
-/**
- * @author joy
- *
- */
-/**
  * @author joy
  * 
  */
@@ -36,29 +31,36 @@ public abstract class AbstractCommand {
 	public abstract void writeCommand(IPrologTermOutput pto);
 
 	/**
+	 * <p>
 	 * After performing the query this method receives a Map of bindings from
 	 * variable names used in the query to Prolog terms representing the answer.
+	 * </p>
 	 * 
+	 * <p>
 	 * A number of helper tools can be used when processing the results (see
 	 * {@link de.prob.parser.BindingGenerator})
+	 * </p>
 	 * 
+	 * <p>
 	 * Note: This method is allowed to throw {@link ResultParserException} if
 	 * the answer from Prolog does not match the expectation. The exception is a
 	 * subclass of RuntimeException and it should always indicate a bug (or
 	 * version inconsistency)
+	 * </p>
 	 * 
 	 * @param bindings
+	 *            {@link ISimplifiedROMap} of String variable names to their
+	 *            calculated answers represented as {@link PrologTerm}s
 	 */
 	public abstract void processResult(
 			ISimplifiedROMap<String, PrologTerm> bindings);
 
 	/**
-	 * Returns the list of subcommands contained in a given
-	 * {@link AbstractCommand}. This is called by {@code AnimatorImpl} when run
-	 * in debug mode. If developers want individual commands to be executed
-	 * separately when an {@link AbstractCommand} is executed, then this method
-	 * MUST be overriden. By default, the {@link Collections#emptyList()} is
-	 * returned.
+	 * Returns the list of sub-commands contained in a given
+	 * {@link AbstractCommand}. This allow the animator to debug the code. If
+	 * developers want individual commands to be executed separately in debug
+	 * mode when an {@link AbstractCommand} is executed, then this method MUST
+	 * be overridden. By default, {@link Collections#emptyList()} is returned.
 	 * 
 	 * @return {@code List} of {@link AbstractCommand} subcommands
 	 */
