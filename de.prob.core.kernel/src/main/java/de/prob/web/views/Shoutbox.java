02d595ca1e1c5b8f8b56881e3b05fb188673c461
Jens Bendisposto <jens@bendisposto.de>
2014-02-17 11:27:29 +0100

    Simple shoutbox

---
+++ de.prob.core.kernel/src/main/java/de/prob/web/views/Shoutbox.java
@@ -0,0 +1,59 @@
+package de.prob.web.views;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.AsyncContext;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.inject.Singleton;
+
+import de.prob.web.AbstractSession;
+import de.prob.web.WebUtils;
+
+@Singleton
+public class Shoutbox extends AbstractSession {
+
+	private final Logger logger = LoggerFactory.getLogger(Shoutbox.class);
+	private final List<Shout> texts = new ArrayList<Shout>();
+	protected boolean incrementalUpdate = false;
+
+	private static class Shout {
+		public final String text;
+		public final Date time;
+
+		public Shout(String text) {
+			this.text = text;
+			time = Calendar.getInstance().getTime();
+		}
+	}
+
+	public Object addText(final Map<String, String[]> params) {
+		String text = params.get("text")[0];
+		Shout shout = new Shout(text);
+		texts.add(shout);
+		return WebUtils.wrap("cmd", "Shoutbox.append", "line",
+				WebUtils.toJson(shout));
+	}
+
+	@Override
+	public String html(final String clientid,
+			final Map<String, String[]> parameterMap) {
+		return simpleRender(clientid, "ui/shoutbox/index.html");
+	}
+
+	@Override
+	public void reload(final String client, final int lastinfo,
+			final AsyncContext context) {
+		super.reload(client, lastinfo, context);
+		Map<String, String> wrap = WebUtils.wrap("cmd", "Shoutbox.setText",
+				"texts", WebUtils.toJson(texts));
+		submit(wrap);
+	}
+
+}
