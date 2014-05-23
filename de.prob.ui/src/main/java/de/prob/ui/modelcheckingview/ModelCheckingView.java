43a25b19fa66a91e46d423fd47c00627fab02e19
Lukas Ladenberger <lukas.ladenberger@googlemail.com>
2014-05-23 16:16:24 +0200

    fixed id reference to modelchecking view

--- de.prob.ui/src/main/java/de/prob/ui/modelcheckingview/ModelCheckingView.java
+++ de.prob.ui/src/main/java/de/prob/ui/modelcheckingview/ModelCheckingView.java
@@ -6,7 +6,7 @@ public class ModelCheckingView extends BrowserView {
 	/**
 	 * The ID of the view as specified by the extension.
 	 */
-	public static final String ID = "de.prob.ui.modelcheckingview.ModelCheckingView";
+	public static final String ID = "de.prob.ui.model_check";
 
 	@Override
 	protected String getUrl() {
