2d787c738fdddc9a5bb38f1872404a22f41d0c0d
Lukas Ladenberger <lukas.ladenberger@googlemail.com>
2014-05-27 10:12:54 +0200

    improved observer attribute handling + some minor bugfixes

--- de.prob.core.kernel/src/main/resources/ui/bmsview/bms-editor/src/svg-attributes.js
+++ de.prob.core.kernel/src/main/resources/ui/bmsview/bms-editor/src/svg-attributes.js
@@ -1,64 +1,109 @@
+var svgAttributeList = {
+	"x" : {value: 'x', text: 'x'},
+	"y" : {value: 'y', text: 'y'},
+	"rx" : {value: 'rx', text: 'rx'},
+	"ry" : {value: 'ry', text: 'ry'},
+	"cx" : {value: 'cx', text: 'cx'},
+	"cy" : {value: 'cy', text: 'cy'},
+	"x1" : {value: 'x1', text: 'x1'},
+	"x2" : {value: 'x2', text: 'x2'},
+	"y1" : {value: 'y1', text: 'y1'},
+	"y2" : {value: 'y2', text: 'y2'},	
+   	"width" : {value: 'width', text: 'width'},
+  	"height" : {value: 'height', text: 'height'},
+	"style" : {value: "style", text: 'style'},
+	"class" : {value: "class", text: 'class'},
+	"image" : {value: 'image', text: 'image', change: function(selector, value) {
+		$(selector).attr('xlink:href', value)
+	}},
+	"font-size" : {value: 'font-size', text: 'font-size'},
+	"font-family" : {value: 'font-family', text: 'font-family'},
+	"font-weight" : {value: 'font-weight', text: 'font-weight'},
+	"font-family" : {value: 'font-family', text: 'font-family'},	
+	"text" :  {value: 'text', text: 'text', change: function(selector, value) {
+		$(selector).html(value)
+	}},
+	"clip-path" : {value: 'clip-path', text: 'clip-path'},
+	"fill" : {value: 'fill', text: 'fill'},
+	"fill-opacity" : {value: 'fill-opacity', text: 'fill-opacity'},
+	"fill-rule" : {value: 'fill-rule', text: 'fill-rule'},
+	"marker-end" : {value: 'marker-end', text: 'marker-end'},
+	"marker-mid" : {value: 'marker-mid', text: 'marker-mid'},
+	"marker-start" : {value: 'marker-start', text: 'marker-start'},
+	"pattern" : {value: 'pattern', text: 'pattern'},
+	"stroke" : {value: 'stroke', text: 'stroke'},
+	"stroke-dasharray" : {value: 'stroke-dasharray', text: 'stroke-dasharray'},
+	"stroke-dashoffset" : {value: 'stroke-dashoffset', text: 'stroke-dashoffset'},
+	"stroke-linecap" : {value: 'stroke-linecap', text: 'stroke-linecap'},
+	"stroke-linejoin" : {value: 'stroke-linejoin', text: 'stroke-linejoin'},
+	"stroke-miterlimit" : {value: 'stroke-miterlimit', text: 'stroke-miterlimit'},
+	"stroke-opacity" : {value: 'stroke-opacity', text: 'stroke-opacity'},
+	"stroke-width" : {value: 'stroke-width', text: 'stroke-width'},
+	"transform" : {value: 'transform', text: 'transform'}
+};
+
 var svgAllElementAttributes = [
-	{value: "style", text: 'style'},
-	{value: "class", text: 'class'}
+    svgAttributeList["style"], 
+    svgAttributeList["class"]
 ];
 
 var svgAllShapesAttributes = [
-	{value: 'clip-path', text: 'clip-path'},
-	{value: 'fill', text: 'fill'},
-	{value: 'fill-opacity', text: 'fill-opacity'},
-	{value: 'fill-rule', text: 'fill-rule'},
-	{value: 'marker-end', text: 'marker-end'},
-	{value: 'marker-mid', text: 'marker-mid'},
-	{value: 'marker-start', text: 'marker-start'},
-	{value: 'pattern', text: 'pattern'},
-	{value: 'stroke', text: 'stroke'},
-	{value: 'stroke-dasharray', text: 'stroke-dasharray'},
-	{value: 'stroke-dashoffset', text: 'stroke-dashoffset'},
-	{value: 'stroke-linecap', text: 'stroke-linecap'},
-	{value: 'stroke-linejoin', text: 'stroke-linejoin'},
-	{value: 'stroke-miterlimit', text: 'stroke-miterlimit'},
-	{value: 'stroke-opacity', text: 'stroke-opacity'},
-	{value: 'stroke-width', text: 'stroke-width'},
-	{value: 'transform', text: 'transform'}
+	svgAttributeList['clip-path'],
+	svgAttributeList['fill'],
+	svgAttributeList['fill-opacity'],
+	svgAttributeList['fill-rule'],
+	svgAttributeList['marker-end'],
+	svgAttributeList['marker-mid'],
+	svgAttributeList['marker-start'],
+	svgAttributeList['pattern'],
+	svgAttributeList['stroke'],
+	svgAttributeList['stroke-dasharray'],
+	svgAttributeList['stroke-dashoffset'],
+	svgAttributeList['stroke-linecap'],
+	svgAttributeList['stroke-linejoin'],
+	svgAttributeList['stroke-miterlimit'],
+	svgAttributeList['stroke-opacity'],
+	svgAttributeList['stroke-width'],
+	svgAttributeList['transform']
 ];
 
 var svgCoordinatesAttributes = [
-	{value: 'x', text: 'x'},
-	{value: 'y', text: 'y'}
+	svgAttributeList['x'],
+	svgAttributeList['y']
 ];
 
 var svgRoundedCoordinatesAttributes = [
-	{value: 'rx', text: 'rx'},
-	{value: 'ry', text: 'ry'}
+	svgAttributeList['rx'],
+	svgAttributeList['ry']
 ];
 
 var svgRadiusCoordinatesAttributes = [
-	{value: 'cx', text: 'cx'},
-	{value: 'cy', text: 'cy'}
+	svgAttributeList['cx'],
+	svgAttributeList['cy']
 ];
 
 var svgDimensionAttributes = [
-   	{value: 'width', text: 'width'},
-  	{value: 'height', text: 'height'}
+   	svgAttributeList['width'],
+  	svgAttributeList['height']
 ];
 
 var svgTextAttributes = [
-	{value: 'font-size', text: 'font-size'},
-	{value: 'font-family', text: 'font-family'},
-	{value: 'font-weight', text: 'font-weight'},
-	{value: 'font-family', text: 'font-family'}
+    svgAttributeList['text'],
+    svgAttributeList['font-size'],
+	svgAttributeList['font-family'],
+	svgAttributeList['font-weight'],
+	svgAttributeList['font-family']
 ];
 
 var svgLineCoordinatesAttributes = [
-	{value: 'x1', text: 'x1'},
-	{value: 'x2', text: 'x2'},
-	{value: 'y1', text: 'y1'},
-	{value: 'y2', text: 'y2'}
+	svgAttributeList['x1'],
+	svgAttributeList['x2'],
+	svgAttributeList['y1'],
+	svgAttributeList['y2']
 ];
 
 var svgImageAttributes = [
-	{value: 'image', text: 'image'},
+	svgAttributeList['image']
 ]
 
 var svgAttributes = {
