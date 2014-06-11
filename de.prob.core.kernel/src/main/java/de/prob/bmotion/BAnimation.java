a727730c417a9360086633b4bb632908b0b7811c
Joy Clark <clark.joy@gmail.com>
2014-06-11 15:18:36 +0200

    PROBCORE-541 Created two example ITool implementations.

---
+++ de.prob.core.kernel/src/main/java/de/prob/bmotion/BAnimation.java
@@ -0,0 +1,82 @@
+package de.prob.bmotion;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import de.prob.animator.domainobjects.EvalResult;
+import de.prob.animator.domainobjects.EvaluationException;
+import de.prob.animator.domainobjects.IEvalElement;
+import de.prob.animator.domainobjects.IEvalResult;
+import de.prob.statespace.StateId;
+import de.prob.statespace.StateSpace;
+import de.prob.statespace.Trace;
+import de.prob.ui.api.ITool;
+import de.prob.ui.api.IllegalFormulaException;
+import de.prob.ui.api.ImpossibleStepException;
+
+public class BAnimation implements ITool {
+
+	private Trace trace;
+	Map<String, IEvalElement> formulas = new HashMap<String, IEvalElement>();
+
+	public BAnimation(final Trace trace) {
+		this.trace = trace;
+	}
+
+	@Override
+	public String doStep(final String stateref, final String event,
+			final String... parameters) throws ImpossibleStepException {
+		trace = trace.execute(event, Arrays.asList(parameters));
+		return trace.getCurrentState().getId();
+	}
+
+	@Override
+	public String evaluate(final String stateref, final String formula)
+			throws IllegalFormulaException {
+		StateSpace space = trace.getStateSpace();
+		if (!formulas.containsKey(formula)) {
+			IEvalElement e = trace.getModel().parseFormula(formula);
+			space.subscribe(this, e);
+			formulas.put(formula, e);
+		}
+		StateId sId = space.getVertex(stateref);
+		IEvalResult res = space.valuesAt(sId).get(formulas.get(formula));
+		if (res instanceof EvalResult) {
+			return ((EvalResult) res).getValue();
+		}
+		return res.toString();
+	}
+
+	@Override
+	public List<String> getErrors(final String state, final String formula) {
+		List<String> errors = new ArrayList<String>();
+		try {
+			IEvalElement e = trace.getModel().parseFormula(formula);
+			StateSpace space = trace.getStateSpace();
+			StateId sId = space.getVertex(state);
+			if (!space.canBeEvaluated(sId)) {
+				errors.add("State not initialized");
+			}
+			space.eval(sId, Arrays.asList(e));
+		} catch (EvaluationException e) {
+			errors.add("Could not parse: " + e.getMessage());
+		} catch (Exception e) {
+			errors.add(e.getClass() + " thrown: " + e.getMessage());
+		}
+		return errors;
+	}
+
+	@Override
+	public String getCurrentState() {
+		return trace.getCurrentState().getId();
+	}
+
+	@Override
+	public boolean canBacktrack() {
+		return true;
+	}
+
+}
---
+++ de.prob.core.kernel/src/main/java/de/prob/bmotion/CSPAnimation.java
@@ -0,0 +1,76 @@
+package de.prob.bmotion;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import de.prob.animator.domainobjects.EvalResult;
+import de.prob.animator.domainobjects.EvaluationException;
+import de.prob.animator.domainobjects.IEvalElement;
+import de.prob.animator.domainobjects.IEvalResult;
+import de.prob.statespace.StateSpace;
+import de.prob.statespace.Trace;
+import de.prob.ui.api.ITool;
+import de.prob.ui.api.IllegalFormulaException;
+import de.prob.ui.api.ImpossibleStepException;
+
+public class CSPAnimation implements ITool {
+
+	private Trace trace;
+	private final Map<String, IEvalResult> formulaCache = new HashMap<String, IEvalResult>();
+
+	public CSPAnimation(final Trace trace) {
+		this.trace = trace;
+	}
+
+	@Override
+	public String doStep(final String stateref, final String event,
+			final String... parameters) throws ImpossibleStepException {
+		trace = trace.execute(event, Arrays.asList(parameters));
+		return trace.getCurrentState().getId();
+	}
+
+	@Override
+	public String evaluate(final String stateref, final String formula)
+			throws IllegalFormulaException {
+		if (!formulaCache.containsKey(formula)) {
+			IEvalElement e = trace.getModel().parseFormula(formula);
+			StateSpace space = trace.getStateSpace();
+			List<IEvalResult> results = space.eval(space.getVertex(stateref),
+					Arrays.asList(e));
+			formulaCache.put(formula, results.get(0));
+		}
+		IEvalResult res = formulaCache.get(formula);
+		if (res instanceof EvalResult) {
+			return ((EvalResult) res).getValue();
+		}
+		return res.toString();
+	}
+
+	@Override
+	public List<String> getErrors(final String state, final String formula) {
+		List<String> errors = new ArrayList<String>();
+		try {
+			IEvalElement e = trace.getModel().parseFormula(formula);
+			StateSpace space = trace.getStateSpace();
+			space.eval(space.getVertex(state), Arrays.asList(e));
+		} catch (EvaluationException e) {
+			errors.add("parse error : " + e.getMessage());
+		} catch (Exception e) {
+			errors.add("thrown " + e.getClass() + " because " + e.getMessage());
+		}
+		return errors;
+	}
+
+	@Override
+	public String getCurrentState() {
+		return trace.getCurrentState().getId();
+	}
+
+	@Override
+	public boolean canBacktrack() {
+		return true;
+	}
+}
