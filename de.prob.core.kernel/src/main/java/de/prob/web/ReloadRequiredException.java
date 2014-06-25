4c208fc2f3df9f6cb7419e75861c47a1efc3a67c
Jens Bendisposto <jens@bendisposto.de>
2014-06-25 11:10:35 +0200

    fixed some warnings

--- de.prob.core.kernel/src/main/java/de/prob/web/ReloadRequiredException.java
+++ de.prob.core.kernel/src/main/java/de/prob/web/ReloadRequiredException.java
@@ -1,5 +1,6 @@
 package de.prob.web;
 
+@SuppressWarnings("serial")
 public class ReloadRequiredException extends Exception {
 
 }
--- de.prob.ui/src/main/java/de/prob/ui/visualization/OpenStateSpaceVizHandler.java
+++ de.prob.ui/src/main/java/de/prob/ui/visualization/OpenStateSpaceVizHandler.java
@@ -4,9 +4,7 @@ import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.IHandler;
-import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.PartInitException;
-import org.eclipse.ui.handlers.HandlerUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -32,7 +30,6 @@ public class OpenStateSpaceVizHandler extends AbstractHandler implements
 
 	@Override
 	public Object execute(final ExecutionEvent event) throws ExecutionException {
-		Shell shell = HandlerUtil.getActiveShell(event);
 
 		try {
 			String sessionId = ProB2ViewUtil.createSessionId();
