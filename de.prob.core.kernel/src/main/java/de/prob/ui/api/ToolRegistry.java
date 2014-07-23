782d90a3ea43f23e9907074bdb41b108033e0c55
Lukas Ladenberger <lukas.ladenberger@googlemail.com>
2014-07-23 15:29:02 +0200

    set to singleton

--- de.prob.core.kernel/src/main/java/de/prob/ui/api/ToolRegistry.java
+++ de.prob.core.kernel/src/main/java/de/prob/ui/api/ToolRegistry.java
@@ -6,6 +6,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import com.google.inject.Singleton;
+
+@Singleton
 public class ToolRegistry {
 
 	private final Map<String, ITool> tools = new HashMap<String, ITool>();
