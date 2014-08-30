42f6c762e6ea99f7bae3be5164b70fd5bb55a33c
Jens Bendisposto <jens@bendisposto.de>
2014-08-30 15:48:43 +0200

    implemented code to restart app

---
+++ de.prob2/dev/user.clj
@@ -0,0 +1,30 @@
+(ns user
+   (:require [com.stuartsierra.component :as component]
+             [clojure.tools.namespace.repl :refer (refresh)]
+             [de.prob2 :as app]))
+
+(def system nil)
+
+(defn init []
+  (alter-var-root #'system
+    (constantly (app/new-system {:host "localhost" :port 9001}))))
+
+(defn start []
+  (alter-var-root #'system component/start))
+
+(defn stop []
+  (alter-var-root #'system
+    (fn [s] (when s (component/stop s)))))
+
+(defn go []
+  (init)
+  (start))
+
+(defn reset []
+  (stop)
+  (refresh :after 'user/go))
+
+(println "Devlopment Mode")
+(println "Use (go) to start the system")
+(println "Use (reset) to reload all sources and restart the system")
+
