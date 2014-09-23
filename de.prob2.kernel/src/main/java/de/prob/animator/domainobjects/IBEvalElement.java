8a6418f595548bcad31b1401a8a760f696d62f8d
Sebastian Krings <sebastian@krin.gs>
2014-09-23 11:29:10 +0200

    move printProlog to interface in order to write a command that takes both classical B and Event-B as an input

--- de.prob2.kernel/src/main/java/de/prob/animator/domainobjects/IBEvalElement.java
+++ de.prob2.kernel/src/main/java/de/prob/animator/domainobjects/IBEvalElement.java
@@ -1,7 +1,9 @@
 package de.prob.animator.domainobjects;
 
 import de.be4.classicalb.core.parser.node.Node;
+import de.prob.prolog.output.IPrologTermOutput;
 
 public interface IBEvalElement extends IEvalElement {
 	public Node getAst();
+	public void printProlog(IPrologTermOutput pout);
 }
