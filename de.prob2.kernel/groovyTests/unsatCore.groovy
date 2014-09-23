a6f4fa333141278288cf4be545e19f10bd02dd70
Sebastian Krings <sebastian@krin.gs>
2014-09-23 11:45:53 +0200

    load a machine to make the testcase work

--- de.prob2.kernel/groovyTests/unsatCore.groovy
+++ de.prob2.kernel/groovyTests/unsatCore.groovy
@@ -3,13 +3,13 @@ import de.prob.animator.command.*
 import de.prob.statespace.*
 import de.prob.animator.*
 
-IAnimator anim = Main.getInjector().getInstance(
-	IAnimator.class);
+m = api.b_load(dir+"/machines/scheduler.mch") // machine is not needed...
+s = m as StateSpace
 
 pred = new ClassicalB("x > 7 & 1=4")
 c = new UnsatCoreCommand(pred,[])
 
-anim.execute(c)
+s.execute(c)
 assert("1=4".equals(c.getCore().toString()))
 
 " The UnsatCoreCommand and MinimumUnsatCoreCommand work as expected."
