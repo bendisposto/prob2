ea3b6c843929903bafc2c1e1bb2795999cf9291c
Sebastian Krings <sebastian@krin.gs>
2014-09-24 16:40:45 +0200

    remove method from interface because it is already in extended interface

--- de.prob2.kernel/src/main/java/de/prob/animator/domainobjects/IBEvalElement.java
+++ de.prob2.kernel/src/main/java/de/prob/animator/domainobjects/IBEvalElement.java
@@ -1,9 +1,7 @@
 package de.prob.animator.domainobjects;
 
 import de.be4.classicalb.core.parser.node.Node;
-import de.prob.prolog.output.IPrologTermOutput;
 
 public interface IBEvalElement extends IEvalElement {
 	public Node getAst();
-	public void printProlog(IPrologTermOutput pout);
 }
