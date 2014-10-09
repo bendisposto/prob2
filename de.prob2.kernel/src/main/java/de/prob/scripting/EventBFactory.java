4d6da802d4629dccccb46f9bcf62a6610eecbcf7
Joy Clark <clark.joy@gmail.com>
2014-10-09 16:30:19 +0200

    PROBCORE-598 first check to see how to reload EventBModels

--- de.prob2.kernel/src/main/java/de/prob/scripting/EventBFactory.java
+++ de.prob2.kernel/src/main/java/de/prob/scripting/EventBFactory.java
@@ -13,10 +13,16 @@ import de.prob.animator.command.ComposedCommand;
 import de.prob.animator.command.LoadEventBProjectCommand;
 import de.prob.animator.command.SetPreferenceCommand;
 import de.prob.animator.command.StartAnimationCommand;
+import de.prob.model.eventb.Context;
+import de.prob.model.eventb.EventBMachine;
 import de.prob.model.eventb.EventBModel;
+import de.prob.model.eventb.theory.Theory;
 import de.prob.model.eventb.translate.EventBDatabaseTranslator;
 import de.prob.model.eventb.translate.EventBModelTranslator;
+import de.prob.model.representation.RefType;
 import de.prob.statespace.StateSpace;
+import edu.uci.ics.jung.graph.DirectedSparseMultigraph;
+import edu.uci.ics.jung.graph.util.Pair;
 
 public class EventBFactory extends ModelFactory {
 
@@ -36,6 +42,34 @@ public class EventBFactory extends ModelFactory {
 		new EventBDatabaseTranslator(model, file);
 		model.isFinished();
 
+		return load2(model, prefs, loadVariables);
+	}
+
+	public EventBModel reload(final EventBModel model,
+			final Map<String, String> prefs, final boolean loadVariables) {
+		EventBModel newModel = modelProvider.get();
+
+		for (EventBMachine eventBMachine : model.getMachines()) {
+			newModel.addMachine(eventBMachine);
+		}
+		for (Context context : model.getContexts()) {
+			newModel.addContext(context);
+		}
+		newModel.addTheories(model.getChildrenOfType(Theory.class));
+		DirectedSparseMultigraph<String, RefType> graph = model.getGraph();
+		for (RefType refType : graph.getEdges()) {
+			Pair<String> endpoints = graph.getEndpoints(refType);
+			newModel.addRelationship(endpoints.getFirst(),
+					endpoints.getSecond(), refType);
+		}
+		newModel.setModelFile(model.getModelFile());
+		newModel.isFinished();
+
+		return load2(newModel, prefs, loadVariables);
+	}
+
+	private EventBModel load2(final EventBModel model,
+			final Map<String, String> prefs, final boolean loadVariables) {
 		List<AbstractCommand> cmds = new ArrayList<AbstractCommand>();
 
 		for (Entry<String, String> pref : getPreferences(model, prefs)
