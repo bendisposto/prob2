21a33c6a6ca8b9449f5b1d6703bee1f46316d581
Joy Clark <clark.joy@gmail.com>
2014-10-20 17:08:58 +0200

    one 's' too many

--- de.prob2.kernel/src/main/groovy/de/prob/bmotion/BSetObserver.groovy
+++ de.prob2.kernel/src/main/groovy/de/prob/bmotion/BSetObserver.groovy
@@ -8,32 +8,32 @@ class BSetObserver extends TransformerObserver {
 
 	def String expression
 	def convertfn = { "#" + it }
-	def resolvefn = { it != null ? it.value.replace("{","").replace("}","").replaceAll(" ","").tokenize(",") : [] }
+	def resolvefn = { it != null ? it.value.replace("{","").replace("}","").replaceAll(" ","").tokenize(",") : []}
 
-    def static BSetObserver make(Closure cls) {
-        new BSetObserver().with cls
-    }
+	def static BSetObserver make(Closure cls) {
+		new BSetObserver().with cls
+	}
 
-    def BSetObserver expression(exp) {
-        this.expression = exp
-        this
-    }
+	def BSetObserver expression(exp) {
+		this.expression = exp
+		this
+	}
 
-    def BSetObserver convert(Closure cls) {
-        this.convertfn = clss
-        this
-    }
+	def BSetObserver convert(Closure cls) {
+		this.convertfn = cls
+		this
+	}
 
-    def BSetObserver resolve(Closure cls) {
-        this.resolvefn = cls
-        this
-    }
+	def BSetObserver resolve(Closure cls) {
+		this.resolvefn = cls
+		this
+	}
 
-    def List<TransformerObject> update(BMotion bms) {
-        def a = resolvefn(bms.eval(expression))
-        def b = a.collect{ convertfn(it) }
-        selector = b == []? "" : b.join(",")
-        super.update(bms)
-    }
+	def List<TransformerObject> update(BMotion bms) {
+		def a = resolvefn(bms.eval(expression))
+		def b = a.collect{ convertfn(it) }
+		selector = b == []? "" : b.join(",")
+		super.update(bms)
+	}
 
 }
