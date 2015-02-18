bbeeebf4e7566355f6afa281381fdc69c42d20c9
Jens Bendisposto <jens@bendisposto.de>
2015-02-18 10:08:50 +0100

    register change listeners

--- de.prob2/src/clj/de/prob2/kernel.clj
+++ de.prob2/src/clj/de/prob2/kernel.clj
@@ -1,24 +1,46 @@
 (ns de.prob2.kernel
   (:require [com.stuartsierra.component :as component])
-  (:import de.prob.Main))
+  (:import de.prob.Main
+           (de.prob.statespace AnimationSelector Trace IModelChangedListener IAnimationChangeListener StateSpace)))
+
+
+(defn instantiate [{inj :injector :as prob} cls]
+  (.getInstance inj cls))
+
+(defn- install-handlers [injector]
+  (println :enet)
+  (let [animations (.getInstance injector AnimationSelector)
+        _ (println :animations animations)
+        listener
+        (reify
+          IModelChangedListener
+          (modelChanged [this state-space] (println :model-changed state-space))
+          IAnimationChangeListener
+          (traceChange [this trace current] (println :trace-changed trace :current current))
+          (animatorStatus [this busy] (println :animator-busy busy)))]
+    (.registerAnimationChangeListener animations listener)
+    (.registerModelChangedListener animations listener)
+    listener))
 
 
 
-(defrecord ProB [injector]
+(defrecord ProB [injector listener]
   component/Lifecycle
   (start [this]
     (if injector
       this
       (do (println "Preparing ProB 2.0 Kernel")
-          (assoc this :injector (Main/getInjector)))))
+          (let [injector (Main/getInjector)
+                _ (println "Got the injector")
+                listener (install-handlers injector)
+                _ (println "Installed Listeners")]
+            (assoc this :injector injector :listener listener)))))
   (stop [this]
     (if injector (do (println "Shutting down ProB 2.0")
-                     (dissoc this :injector))
+                     (dissoc this :injector :listener))
         this)))
 
 (defn prob []
   (map->ProB {}))
 
-(defn instantiate [{inj :injector} cls]
-  (.getInstance inj cls))
 
