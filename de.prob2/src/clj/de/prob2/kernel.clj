b8c38996874a7bfd663d40bd4fb75ed30385186f
Jens Bendisposto <jens@bendisposto.de>
2015-02-21 09:06:06 +0100

    first sketch of a model transformer

--- de.prob2/src/clj/de/prob2/kernel.clj
+++ de.prob2/src/clj/de/prob2/kernel.clj
@@ -4,10 +4,64 @@
            (de.prob.statespace AnimationSelector Trace IModelChangedListener IAnimationChangeListener StateSpace)))
 
 
+(defn kebap-case
+  ([cls] (kebap-case cls ""))
+  ([cls add]
+     (keyword
+      (str (clojure.string/join
+            "-"
+            (map (fn [s] (.toLowerCase s))
+                 (map second (re-seq #"([A-Z][a-z]*)" (.getSimpleName cls))))) add))))
+
+(defn default-map [v m]
+  (into  {:type (kebap-case (class v)) :name (.toString v)} m))
+
+
+(defprotocol Transform (transform [this]))
+(extend-protocol Transform
+  de.prob.model.representation.Machine
+  (transform [v] (default-map v {}))
+  de.prob.model.eventb.Context
+  (transform [v] (default-map v {}))
+  de.prob.model.representation.BSet
+  (transform [v] (default-map v {}))
+  de.prob.model.representation.AbstractFormulaElement
+  (transform [v] (default-map v {:formula-id (.. v getFormula getFormulaId getUUID)}))
+  de.prob.model.classicalb.Operation
+  (transform [v] {:type (kebap-case (class v)) :name (.getName v) :output (.getProperty v "output") :parameter (.getProperty v "parameters")})
+  de.prob.model.eventb.Event
+  (transform [v] {:type (kebap-case (class v)) :name (.getName v)})
+  de.prob.model.representation.Action
+  (transform [v] (default-map v {}))
+  java.lang.Object
+  (transform [o] {:type :unknown :class (type o) :object (.toString o)}))
+
+(def exclude #{de.prob.model.eventb.ProofObligation})
+
+(declare extractE)
+
+(defn extractV [x]
+  (let [c (.getKey x) lv (.getValue x)]
+    [(keyword (.. c getSimpleName toLowerCase)) (if (exclude c) [] (map extractE lv))]))
+
+(defn extractE [absel]
+  (let [chd (.getChildren absel)
+            tgt (transform absel)]
+        (if (seq chd)
+          (do (->> chd (map extractV) (into tgt)))
+          tgt)))
+
+
+
+
 (defn notify-model-changed [{:keys [clients send-fn!] :as sente} state-space]
   (doseq [c (:any @clients)] (send-fn! c [::model-changed {:space :dude}])))
 (defn notify-trace-changed [send-fn trace current?])
 (defn notify-animator-busy [busy?])
+
+
+
+
 
 
 (defn instantiate [{inj :injector :as prob} cls]
