8bbcf83a49a5f1ac356d586104ec2caae2a15c47
Jens Bendisposto <jens@bendisposto.de>
2015-02-26 15:32:35 +0100

    message handling (client)

--- de.prob2/src/cljs/de/prob2/core.cljs
+++ de.prob2/src/cljs/de/prob2/core.cljs
@@ -1,40 +1,55 @@
 (ns de.prob2.core
-   (:require-macros
-      [cljs.core.async.macros :as asyncm :refer (go go-loop)])
-    (:require [reagent.core :as reagent :refer [atom]]
-              [reagent.session :as session]
-              [secretary.core :as secretary :include-macros true]
-              [goog.events :as events]
-              [goog.history.EventType :as EventType]
-              [cljs.core.async :as async :refer (<! >! put! chan)]
-              [taoensso.sente  :as sente :refer (cb-success?)]
-              [taoensso.encore :as enc    :refer (logf log logp)]
-              [cljsjs.react :as react])
-    (:import goog.History))
+  (:require-macros
+   [cljs.core.async.macros :as asyncm :refer (go go-loop)])
+  (:require [reagent.core :as reagent :refer [atom]]
+            [reagent.session :as session]
+            [secretary.core :as secretary :include-macros true]
+            [goog.events :as events]
+            [cognitect.transit :as transit]
+            [goog.history.EventType :as EventType]
+            [cljs.core.async :as async :refer (<! >! put! chan)]
+            [taoensso.sente  :as sente :refer (cb-success?)]
+            [taoensso.encore :as enc    :refer (logf log logp)]
+            [cljsjs.react :as react])
+  (:import goog.History))
 
 ;; -------------------------
 ;; Views
-;(sente/set-logging-level! :trace)
+                                        ;(sente/set-logging-level! :trace)
 
 
 (let [{:keys [chsk ch-recv send-fn state]}
       (sente/make-channel-socket! "/updates" ; Note the same path as before
-       {:type :auto ; e/o #{:auto :ajax :ws}
-       })]
+                                  {:type :auto ; e/o #{:auto :ajax :ws}
+                                   })]
   (def chsk       chsk)
   (def ch-chsk    ch-recv) ; ChannelSocket's receive channel
   (def chsk-send! send-fn) ; ChannelSocket's send API fn
   (def chsk-state state)   ; Watchable, read-only atom
   )
 
-(defn event-msg-handler [event]
-  (let [msg (:?data event)]
-    (logf "Message-Type: %s" (first msg))
-    (log (:id event))
-    (logp msg)))
+(defn read-transit [msg]
+  (let [r (transit/reader :json-verbose)]
+    (transit/read r msg)))
 
-(sente/start-chsk-router! ch-chsk event-msg-handler)
 
+(defmulti handle first)
+
+(defmethod handle :de.prob2.kernel/model-changed [[_ m]]
+  (logp m))
+
+
+(defmethod handle :default [[t m]]
+  (logp "Received Type: " t)
+  (logp "Received Msg: " m))
+
+
+(sente/start-chsk-router!
+ ch-chsk
+ (fn [e]
+   (when (= (:id e) :chsk/recv)
+     (let [[e-type raw-msg] (:?data e)]
+       (handle [e-type (read-transit raw-msg)])))))
 
 (defn home-page []
   [:div [:h2 "Welcome to ProB 2.0"]
