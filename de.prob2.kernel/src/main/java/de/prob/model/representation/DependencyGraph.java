30b6d49254bd75c80655e6a19f973de90e2784e7
Joy Clark <clark.joy@gmail.com>
2015-03-10 17:54:34 +0100

    PROBCORE-767 made the DependencyGraph usable from Clojure

--- de.prob2.kernel/src/main/java/de/prob/model/representation/DependencyGraph.java
+++ de.prob2.kernel/src/main/java/de/prob/model/representation/DependencyGraph.java
@@ -10,142 +10,159 @@ import java.util.Set;
 
 import com.google.common.base.Objects;
 
-
 /**
- * A simple graph implementation intended to display the relationships between the components
- * in a model. The nodes in the graph are represented as the string element name of the component
- * and the edges are the type of relationship as defined by {@link ERefType}. It is a multigraph,
- * which means that there can be multiple edges between two nodes.
+ * A simple graph implementation intended to display the relationships between
+ * the components in a model. The nodes in the graph are represented as the
+ * string element name of the component and the edges are the type of
+ * relationship as defined by {@link ERefType}. It is a multigraph, which means
+ * that there can be multiple edges between two nodes.
  * 
  * @author joy
- *
+ * 
  */
 public class DependencyGraph {
-	/** 
+	/**
 	 * RefType is used for both ClassicalBModels and EventBModels
 	 * 
 	 * ClassicalB: SEES, USES, REFINES, INCLUDES, IMPORTS EventB: SEES, REFINES,
 	 * EXTENDS
 	 * 
 	 * @author joy
-	 *
+	 * 
 	 */
 	public enum ERefType {
 		SEES, USES, REFINES, INCLUDES, IMPORTS, EXTENDS
 	}
-	
-	private class Node {
+
+	public class Node {
 		final String elementName;
 		List<Edge> outEdges = new ArrayList<Edge>();
-		
-		public Node(String elementName) {
+
+		public Node(final String elementName) {
 			this.elementName = elementName;
 		}
-		
+
 		public String getElementName() {
 			return elementName;
 		}
-		
+
 		public List<Edge> getOutEdges() {
 			return outEdges;
 		}
-		
+
 		/**
-		 * @param edge to be added.
+		 * @param edge
+		 *            to be added.
 		 */
-		public void addEdge(Edge edge) {
+		public void addEdge(final Edge edge) {
 			outEdges.add(edge);
 		}
-		
+
 		@Override
-		public boolean equals(Object that) {
+		public boolean equals(final Object that) {
 			if (this == that) {
 				return true;
 			}
 			if (that instanceof Node) {
-				return this.getElementName().equals(((Node) that).getElementName());
+				return getElementName().equals(((Node) that).getElementName());
 			}
 			return false;
- 		}
-		
+		}
+
 		@Override
 		public int hashCode() {
-			return this.getElementName().hashCode();
+			return getElementName().hashCode();
 		}
 	}
-	
-	private class Edge {
+
+	public class Edge {
 		Node from;
 		Node to;
 		ERefType relationship;
-		
-		public Edge(Node from, Node to, ERefType relationship) {
+
+		public Edge(final Node from, final Node to, final ERefType relationship) {
 			this.from = from;
 			this.to = to;
 			this.relationship = relationship;
 		}
-		
+
 		public Node getFrom() {
 			return from;
 		}
-		
+
 		public Node getTo() {
 			return to;
 		}
-		
+
 		public ERefType getRelationship() {
 			return relationship;
 		}
-		
+
 		@Override
-		public boolean equals(Object that) {
+		public boolean equals(final Object that) {
 			if (this == that) {
 				return true;
 			}
 			if (that instanceof Edge) {
-				return this.getFrom().equals(((Edge) that).getFrom()) &&
-						this.getTo().equals(((Edge) that).getTo()) &&
-						this.getRelationship().equals(((Edge) that).getRelationship());
+				return getFrom().equals(((Edge) that).getFrom())
+						&& getTo().equals(((Edge) that).getTo())
+						&& getRelationship().equals(
+								((Edge) that).getRelationship());
 			}
 			return false;
 		}
-		
+
 		@Override
 		public int hashCode() {
 			return Objects.hashCode(from, to, relationship);
 		}
 	}
-	
+
 	Map<String, Node> graph = new HashMap<String, DependencyGraph.Node>();
-	
+
 	/**
-	 * @param element element to be added to the graph it is has not already been added;
+	 * @param element
+	 *            element to be added to the graph it is has not already been
+	 *            added;
 	 */
-	public void addVertex(String element) {
+	public void addVertex(final String element) {
 		if (!graph.containsKey(element)) {
 			graph.put(element, new Node(element));
 		}
 	}
-	
+
 	/**
-	 * @param element to be found in the graph
+	 * @param element
+	 *            to be found in the graph
 	 * @return whether or not the element has been added to the graph
 	 */
-	public boolean containsVertex(String element) {
+	public boolean containsVertex(final String element) {
 		return graph.containsKey(element);
 	}
-	
+
 	public Set<String> getVertices() {
 		return new HashSet<String>(graph.keySet());
 	}
-	
+
+	public Set<Edge> getEdges() {
+		HashSet<Edge> set = new HashSet<Edge>();
+		for (Node node : graph.values()) {
+			set.addAll(node.getOutEdges());
+		}
+		return set;
+	}
+
 	/**
 	 * 
-	 * @param from source element
-	 * @param to destination element
-	 * @param relationship relationship between the two elements
+	 * @param from
+	 *            source element
+	 * @param to
+	 *            destination element
+	 * @param relationship
+	 *            relationship between the two elements
 	 */
-	public void addEdge(String from, String to, ERefType relationship) {
+	public void addEdge(final String from, final String to,
+			final ERefType relationship) {
 		if (!graph.containsKey(from)) {
 			addVertex(from);
 		}
@@ -157,22 +174,26 @@ public class DependencyGraph {
 		Edge e = new Edge(f, t, relationship);
 		f.addEdge(e);
 	}
-	
+
 	/**
-	 * @param from source node
-	 * @param to destination node
+	 * @param from
+	 *            source node
+	 * @param to
+	 *            destination node
 	 * @return the list of relationships between the two elements.
 	 */
-	public List<ERefType> getRelationships(String from, String to) {
+	public List<ERefType> getRelationships(final String from, final String to) {
 		if (!graph.containsKey(from)) {
-			throw new IllegalArgumentException("Element "+from+" is not in graph.");
+			throw new IllegalArgumentException("Element " + from
+					+ " is not in graph.");
 		}
 		if (!graph.containsKey(to)) {
-			throw new IllegalArgumentException("Element "+to+" is not in graph.");
+			throw new IllegalArgumentException("Element " + to
+					+ " is not in graph.");
 		}
 		Node f = graph.get(from);
 		Node t = graph.get(to);
-		
+
 		List<Edge> edgeSet = f.getOutEdges();
 		List<ERefType> relationships = new ArrayList<DependencyGraph.ERefType>();
 		for (Edge edge : edgeSet) {
@@ -180,27 +201,29 @@ public class DependencyGraph {
 				relationships.add(edge.getRelationship());
 			}
 		}
-		
+
 		return relationships;
 	}
-	
+
 	@Override
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
 		sb.append("(");
 		sb.append(graph.keySet().toString());
 		sb.append(", ");
-		
+
 		List<String> s = new ArrayList<String>();
 		for (Entry<String, Node> entry : graph.entrySet()) {
 			List<Edge> outEdges = entry.getValue().getOutEdges();
 			for (Edge edge : outEdges) {
-				s.add(edge.getRelationship().toString() + "=(" + edge.getTo().getElementName() + "," + edge.getFrom().getElementName() + ")");
+				s.add(edge.getRelationship().toString() + "=("
+						+ edge.getTo().getElementName() + ","
+						+ edge.getFrom().getElementName() + ")");
 			}
 		}
 		sb.append(s.toString());
 		sb.append(")");
 		return sb.toString();
 	}
-	
+
 }
