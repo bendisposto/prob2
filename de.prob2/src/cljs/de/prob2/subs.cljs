64b837eeef55e913ab8b37e0788cbc6548d9647b
Jens Bendisposto <jens@bendisposto.de>
2015-03-15 17:27:46 +0100

    more work on the js interface

--- de.prob2/src/cljs/de/prob2/subs.cljs
+++ de.prob2/src/cljs/de/prob2/subs.cljs
@@ -46,20 +46,27 @@
    (reaction (get-in @db [:states state-spec]))))
 
 
-(defn ^extern subs-js-handler
+(defn ^:extern js-subs-handler
   "Forwards changes to a subscription to a handler. First argument
   is the handler id that should be triggered. The second argument
   is a subscription pattern. All following arguments are added to
   the handler dispatch call. All arguments are preprocessed by the
-  Clojurescript reader. The handler is called with the complete
-  subscription specification and the changes. Registration of the
-  handler must be done separately.
+  Clojurescript reader. The handler is called with the changed
+  subscription. The arguments are also passed into this funtion.
+  The registration of the handler must be done separately.
 
   For instance: We have a handler registered that uses the key :foo
   and we want to trigger :foo each time the trace with id
-  09b2fdfa-f49b-4c5f-be64-ea5e63f0d628 changes. 
+  09b2fdfa-f49b-4c5f-be64-ea5e63f0d628 changes.
 
   de.prob2.subs.subs_js_handler(':foo', '[:trace #uuid \"09b2fdfa-f49b-4c5f-be64-ea5e63f0d628\"])
+
+  A matching handler could be registered with
+  de.prob2.subs.js_register_handler(
+  ':foo',
+  function(x,db) { console.log('changed',x,
+  'fulltree',db,
+  'args',arguments);})
 
   Important: A handler should not take longer than 16ms to run. Long
   running handlers are supposed to split up the work into chunks and
@@ -69,28 +76,33 @@
   [handler hook & args]
   (let [real-handler (cljs.reader/read-string handler)
         real-hook (cljs.reader/read-string hook)
-        real-args (map cljs.reader/read-string args)
         x (rf/subscribe real-hook)]
     (ra/run! (rf/dispatch
               (into [real-handler
-                     (clj->js real-hook)
                      (clj->js @x)]
-                    (clj->js real-args))))))
+                    args)))))
 
 
-(comment  (defn ^extern subscribe-js-callback
-  "Subscribes a Javascript callback function to a normal subscription
-  hook. First argument is a JS function of one argument that should
-  be called each time the subscription changes. The second argument is
-  a Clojurescript subscription encoded as a String. i.e. it is read
-  using the Clojurescript reader.
+(defn ^:extern js-register-handler
+  "Registers a handler function. The first argument is the handler key,
+  the second argument is a function of at least two arguments.
+  The first argument is the changed part of the state. The second argument
+  is the full state. Additional arguments are those that were passed from
+  the subscription.
 
-  Example:
-  de.prob2.subs.register_js_callback(function(x) { console.log(\"changed\" x},'[:trace #uuid \"09b2fdfa-f49b-4c5f-be64-ea5e63f0d628\"]')
+  Important: A handler should not take longer than 16ms to run. Long
+  running handlers are supposed to split up the work into chunks and
+  call themselfs after 16 ms. Information for splitting up the work
+  can be encoded in the optional arguments.
+  "
+  [handler f]
+  (let [real-handler (cljs.reader/read-string handler)]
+    (rf/register-handler
+     real-handler
+     (fn [db [_ x & args]]
+       (let [jsdb (clj->js db)
+             jsx (clj->js x)
+             res (apply f jsx jsdb args)]
+         db)))))
 
-  Warning:
-  Be careful what you put into the callback. The UI can become unresponsive if the callback is taking too long."
 
-  [callback hook]
-  (let [x (rf/subscribe (cljs.reader/read-string hook))]
-    (ra/run! (callback (clj->js @x))))))
