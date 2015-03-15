d1c8f467f2b0f55a8760b6569f10de46dcc24e19
Jens Bendisposto <jens@bendisposto.de>
2015-03-15 09:26:08 +0100

    added subsciption function for external tree listeners

--- de.prob2/src/cljs/de/prob2/subs.cljs
+++ de.prob2/src/cljs/de/prob2/subs.cljs
@@ -1,5 +1,5 @@
 (ns de.prob2.subs
-  (:require-macros [reagent.ratom :refer [reaction]])
+  (:require-macros [reagent.ratom :as ra :refer [reaction]])
   (:require [re-frame.core :as rf :refer [register-sub]]
             [taoensso.encore :as enc  :refer (logf log logp)]))
 
@@ -17,7 +17,7 @@
 (register-sub
  :connected?
  (fn [db]
-  (reaction (:connected? @db))))
+   (reaction (:connected? @db))))
 
 (register-sub
  :traces
@@ -32,8 +32,8 @@
 (register-sub
  :model
  (fn [db [_ trace-id]]
-    (let [model-id (reaction (get-in @db [:traces trace-id :model]))]
-      (reaction (get-in @db [:models @model-id])))))
+   (let [model-id (reaction (get-in @db [:traces trace-id :model]))]
+     (reaction (get-in @db [:models @model-id])))))
 
 (register-sub
  :trace
@@ -44,3 +44,18 @@
  :state
  (fn [db [_ state-spec]]
    (reaction (get-in @db [:states state-spec]))))
+
+
+(defn ^extern subscribe-js-callback
+  "Subscribes a Javascript callback function to a normal subscription
+  hook. First argument is a JS function of one argument that should
+  be called each time the subscription changes. The second argument is
+  a Clojurescript subscription encoded as a String. i.e. it is read
+  using the Clojurescript reader.
+
+  Example:
+  de.prob2.subs.register_js_callback(function(x) { console.log(\"changed\" x},'[:trace #uuid \"09b2fdfa-f49b-4c5f-be64-ea5e63f0d628\"]')"
+
+  [callback hook]
+  (let [x (rf/subscribe (cljs.reader/read-string hook))]
+    (ra/run! (callback (clj->js @x)))))
