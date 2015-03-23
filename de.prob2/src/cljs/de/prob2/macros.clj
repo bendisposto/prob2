abb00386d8b102b1d739212ad1889bfda54cc5df
Jens Bendisposto <jens@bendisposto.de>
2015-03-23 06:57:17 +0100

    PROBCORE-740 use full namespace

--- de.prob2/src/cljs/de/prob2/macros.clj
+++ de.prob2/src/cljs/de/prob2/macros.clj
@@ -1,8 +1,8 @@
 (ns de.prob2.macros)
 
 (defmacro remote-let [[v [m & args]] & body]
-      `(~'go (let [c# (async/chan)
-      	          x# (rf/dispatch [:prob2/call c# ~(str m) ~@args])
-      	          ~v (async/<! c#)]
-             ~@body)))
-
+  `(cljs.core.async.macro/go
+     (let [c# (cljs.core.async/chan)
+           x# (re-frame.core/dispatch [:prob2/call c# ~(str m) ~@args])
+           ~v (cljs.core.async/<! c#)]
+       ~@body)))
