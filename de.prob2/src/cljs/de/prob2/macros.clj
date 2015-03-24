8fc7936abf345ff22273e9db2d3e1bd395481359
Jens Bendisposto <jens@bendisposto.de>
2015-03-24 06:32:48 +0100

    PROBCORE-740 fixed macro docstring + send command as a String

--- de.prob2/src/cljs/de/prob2/macros.clj
+++ de.prob2/src/cljs/de/prob2/macros.clj
@@ -2,12 +2,14 @@
   (require [cljs.core.async.macros :refer [go]]))
 
 (defmacro remote-let
-  "remote-let fetches a value from the server. It is used like this:
+  "remote-let calls a Groovy function on the server and fetches the
+   result value. It is used like this:
 
     (remote-let [res (foo x y z)]
         (bar res))
 
-   It assumes that foo is a funtion that is in scop on the server side. 
+   It assumes that foo is a function that has been registered in the 
+   UIFunctionregistry on the server side. 
    The code is executed asynchronously, i.e., foo can be a function that 
    takes some time. The body of the remote-let is executed once res 
    becomes available, but the remote-let itself immediately terminates.
@@ -16,6 +18,6 @@
   [[v [command & args]] & body]
   `(go
      (let [c# (cljs.core.async/chan)
-           x# (re-frame.core/dispatch [:prob2/call c# identity ~command ~args])
+           x# (re-frame.core/dispatch [:prob2/call c# identity ~(str command) ~args])
            ~v (cljs.core.async/<! c#)]
        ~@body)))  
