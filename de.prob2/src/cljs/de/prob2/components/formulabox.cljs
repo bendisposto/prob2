f7d2143a59e652b9801defe4161d3cef852a484f
Jens Bendisposto <jens@bendisposto.de>
2015-03-29 10:38:03 +0200

    improved parse function using remote-let

--- de.prob2/src/cljs/de/prob2/components/formulabox.cljs
+++ de.prob2/src/cljs/de/prob2/components/formulabox.cljs
@@ -1,5 +1,6 @@
 (ns de.prob2.components.formulabox
-  (:require-macros [cljs.core.async.macros :as m])
+  (:require-macros [cljs.core.async.macros :as ma]
+                   [de.prob2.macros :as m])
   (:require [cljs.core.async :as a]
             [reagent.core :as r]
             [de.prob2.helpers :as h]
@@ -7,20 +8,15 @@
 
 
 (defn parse [trace-id formula ratom]
-  (h/remote-call
-   (fn [res]
-     (let [cls ({true "formula-ok" false "has-error"} res)]
-       (swap! ratom (fn [x] (logp :old x) cls))))
-   "parse"
-   trace-id
-   formula))
-
+  (m/remote-let [res (parse trace-id formula)]
+       (reset! ratom ({true "formula-ok" false "has-error"} res))))
+ 
 (defn formulabox
   ([trace-id] (formulabox trace-id (gensym) nil nil))
   ([trace-id id bfor aftr]
    (let [error (r/atom "formula-ok")
          c (a/chan)]
-     (m/go-loop [formula "" last-formula ""]
+     (ma/go-loop [formula "" last-formula ""]
        (let [t (a/timeout 500)
              [v port] (a/alts! [c t])]
          (if (= port c)
@@ -36,5 +32,5 @@
         (if bfor bfor "")
         [:input {:id id :class "form-control" :on-change
                  (fn [e] (let [v (-> e .-target .-value)]
-                          (m/go (a/>! c v))))}]
+                          (ma/go (a/>! c v))))}]
         (if aftr aftr "")]))))
