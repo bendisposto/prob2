e39a250dea1c35956d991007c046dce6854eebda
Jens Bendisposto <jens@bendisposto.de>
2015-03-30 11:40:31 +0200

    improved formulabox to support unicode replacing

--- de.prob2.kernel/src/main/groovy/de/prob/scripting/UiFunctionRegistry.groovy
+++ de.prob2.kernel/src/main/groovy/de/prob/scripting/UiFunctionRegistry.groovy
@@ -4,6 +4,7 @@ import com.google.inject.Inject
 import com.google.inject.Singleton
 
 import de.prob.statespace.Animations
+import de.prob.unicode.UnicodeTranslator;
 
 @Singleton
 class UiFunctionRegistry {
@@ -14,7 +15,10 @@ class UiFunctionRegistry {
 	def UiFunctionRegistry(Animations animations) {
 		this.animations = animations;
 		addFunction("echo", {a -> "echo:"+a});
-		addFunction("parse", {traceId, formula-> animations.getTrace(traceId).model.checkSyntax(formula);})
+		addFunction("parse", {traceId, formula-> 
+			["status":animations.getTrace(traceId).model.checkSyntax(formula), 
+			 "unicode":UnicodeTranslator.toUnicode(formula), 
+			 "ascii":UnicodeTranslator.toAscii(formula)];})
 	}
 
 	def Map<String,Closure> registry = [:];
--- de.prob2/src/cljs/de/prob2/components/formulabox.cljs
+++ de.prob2/src/cljs/de/prob2/components/formulabox.cljs
@@ -3,34 +3,53 @@
                    [de.prob2.macros :as m])
   (:require [cljs.core.async :as a]
             [reagent.core :as r]
+            [re-frame.core :as rf]
+            [cljs-uuid.core :as uuid]
             [de.prob2.helpers :as h]
             [taoensso.encore :as enc  :refer (logf log logp)]))
 
 
-(defn parse [trace-id formula ratom]
-  (m/remote-let [res (parse trace-id formula)]
-       (reset! ratom ({true "formula-ok" false "has-error"} res))))
- 
+(defn parse [trace-id ratom]
+  (m/remote-let [res (parse trace-id (:ascii @ratom))]
+                (swap! ratom (fn [s]
+                               (let [{status "status" unicode "unicode" ascii "ascii"} res]
+                                 (if-not (= ascii (:ascii s))
+                                   (assoc s :unicode nil)
+                                   (assoc s
+                                          :status ({true "" false "has-error"} status)
+                                          :unicode unicode)))))))
+
 (defn formulabox
   ([trace-id] (formulabox trace-id (gensym) nil nil))
   ([trace-id id bfor aftr]
-   (let [error (r/atom "formula-ok")
+   (let [ratom (r/atom {:status "" :ascii "" :unicode ""})
          c (a/chan)]
      (ma/go-loop [formula "" last-formula ""]
        (let [t (a/timeout 500)
-             [v port] (a/alts! [c t])]
+             [v port] (a/alts! [c t] {:priority true})]
          (if (= port c)
            (do (a/close! t)
+               (swap! ratom assoc :ascii v :unicode nil)
                (recur v last-formula))
            (do (when-not (= formula last-formula)
                  (if (empty? formula)
-                   (reset! error "formula-ok")
-                   (parse trace-id formula error)))
+                   (reset! ratom {:status "" :ascii "" :unicode ""})
+                   (do (parse trace-id ratom) (logp @ratom))))
                (recur formula formula)))))
      (fn []
-       [:div {:class (str "form-group " @error)}
-        (if bfor bfor "")
-        [:input {:id id :class "form-control" :on-change
-                 (fn [e] (let [v (-> e .-target .-value)]
-                          (ma/go (a/>! c v))))}]
-        (if aftr aftr "")]))))
+       (let [s (:status @ratom)
+             f0 (:unicode @ratom)
+             f1 (get @ratom :ascii "")
+             formula (if f0 f0 f1)]
+         [:div
+          [:div {:class (str "form-group " s)}
+           (if bfor bfor "")
+           [:input {:id id
+                    :class "form-control"
+                    :value formula
+                                        ;  :on-key-up (fn [e] (log e) (logp (.-keyCode e)))
+                    :on-change
+                    (fn [e] (let [v (-> e .-target .-value)]
+                                        ;(swap! ratom assoc :ascii v :unicode nil)
+                             (ma/go (a/>! c v))))}]
+           (if aftr aftr "")]])))))
