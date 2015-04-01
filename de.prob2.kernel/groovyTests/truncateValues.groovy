38839cc38468ffd33db972ab51837ee6d159a6e9
Joy Clark <joyclark2004@msn.com>
2015-04-01 10:38:29 +0200

    fixed broken integration test

--- de.prob2.kernel/groovyTests/truncateValues.groovy
+++ de.prob2.kernel/groovyTests/truncateValues.groovy
@@ -7,12 +7,12 @@ s = m as StateSpace
 t = (m as Trace).$initialise_machine()
 t = t.Crazy2("p1 = 5", "p2 = {2,4,6,8}")
 trans = t.getCurrentTransition()
-assert trans.evaluate(FormulaExpand.truncate).getRep() == "4,{(5|->#4:{2,4,...,6,8})},6 <-- Crazy2(5,#4:{2,4,...,6,8})"
+assert trans.evaluate(FormulaExpand.truncate).getRep() == "4,{(5|->#4:{2,...,8})},6 <-- Crazy2(5,#4:{2,...,8})"
 assert trans.evaluate(FormulaExpand.expand).getRep() == "4,{(5|->{2,4,6,8})},6 <-- Crazy2(5,{2,4,6,8})"
 assert trans.evaluate(FormulaExpand.truncate).getRep() == "4,{(5|->{2,4,6,8})},6 <-- Crazy2(5,{2,4,6,8})"
 
 truncated = new ClassicalB("{2,4,6,8,10}")
-assert t.evalCurrent(truncated).getValue() == "#5:{2,4,...,8,10}"
+assert t.evalCurrent(truncated).getValue() == "#5:{2,...,10}"
 expanded = new ClassicalB("{2,4,6,8,10}",FormulaExpand.expand)
 assert t.evalCurrent(expanded).getValue() == "{2,4,6,8,10}"
 
