1816444938b187959c9aaa9b4eb52b6d37e2e8d9
Joy Clark <joyclark2004@msn.com>
2015-04-10 21:41:09 +0200

    compiles. Finished tokenize method, and all ascii symbols should be implemented...

--- de.prob2/src/clj/de/prob2/unicodetranslator.clj
+++ de.prob2/src/clj/de/prob2/unicodetranslator.clj
@@ -4,8 +4,8 @@
 #_(defmulti lex [s] (first s))
 
 #_(defmethod lex \: [s] (cond
-						(.startsWith s ":!") :bcmsuch
-            (.startsWith s ":=") :bcmeq
+						(starts-with? s ":!") :bcmsuch
+            (starts-with? s ":=") :bcmeq
             :else                :in))
 
 (def tokens
@@ -84,12 +84,25 @@
 (defn tokenize [s]
   (let [sp (string/split s #"\s+")]))
 
+(defn starts-with? 
+    "tests a sequence and a prefix to see if the sequence starts with the prefix"
+    [l prefix]
+    (if (> (count prefix) (count l)) 
+        false
+        (=  (take (count prefix) l) (seq prefix))))
+
 (defn extract-identifier [s])
-(defn extract-other [s])
+(def whitespace #{\newline \space \tab \formfeed \backspace \return})
+
+(defn match? 
+    "checks to see if the pattern is a prefix to the sequence. However, after the sequence there needs to be either a whitespace or the end"
+    [s pattern]
+    (let [p      (seq pattern)
+          pcount (count p)]
+        (when (starts-with? s p) (or (= (count s) pcount) (whitespace (nth s pcount))))))
 
 (defn handle-letters [s pattern token]
-    (let [p-with  (re-pattern (str "^" pattern "\\s?"))]
-        (if (re-matches p-with s) [token (drop (len pattern) s)] (extract-identifier s))))
+    (if (match? s pattern) [token (drop (count pattern) s)] (extract-identifier s)))
 
 (defmulti lex first)
 (defmethod lex \! [s] [:forall (rest s)])
@@ -99,66 +112,93 @@
 (defmethod lex \& [s] [:land   (rest s)])
 (defmethod lex \f [s] (handle-letters s "false" :false))
 (defmethod lex \I [s] (cond
-                        (re-matches #"^INTER\s?" s) [:inter (drop 5 s] 
-                        (re-matches #"^INT\s?" s)   [:intg  (drop 3 s)]
+                        (match? s "INTER") [:inter (drop 5 s)] 
+                        (match? s "INT")   [:intg  (drop 3 s)]
                         :else                   (extract-identifier s)))
 (defmethod lex \* [s] (cond
-                        (.startsWith s "**")    [:cprod (drop 2 s)]
+                        (starts-with? s "**")    [:cprod (drop 2 s)]
                         "*"                     [:mult  (rest s)]))
 (defmethod lex \+ [s] (cond 
-                        (.startsWith s "+->")   [:pfun  (drop 3 s)]
-                        (.startsWith s "+>>")   [:psur  (drop 3 s)]
-                        :else                   :fail))
+                        (starts-with? s "+->")   [:pfun  (drop 3 s)]
+                        (starts-with? s "+>>")   [:psur  (drop 3 s)]
+                        :else                    [\+     (rest s)]))
 (defmethod lex \- [s] (cond 
-                        (.startsWith s "-->")   [:tfun  (drop 3 s)]
-                        (.startsWith s "->>")   [:tsur  (drop 3 s)]
+                        (starts-with? s "-->")   [:tfun  (drop 3 s)]
+                        (starts-with? s "->>")   [:tsur  (drop 3 s)]
                         "-"                     [:minus (rest s)]))
 (defmethod lex \. [s] (cond
-                        (.startsWith s "..")    [:dotdot (drop 2 s)]
+                        (starts-with? s "..")    [:dotdot (drop 2 s)]
                         "."                     [:qdot  (rest s)]))
 (defmethod lex \N [s] (cond 
-                        (re-matches #"^NAT1\s?" s) [:nat1  (drop 4 s)]
-                        (re-matches #"^NAT\s?" s) [:nat (drop 3 s)]))
+                        (match? s "NAT1") [:nat1  (drop 4 s)]
+                        (match? s "NAT")  [:nat (drop 3 s)]
+                        :else             (extract-identifier s)))
 (defmethod lex \n [s] (handle-letters s "not" :lnot))
 (defmethod lex \/ [s] (cond
-                        (.startsWith s "/<:")   [:notsubseteq  (drop 3 s)]
-                        (.startsWith s "/<<:")  [:notsubset  (drop 4 s)]
-                        (.startsWith s "/\\")   [:binter  (drop 2 s)]
-                        (.startsWith s "/:")    [:notin   (drop 2 s)]
-                        (.startsWith s "/=")    [:neq     (drop 2 s)]
-                        (.startsWith s "/|\\")  [:take    (drop 3 s)]
-                        "/"                     [:div     (rest s)]))
+                        (starts-with? s "/<:")   [:notsubseteq  (drop 3 s)]
+                        (starts-with? s "/<<:")  [:notsubset  (drop 4 s)]
+                        (starts-with? s "/\\")   [:binter  (drop 2 s)]
+                        (starts-with? s "/:")    [:notin   (drop 2 s)]
+                        (starts-with? s "/=")    [:neq     (drop 2 s)]
+                        (starts-with? s "/|\\")  [:take    (drop 3 s)]
+                        "/"                      [:div     (rest s)]))
 (defmethod lex \o [s] (cond
-                        (re-matches #"^oftype\s?" s)  [:oftype (drop 6 s)]
-                        (re-matches #"^or\s?")        [:lor    (drop 2 s)]
+                        (match? s "oftype")  [:oftype (drop 6 s)]
+                        (match? s "or")        [:lor    (drop 2 s)]
                         :else                   (extract-identifier s)))
 (defmethod lex \P [s] (cond
-                        (re-matches #"^POW1\s?" s)    [:pow1   (drop 4 s)]
-                        (re-matches #"^POW\s?"  s)    [:pow    (drop 3 s)]))
+                        (match? s "POW1")    [:pow1   (drop 4 s)]
+                        (match? s "POW")    [:pow    (drop 3 s)]
+                        :else               (extract-identifier s)))
 (defmethod lex \t [s] (handle-letters s "true" :btrue))
 (defmethod lex \U [s] (handle-letters s "UNION" :union))
 (defmethod lex \: [s] (cond
-                        (.startsWith s ":|")  [:bcmsuch  (drop 2 s)]
-                        (.startsWith s ":=")  [:bcmeq    (drop 2 s)]
-                        (.startsWith s "::")  [:bcmin    (drop 2 s)]
-                        ":"                   [:in       (rest s)])) 
+                        (starts-with? s ":|")  [:bcmsuch  (drop 2 s)]
+                        (starts-with? s ":=")  [:bcmeq    (drop 2 s)]
+                        (starts-with? s "::")  [:bcmin    (drop 2 s)]
+                        ":"                    [:in       (rest s)])) 
 (defmethod lex \{ [s] (cond
-                        (.startsWith s "{}")  [:emptyset (drop 2 s)]
-                        "{"                   (extract-other s)))
+                        (starts-with? s "{}")  [:emptyset (drop 2 s)]
+                        "{"                     [\{        (rest s)]))
 (defmethod lex \< [s] (cond
-                        (.startsWith s "<<->>") [:strel  (drop 5 s)]
-                        (.startsWith s "<->>")  [:srel   (drop 4 s)]
-                        (.startsWith s "<<->")  [:trel   (drop 4 s)]
-                        (.startsWith s "<->")   [:rel    (drop 3 s)]
-                        (.startsWith s "<=>")   [:leqv   (drop 3 s)]
-                        (.startsWith s "<<|")   [:domsub (drop 3 s)]
-                        (.startsWith s "<<:")   [:subset (drop 3 s)]
-                        (.startsWith s "<=")    [:leq    (drop 2 s)]
-                        (.startsWith s "<+")    [:ovl    (drop 2 s)]
-                        (.startsWith s "<|")    [:domres (drop 2 s)]
-                        (.startsWith s "<:")    [:subseteq (drop 2 s)]))
-    \\ [{:ascii "\\", :token :setminus, :unicode "∖"} {:ascii "\\/", :token :bunion, :unicode "∪"} {:ascii "\\fcomp", :token :fcomp, :unicode ";"} {:ascii "\\expn", :token :expn, :unicode "^"} {:ascii "\\mid", :token :mid, :unicode "∣"} {:ascii "\\|/", :token :drop, :unicode "\\|/"}],
-\| [{:ascii "|->", :token :mapsto, :unicode "↦"} {:ascii "|>", :token :ranres, :unicode "▷"} {:ascii "|>>", :token :ransub, :unicode "⩥"} {:ascii "||", :token :pprod, :unicode "∥"}],
-\= [{:ascii "=>", :token :limp, :unicode "⇒"}],
-\> [{:ascii "><", :token :dprod, :unicode "⊗"} {:ascii ">+>", :token :pinj, :unicode "⤔"} {:ascii ">->", :token :tinj, :unicode "↣"} {:ascii ">->>", :token :tbij, :unicode "⤖"} {:ascii ">=", :token :geq, :unicode "≥"}],
-\~ [{:ascii "~", :token :conv, :unicode "∼"}]}
+                        (starts-with? s "<<->>") [:strel  (drop 5 s)]
+                        (starts-with? s "<->>")  [:srel   (drop 4 s)]
+                        (starts-with? s "<<->")  [:trel   (drop 4 s)]
+                        (starts-with? s "<->")   [:rel    (drop 3 s)]
+                        (starts-with? s "<=>")   [:leqv   (drop 3 s)]
+                        (starts-with? s "<<|")   [:domsub (drop 3 s)]
+                        (starts-with? s "<<:")   [:subset (drop 3 s)]
+                        (starts-with? s "<=")    [:leq    (drop 2 s)]
+                        (starts-with? s "<+")    [:ovl    (drop 2 s)]
+                        (starts-with? s "<|")    [:domres (drop 2 s)]
+                        (starts-with? s "<:")    [:subseteq (drop 2 s)]
+                        \<                       [\<      (rest s)]))
+(defmethod lex \\ [s] (cond
+                        (starts-with? s "\\/")      [:bunion   (drop 2 s)]
+                        (starts-with? s "\\fcomp")  [:fcomp    (drop 5 s)]
+                        (starts-with? s "\\expn")   [:expn     (drop 5 s)]
+                        (starts-with? s "\\mid")    [:mid      (drop 4 s)]
+                        (starts-with? s "\\|/")     [:drop     (drop 3 s)]
+                        \\                          [:setminus (rest s)]))
+(defmethod lex \| [s] (cond
+                        (starts-with? s "|->")      [:mapsto   (drop 3 s)]
+                        (starts-with? s "|>>")      [:ransub   (drop 3 s)]
+                        (starts-with? s "||")       [:pprod    (drop 2 s)]
+                        (starts-with? s "|>")       [:ranres   (drop 2 s)]
+                        \|                          [\|        (rest s)]))
+(defmethod lex \= [s] (cond 
+                        (starts-with? s "=>")       [:limp  (drop 2 s)]
+                        "="                         [\=     (rest s)]))
+(defmethod lex \> [s] (cond
+                        (starts-with? s "><")       [:dprod (drop 2 s)]
+                        (starts-with? s ">+>")      [:pinj  (drop 3 s)]
+                        (starts-with? s ">->>")     [:tbij  (drop 4 s)]
+                        (starts-with? s ">->")      [:tinj  (drop 3 s)]
+                        (starts-with? s ">=")       [:geq   (drop 2 s)]
+                        \>                          [\>     (rest s)]))
+(defmethod lex \~ [s] [:conv  (rest s)])
+
+(defn tokenize ([s] (tokenize s [])
+    ([s tokens] (if (empty? s) (reverse tokens)
+                    (let [[token r] (lex s)]
+                        (recur r (cons t tokens)))))))
