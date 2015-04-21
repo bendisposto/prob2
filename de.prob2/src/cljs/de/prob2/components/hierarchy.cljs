b2934db1632a536f6d8352ad7862cb3052591215
Joy Clark <clark.joy@gmail.com>
2015-04-21 18:07:05 +0200

    PROBCORE-810

--- de.prob2/src/cljs/de/prob2/components/hierarchy.cljs
+++ de.prob2/src/cljs/de/prob2/components/hierarchy.cljs
@@ -4,64 +4,19 @@
             [reagent.core :as r]
             [taoensso.encore :as enc  :refer (logf log logp)]
             [re-frame.core :as rf]
-            [de.prob2.helpers :as h]))
+            [de.prob2.helpers :as h]
+            [de.prob2.dagre-helper :as dh]))
 
-
-
-(defn render-line [[k c]]
-  (let [e (map :label c)]
-    [[:tr {:style "background-color: lightblue;"} [:td (h/title-case k)]]
-     [:tr [:td (clojure.string/join " " e)]]]))
-
-(defn render-box [n c]
-  (let [kind (if (contains? c :events) "Machine" "Context")
-        c' (into {} (remove (fn [[k v]] (empty? v)) c))
-        elems (dissoc c' :invariants :name :variant :axioms)]
-    (hic/html
-     `[:div {:style "background-color: white;width:100px;top:-40px;right:-50px;position:relative;"}
-       [:table {:style "width:100px;" :border 1}
-        [:tr {:style "background-color: black; color: white"} [:td ~(str kind " " n)]]
-        ~@(mapcat render-line elems)
-        ]])))
-
-
-(rf/register-handler
- :hierarchy-update
- (fn [db [_ [dep-graph components] elem]]
-   (let [nodes (map (fn [e] {:data {:id e :label (render-box e (get components e))}}) (keys  components))
-         edges (map (fn [{:keys [from to type]}] {:data {:source from :target to :label (name type)}}) dep-graph)
-         config {:container elem
-                 :elements {:nodes nodes
-                            :edges edges}
-                 :renderer {:name "css"}
-                 :layout {:name "breadthfirst"}
-                 :style [{:selector "node"
-                          :css {:shape "rectangle"
-                                :width "100px"
-                                :text-valign "center"
-                                :text-halign "center"
-                                :height "100px"
-                                :background-color "white"
-                                :content "data(label)"}}
-                         {:selector "edge"
-                          :css {:content "data(label)"
-                                :target-arrow-shape "triangle"
-                                :target-arrow-color "black"
-                                :width "2px";
-                                :text-outline-color "white"
-                                :text-outline-opacity 1
-                                :text-outline-width 3
-                                :line-color "black"
-                                }}]}]
-     (js/cytoscape (clj->js config))
-     (log (clj->js config))
-     db)))
 
 (defn hierarchy-view [id]
-  (r/create-class
-   { :component-did-mount
-    (fn [c]
-      (let [elem (.getDOMNode c)]
-        (h/subs->handler :hierarchy-update [:hierarchy id] elem)))
-    :display-name  "hierarchy-view"
-    :reagent-render (fn [] [:div {:id "hierarchy-view"}])}))
+  (let [model (rf/subscribe [:model id])
+        dep-graph (:dependency-graph @model)
+        k       (keys (:components @model))
+     ;   _ (.log js/console (into [] (map (fn [x] {:element x}) k)))
+     ;   vertices (map (fn [e] {:name e :label e :width 100 :height 100}) k)
+        edges  (map (fn [edge] (let [t (:type edge)]
+                          (-> edge (dissoc :type)
+                              (assoc :label (name t))))) dep-graph)
+        ]
+    
+    [:div (dh/example)]))
--- de.prob2/src/cljs/de/prob2/dagre_helper.cljs
+++ de.prob2/src/cljs/de/prob2/dagre_helper.cljs
@@ -12,7 +12,7 @@
    (do (.setEdge g
                  (:from edge)
                  (:to edge)
-                 (clj->js (dissoc edge :from :to))) g)))
+                 (clj->js edge)) g)))
 
 (defn create-graph
   ([{:keys [nodes edges]}]
@@ -21,41 +21,58 @@
      (reduce add-edge g1 edges)))
 
   ([]
-   (let [graph (js/dagre.graphlib.Graph.)]
+   (let [graph (js/global.dagre.graphlib.Graph.)]
      (.setGraph graph #js{})
      graph)))
 
 
-(defn render [g] (do (.layout js/dagre g) g))
+(defn render [g] (do (.layout js/global.dagre g) g))
 
-(defn nodes [g] (map #(.node g %) (.nodes g)))
+(defn node-names [g] (.concat #js[] (.nodes g)))
+(defn nodes [g] (map #(.node g %) (.concat #js[] (.nodes g)))) ;; have
+;; to concat for some weird javascript reason
 
-(defn edges [g] (map #(.edge g %) (.edges g))) 
+(defn edge-names [g] (.concat #js[] (.edges g)))
+(defn edges [g] (map #(.edge g %) (.concat #js[] (.edges g)))) 
  
 (defn to-svg [g]
   [:svg {:width 500 :height 500}
-   (for [n (nodes g)]
-     (do  (log "n" n)
-          (let [node (js->clj n)]
+    (.log js/console (node-names g))
+   (for [n (node-names g)]
+     (let [node (js->clj (.node g n))]
+       (logp :n node)
             [:g [:rect {:width (or (node "width") 100)
-                        :height (or (node "height") 100)
+                        :height (or (node "height") 50)
                         :x (node "x")
                         :y (node "y")
                         :style {:fill :white :stroke :black}}]
-             [:text {:x (node "x") :y (node "y") :dx 30 :dy 25} (or (node "label") "")]])))
-   (for [e (edges g)]
-     (do (log "e"  e)
-       (let [edge (js->clj e)
-             start (first (edge "points"))
-             middle (second (edge "points"))
-             end   (last  (edge "points"))]
-         [:g [:line {:x1 (+ 50 (start "x")) :y1 (+ 25 (start "y"))
-                     :x2 (+ 50 (end   "x")) :y2 (+ 25 (end   "y"))
-                     :style {:stroke :black}}]
-          [:text {:x (middle "x") :y (middle "y") :dx 55 :dy 25} (or (edge "label") "")]])))])
+             [:text {:x (node "x") :y (node "y") :dy (if (node "height") (/ (node "height") 2) 25)
+                     :dx 5} n]]))
+   (for [e (edge-names g)]
+     (let [edge (js->clj (.edge g e))
+           _    (logp :edge edge)
+           start (first (edge "points"))
+           middle (second (edge "points"))
+           end   (last  (edge "points"))
+           _     (logp :sme start middle end)
+           start-node (js->clj (.node g (edge "from")))
+           w1         (start-node "width")
+           h1         (start-node "height")
+           end-node   (js->clj (.node g (edge "to")))
+           w2         (end-node "width")
+           h2         (end-node "height")]
+       (.log js/console start-node)
+       (.log js/console end-node)
+       [:g [:line {:x1 (+ (start "x") (/ w1 2)) :y1 (+ (start "y") (/ h1 2))
+-                  :x2 (+ (end   "x") (/ w2 2)) :y2 (+ (end   "y") (/ h2 2))
+-                  :style {:stroke :black}}]
+        [:text {:x (+ (middle "x") (/ w1 2)) :y (+  (middle "y") (/ h1 2)) } (or (edge "label") "")]]))])
 
 
 (defn example []
-  (let [g (-> (create-graph) (add-node {:name "me"}) (add-node (:name "you"))
+  (let [g (-> (create-graph)
+              (add-node {:name "me" :width 30 :height 24})
+              (add-node {:name "you" :width 30 :height 24})
               (add-edge {:from "me" :to "you" :label "us"}) (render))]
     (to-svg g)))
+
