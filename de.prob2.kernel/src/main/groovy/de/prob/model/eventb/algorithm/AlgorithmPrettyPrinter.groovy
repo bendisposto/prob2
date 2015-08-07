2be9ec555ccff27b647e06821834294538d192c0
joyclark <clark.joy@gmail.com>
2015-08-07 13:39:07 +0200

    PROBCORE-861 wrote a pretty printer for the algorithms

---
+++ de.prob2.kernel/src/main/groovy/de/prob/model/eventb/algorithm/AlgorithmPrettyPrinter.groovy
@@ -0,0 +1,58 @@
+package de.prob.model.eventb.algorithm
+
+import static de.prob.unicode.UnicodeTranslator.toUnicode
+
+class AlgorithmPrettyPrinter {
+
+	Block algorithm
+
+	def AlgorithmPrettyPrinter(Block algorithm) {
+		this.algorithm = algorithm
+	}
+
+	def String prettyPrint() {
+		StringBuilder sb = new StringBuilder()
+		printBlock(algorithm, sb, null)
+		sb.toString()
+	}
+
+	def printBlock(Block b, StringBuilder sb, String indent) {
+		def newindent = indent == null ? "" : "  ${indent}"
+		if (b.statements) {
+			b.statements.each {
+				printStatement(it, sb, newindent)
+			}
+		} else {
+			writeLine(sb, newindent,"// do nothing")
+		}
+	}
+
+	def printStatement(While statement, StringBuilder sb, String indent) {
+		writeLine(sb,indent,"while (${toUnicode(statement.condition)}):")
+		printBlock(statement.block, sb, indent)
+	}
+
+	def printStatement(Assignments statement, StringBuilder sb, String indent) {
+		writeLine(sb,indent,toUnicode(statement.assignments.iterator().join(" || ")))
+	}
+
+	def printStatement(If statement, StringBuilder sb, String indent) {
+		writeLine(sb,indent, "if (${toUnicode(statement.condition)}):")
+		printBlock(statement.Then, sb, indent)
+		if (statement.Else.statements) {
+			writeLine(sb,indent,"else:")
+			printBlock(statement.Else, sb, indent)
+		}
+	}
+
+	def printStatement(Assertion statement, StringBuilder sb, String indent) {
+		writeLine(sb,indent, "assert ${toUnicode(statement.assertion)}")
+	}
+
+	def writeLine(StringBuilder sb, String indent, String line) {
+		if (indent) sb.append(indent)
+		sb.append(line)
+		sb.append("\n")
+	}
+
+}
