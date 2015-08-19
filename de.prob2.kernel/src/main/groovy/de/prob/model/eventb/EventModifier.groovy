5da4a7a75a0163b6fa16849fa1603047371039c1
Joy Clark <joyclark2004@msn.com>
2015-08-19 18:20:24 +0200

    EventModifier should have a public constructor

--- de.prob2.kernel/src/main/groovy/de/prob/model/eventb/EventModifier.groovy
+++ de.prob2.kernel/src/main/groovy/de/prob/model/eventb/EventModifier.groovy
@@ -7,13 +7,13 @@ import de.prob.model.representation.Guard
 
 
 
-class EventModifier extends AbstractModifier {
+public class EventModifier extends AbstractModifier {
 	private final actctr
 	private final grdctr
 	def Event event
 	boolean initialisation
 
-	private EventModifier(Event event, boolean initialisation=false) {
+	public EventModifier(Event event, boolean initialisation=false) {
 		this.initialisation = initialisation
 		this.actctr = extractCounter("act",event.actions)
 		this.event = event
