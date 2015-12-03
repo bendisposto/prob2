fe40b7b8f4a2077fcaf80be8523c7fcb02b611f6
joyclark <clark.joy@gmail.com>
2015-12-03 17:15:08 +0100

    PROBCORE-819 shutdown all CLIs if the system exits with -1

--- de.prob2.kernel/src/main/java/de/prob/Main.java
+++ de.prob2.kernel/src/main/java/de/prob/Main.java
@@ -18,6 +18,7 @@ import com.google.inject.Inject;
 import com.google.inject.Injector;
 import com.google.inject.Stage;
 
+import de.prob.cli.ProBInstanceProvider;
 import de.prob.scripting.Downloader;
 import de.prob.scripting.FileHandler;
 
@@ -179,6 +180,7 @@ public class Main {
 
 			main.run(args);
 		} catch (Throwable e) {
+			getInjector().getInstance(ProBInstanceProvider.class).shutdownAll();
 			e.printStackTrace();
 			System.exit(-1);
 		}
