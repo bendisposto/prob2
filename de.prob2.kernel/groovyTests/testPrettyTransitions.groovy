dc982dff9d84dddd74e0a77f5984f596bb096b36
Jens Bendisposto <jens@bendisposto.de>
2016-04-24 13:15:29 +0200

    fixing some more tests

--- de.prob2.kernel/groovyTests/testPrettyTransitions.groovy
+++ de.prob2.kernel/groovyTests/testPrettyTransitions.groovy
@@ -10,7 +10,7 @@ t = t.up()
 
 list = t.getTransitionList(true)
 assert list.collect { it.getName() } == ["\$setup_constants", "\$initialise_machine","up"]
-assert list.collect { it.getRep() } == ["\$setup_constants({(L1|->L0),(L2|->L1),(L3|->L2)},{(L0|->L1),(L1|->L2),(L2|->L3)})","\$initialise_machine(L0)","up()"]
-assert list.collect { it.getPrettyRep() } == ["SETUP_CONSTANTS({(L1|->L0),(L2|->L1),(L3|->L2)},{(L0|->L1),(L1|->L2),(L2|->L3)})", "INITIALISATION(L0)", "up()"]
+assert list.collect { it.getRep() } == ["\$setup_constants()","\$initialise_machine()","up()"]
+assert list.collect { it.getPrettyRep() } == ["SETUP_CONSTANTS()", "INITIALISATION()", "up()"]
 
 "a Transition has a pretty represenation for internal ProB transition names"
