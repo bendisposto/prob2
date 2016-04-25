094c8fa319e6a702d057d15e2a0b4ba7f0d4846e
Jens Bendisposto <jens@bendisposto.de>
2016-04-25 07:56:11 +0200

    typo

--- de.prob2.kernel/groovyTests/testPrettyTransitions.groovy
+++ de.prob2.kernel/groovyTests/testPrettyTransitions.groovy
@@ -13,4 +13,4 @@ assert list.collect { it.getName() } == ["\$setup_constants", "\$initialise_mach
 assert list.collect { it.getRep() } == ["\$setup_constants()","\$initialise_machine()","up()"]
 assert list.collect { it.getPrettyRep() } == ["SETUP_CONSTANTS()", "INITIALISATION()", "up()"]
 
-"a Transition has a pretty represenation for internal ProB transition names"
+"a Transition has a pretty representation for internal ProB transition names"
