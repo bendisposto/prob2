b57bcd1734d3b6016f399bb3ade6f693c8653070
Jens Bendisposto <jens@bendisposto.de>
2016-08-22 15:24:42 +0200

    fixed toString in Assertion, now showing the formula

--- de.prob2.kernel/src/main/java/de/prob/model/classicalb/Assertion.java
+++ de.prob2.kernel/src/main/java/de/prob/model/classicalb/Assertion.java
@@ -4,6 +4,7 @@ import de.be4.classicalb.core.parser.node.Start;
 import de.prob.animator.domainobjects.ClassicalB;
 import de.prob.animator.domainobjects.IEvalElement;
 import de.prob.model.representation.AbstractTheoremElement;
+import de.prob.unicode.UnicodeTranslator;
 
 public class Assertion extends AbstractTheoremElement {
 
@@ -25,5 +26,10 @@ public class Assertion extends AbstractTheoremElement {
 	@Override
 	public boolean isTheorem() {
 		return true;
+	}
+	
+	@Override
+	public String toString() {
+		return UnicodeTranslator.toUnicode(predicate.getCode());
 	}
 }
