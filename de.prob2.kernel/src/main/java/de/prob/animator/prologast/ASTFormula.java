48940140e0061c1c59336b51c73652ec35806b66
miwer106 <michellewerth@hotmail.com>
2017-04-11 13:52:09 +0200

    Change method getFormula() to return ProBEvalElement

--- de.prob2.kernel/src/main/java/de/prob/animator/prologast/ASTFormula.java
+++ de.prob2.kernel/src/main/java/de/prob/animator/prologast/ASTFormula.java
@@ -1,9 +1,11 @@
 package de.prob.animator.prologast;
 
+import de.prob.animator.domainobjects.ProBEvalElement;
+import de.prob.prolog.term.PrologTerm;
 import java.util.List;
 
 public class ASTFormula extends PrologASTNode{
-    private String formula;
+    private PrologTerm formula;
 
     ASTFormula(){
         super();
@@ -13,11 +15,13 @@ public class ASTFormula extends PrologASTNode{
         super(left, right);
     }
 
-    public String getFormula(){
-        return formula;
+    public ProBEvalElement getFormula(){
+        PrologTerm term = this.formula.getArgument(1);
+        String prettyPrint = this.formula.getArgument(2).toString();
+        return new ProBEvalElement(term, prettyPrint);
     }
 
-    public void setFormula(String formula) {
+    public void setFormula(PrologTerm formula) {
         this.formula = formula;
     }
 
