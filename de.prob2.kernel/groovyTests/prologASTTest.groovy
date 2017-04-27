b1ee08ff636f563ebb423eab08180a65a142dbf8
miwer106 <michellewerth@hotmail.com>
2017-04-27 21:16:52 +0200

    Try to fix test

--- de.prob2.kernel/groovyTests/prologASTTest.groovy
+++ de.prob2.kernel/groovyTests/prologASTTest.groovy
@@ -1,30 +1,31 @@
-import de.prob.animator.prologast.PrologASTNode;
+import de.prob.animator.prologast.*
+import de.prob.animator.command.*
+import java.util.List
 
 s = api.b_load(dir+File.separator+"machines"+File.separator+"scheduler.mch")
 assert s != null
 
-c = new GetMachineStructureCommand();
+c = new GetMachineStructureCommand()
 s.execute(c);
 
-List<PrologASTNode> toTest = c.getPrologASTList();
+List<PrologASTNode> toTest = c.getPrologASTList()
 
-/* TODO fix me, I'm breaking the build
-assert "VARIABLES" == toTest.get(0).getName();
-assert true == toTest.get(0).isExpanded();
-assert false == toTest.get(0).getSubnodes().isEmpty();
-List<PrologASTNode> subnodesVar = toTest.get(0).getSubnodes();
-assert "\n[Formula] : formula(b(identifier(active),set(global('PID')),[nodeid(10)]),active)" == subnodesVar.get(0).toString();
+assert "VARIABLES" == toTest.get(0).getName()
+assert true == toTest.get(0).isExpanded()
+assert false == toTest.get(0).getSubnodes().isEmpty()
+List<PrologASTNode> subnodesVar = toTest.get(0).getSubnodes()
+assert "\n[Formula] : formula(b(identifier(active),set(global('PID')),[nodeid(10)]),active)" == subnodesVar.get(0).toString()
 
-assert "INVARIANTS" == toTest.get(1).getName();
-assert false == toTest.get(1).isExpanded();
-assert false == toTest.get(1).getSubnodes().isEmpty();
+assert "INVARIANTS" == toTest.get(1).getName()
+assert false == toTest.get(1).isExpanded()
+assert false == toTest.get(1).getSubnodes().isEmpty()
 
-assert "OPERATIONS" == toTest.get(2).getName();
-assert false == toTest.get(2).isExpanded();
-List<PrologASTNode> subnodesOp = toTest.get(2).getSubnodes();
-assert true == subnodesOp.get(0).isPropagated();
-assert "nr_ready" == subnodesOp.get(0).getName();
-assert true == subnodesOp.get(subnodesOp.size()-1).isPropagated();
-assert "swap" == subnodesOp.get(subnodesOp.size()-1).getName();
-*/
+assert "OPERATIONS" == toTest.get(2).getName()
+assert false == toTest.get(2).isExpanded()
+List<PrologASTNode> subnodesOp = toTest.get(2).getSubnodes()
+assert true == subnodesOp.get(0).isPropagated()
+assert "nr_ready" == subnodesOp.get(0).getName()
+assert true == subnodesOp.get(subnodesOp.size()-1).isPropagated()
+assert "swap" == subnodesOp.get(subnodesOp.size()-1).getName()
+
 "PrologAST is built properly"
