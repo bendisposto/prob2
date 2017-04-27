555d451b06db31fa40a40925fad57be95c9ab9a3
miwer106 <michellewerth@hotmail.com>
2017-04-27 21:25:21 +0200

    Try to fix prologASTTest

--- de.prob2.kernel/groovyTests/prologASTTest.groovy
+++ de.prob2.kernel/groovyTests/prologASTTest.groovy
@@ -10,21 +10,23 @@ s.execute(c);
 
 List<PrologASTNode> toTest = c.getPrologASTList()
 
-assert "VARIABLES" == toTest.get(0).getName()
-assert true == toTest.get(0).isExpanded()
-assert false == toTest.get(0).getSubnodes().isEmpty()
-List<PrologASTNode> subnodesVar = toTest.get(0).getSubnodes()
+assert "SETS" == toTest.get(0).getName()
+
+assert "VARIABLES" == toTest.get(1).getName()
+assert true == toTest.get(1).isExpanded()
+assert false == toTest.get(1).getSubnodes().isEmpty()
+List<PrologASTNode> subnodesVar = toTest.get(1).getSubnodes()
 assert "\n[Formula] : formula(b(identifier(active),set(global('PID')),[nodeid(10)]),active)" == subnodesVar.get(0).toString()
 
-assert "INVARIANTS" == toTest.get(1).getName()
-assert false == toTest.get(1).isExpanded()
-assert false == toTest.get(1).getSubnodes().isEmpty()
-
-assert "OPERATIONS" == toTest.get(2).getName()
+assert "INVARIANTS" == toTest.get(2).getName()
 assert false == toTest.get(2).isExpanded()
-List<PrologASTNode> subnodesOp = toTest.get(2).getSubnodes()
-assert true == subnodesOp.get(0).isPropagated()
-assert "nr_ready" == subnodesOp.get(0).getName()
+assert false == toTest.get(2).getSubnodes().isEmpty()
+
+assert "OPERATIONS" == toTest.get(3).getName()
+assert false == toTest.get(3).isExpanded()
+List<PrologASTNode> subnodesOp = toTest.get(3).getSubnodes()
+assert true == subnodesOp.get(3).isPropagated()
+assert "nr_ready" == subnodesOp.get(3).getName()
 assert true == subnodesOp.get(subnodesOp.size()-1).isPropagated()
 assert "swap" == subnodesOp.get(subnodesOp.size()-1).getName()
 
