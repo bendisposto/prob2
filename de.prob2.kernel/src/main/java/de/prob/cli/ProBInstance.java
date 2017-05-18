57fd82d8fa2c6555547ec7ec6b6f7ba563ac770e
dgelessus <dgelessus@users.noreply.github.com>
2017-05-18 14:04:26 +0200

    Add hash code to ProBInstance output logger thread name
    This allows telling apart messages from multiple ProB instances.

--- de.prob2.kernel/src/main/java/de/prob/cli/ProBInstance.java
+++ de.prob2.kernel/src/main/java/de/prob/cli/ProBInstance.java
@@ -4,14 +4,14 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import com.google.common.base.MoreObjects;
+
 import com.google.inject.Inject;
 
 import de.prob.exception.CliError;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import com.google.common.base.MoreObjects;
 
 public class ProBInstance {
 
@@ -44,7 +44,7 @@ public class ProBInstance {
 	}
 
 	private Thread makeOutputPublisher(final BufferedReader stream) {
-		return new Thread(new ConsoleListener(this, stream, logger), "ProB Output Logger");
+		return new Thread(new ConsoleListener(this, stream, logger), String.format("ProB Output Logger for instance %x", this.hashCode()));
 	}
 
 	public void shutdown() {
