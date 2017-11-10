7439aadf8877ae9873ef745dd03f9ad595c36c7c
Sebastian Krings <sebastian@krin.gs>
2017-11-10 14:57:51 +0100

    add command for unregistering formulas

---
+++ de.prob2.kernel/src/main/java/de/prob/animator/command/UnregisterFormulaCommand.java
@@ -0,0 +1,41 @@
+package de.prob.animator.command;
+
+import de.prob.animator.domainobjects.IEvalElement;
+import de.prob.animator.domainobjects.ProBEvalElement;
+import de.prob.parser.ISimplifiedROMap;
+import de.prob.prolog.output.IPrologTermOutput;
+import de.prob.prolog.term.PrologTerm;
+
+public class UnregisterFormulaCommand extends AbstractCommand {
+
+	private static final String PROLOG_COMMAND_NAME = "register_prob2_formula";
+	private final IEvalElement formula;
+
+	public UnregisterFormulaCommand(final IEvalElement formula) {
+		this.formula = formula;
+	}
+
+	@Override
+	public void writeCommand(final IPrologTermOutput pto) {
+		pto.openTerm(PROLOG_COMMAND_NAME);
+		formula.getFormulaId().printUUID(pto);
+		if (this.formula instanceof ProBEvalElement) {
+			pto.openTerm("eval_typed");
+			formula.printProlog(pto);
+			pto.printAtom(formula.expansion().getPrologName());
+		} else {
+			pto.openTerm("eval");
+			formula.printProlog(pto);
+			pto.printAtom(formula.getKind().getPrologName());
+			pto.printAtom(formula.expansion().getPrologName());
+		}
+		pto.closeTerm();
+		pto.closeTerm();
+	}
+
+	@Override
+	public void processResult(
+			final ISimplifiedROMap<String, PrologTerm> bindings) {
+	}
+
+}
