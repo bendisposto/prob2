65c8179fd9a048cdced9b8a22e2416d438812c70
dgelessus <dgelessus@users.noreply.github.com>
2017-11-13 16:44:01 +0100

    Remove unused test helper code

--- de.prob2.kernel/src/test/java/test/AbstractUnitTest.java
+++ de.prob2.kernel/src/test/java/test/AbstractUnitTest.java
@@ -1,23 +1,10 @@
 package test;
 
-import java.io.IOException;
-
 import org.junit.BeforeClass;
 
 public abstract class AbstractUnitTest {
-
-	protected final boolean integrationTestRun = isIntegrationTestRun();
-
 	@BeforeClass
-	public static void setup() throws IOException {
+	public static void setup() {
 		System.setProperty("PROB_LOG_CONFIG", "fulltrace.xml");
 	}
-
-	private boolean isIntegrationTestRun() {
-		String itp = System.getProperty("integrationtest");
-		if (itp != null)
-			return "true".equals(itp.toLowerCase());
-		return false;
-	}
-
 }
