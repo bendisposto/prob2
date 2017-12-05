a1b27d18d3a8aee8fcaa8125a9eeaad8506fa9e4
dohan <dohan001@hhu.de>
2017-12-05 17:11:43 +0100

    allow null values in persistent transition (null values will be omitted from the output)

--- de.prob2.kernel/src/main/java/de/prob/check/tracereplay/PersistentTransition.java
+++ de.prob2.kernel/src/main/java/de/prob/check/tracereplay/PersistentTransition.java
@@ -1,8 +1,8 @@
 package de.prob.check.tracereplay;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -18,10 +18,10 @@ import de.prob.statespace.Transition;
 public class PersistentTransition {
 
 	private final String name;
-	private final Map<String, String> params = new LinkedHashMap<>();
-	private final Map<String, String> results = new LinkedHashMap<>();
-	private final Map<String, String> destState = new LinkedHashMap<>();
-	private List<String> preds = new ArrayList<>();
+	private Map<String, String> params;
+	private Map<String, String> results;
+	private Map<String, String> destState;
+	private List<String> preds;
 
 	public PersistentTransition(Transition transition) {
 		this(transition, false);
@@ -46,9 +46,11 @@ public class PersistentTransition {
 			} else {
 				// for each operation
 				OperationInfo machineOperationInfo = loadedMachine.getMachineOperationInfo(name);
+				params = new HashMap<>();
 				for (int i = 0; i < machineOperationInfo.getParameterNames().size(); i++) {
 					params.put(machineOperationInfo.getParameterNames().get(i), transition.getParameterValues().get(i));
 				}
+				results = new HashMap<>();
 				for (int i = 0; i < machineOperationInfo.getOutputParameterNames().size(); i++) {
 					results.put(machineOperationInfo.getOutputParameterNames().get(i),
 							transition.getReturnValues().get(i));
@@ -58,6 +60,9 @@ public class PersistentTransition {
 	}
 
 	private void addValuesToDestState(Map<IEvalElement, AbstractEvalResult> map) {
+		if (destState == null) {
+			destState = new HashMap<>();
+		}
 		for (Entry<IEvalElement, AbstractEvalResult> entry : map.entrySet()) {
 			if (entry.getValue() instanceof EvalResult) {
 				EvalResult evalResult = (EvalResult) entry.getValue();
@@ -71,19 +76,31 @@ public class PersistentTransition {
 	}
 
 	public Map<String, String> getParameters() {
+		if (this.params == null) {
+			return null;
+		}
 		return new HashMap<>(this.params);
 	}
 
 	public Map<String, String> getOuputParameters() {
+		if (this.results == null) {
+			return null;
+		}
 		return new HashMap<>(this.results);
 	}
 
 	public Map<String, String> getDestinationStateVariables() {
+		if (this.destState == null) {
+			return null;
+		}
 		return new HashMap<>(this.destState);
 	}
 
 	public List<String> getAdditionalPredicates() {
-		return this.preds;
+		if (this.preds == null) {
+			return Collections.emptyList();
+		}
+		return new ArrayList<>(this.preds);
 	}
 
 }
