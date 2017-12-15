54db59447445ab501d01769aae43b5c94518be18
dgelessus <dgelessus@users.noreply.github.com>
2017-12-15 15:19:45 +0100

    Add all constructors to EvaluationException

--- de.prob2.kernel/src/main/java/de/prob/animator/domainobjects/EvaluationException.java
+++ de.prob2.kernel/src/main/java/de/prob/animator/domainobjects/EvaluationException.java
@@ -1,18 +1,21 @@
 package de.prob.animator.domainobjects;
 
 public class EvaluationException extends RuntimeException {
-
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 8968134130086367905L;
 
-	public EvaluationException(final String message, final Throwable thrown) {
-		super(message, thrown);
+	public EvaluationException() {
+		super();
 	}
 
 	public EvaluationException(final String message) {
 		super(message);
 	}
 
+	public EvaluationException(final String message, final Throwable cause) {
+		super(message, cause);
+	}
+
+	public EvaluationException(final Throwable cause) {
+		super(cause);
+	}
 }
