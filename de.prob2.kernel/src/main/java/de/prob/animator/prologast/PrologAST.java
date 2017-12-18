347fdd297bf5ad1a1c9a6204de8456e6862d92e2
dgelessus <dgelessus@users.noreply.github.com>
2017-12-18 13:55:30 +0100

    Merge buildAST and makeSubnodes methods in PrologAST
    Both methods did almost exactly the same thing, except that buildAST
    also included an unneeded cast.

--- de.prob2.kernel/src/main/java/de/prob/animator/prologast/PrologAST.java
+++ de.prob2.kernel/src/main/java/de/prob/animator/prologast/PrologAST.java
@@ -15,32 +15,23 @@ public final class PrologAST {
 	private PrologAST() {}
 
 	public static List<PrologASTNode> buildAST(ListPrologTerm nodes) {
-		List<PrologASTNode> categoryList = new ArrayList<>();
-		for (int i = 0; i < nodes.size(); i++) {
-			ASTCategory temp = (ASTCategory) makeASTNode(nodes.get(i));
-			categoryList.add(temp);
+		List<PrologASTNode> convertedNodes = new ArrayList<>();
+		for (PrologTerm node : nodes) {
+			convertedNodes.add(makeASTNode(node));
 		}
-		return categoryList;
+		return convertedNodes;
 	}
 
 	private static PrologASTNode makeASTNode(PrologTerm node) {
 		if (node.getFunctor().equals("formula")) {
 			return new ASTFormula(node);
 		} else if (node.getFunctor().equals("category")) {
-			final List<PrologASTNode> subnodes = makeSubnodes(BindingGenerator.getList(node.getArgument(3)));
+			final List<PrologASTNode> subnodes = buildAST(BindingGenerator.getList(node.getArgument(3)));
 			final String name = node.getArgument(1).getFunctor();
 			final boolean expanded = node.getArgument(2).toString().contains("expanded");
 			final boolean propagated = node.getArgument(2).toString().contains("propagated");
 			return new ASTCategory(subnodes, name, expanded, propagated);
 		}
 		return null;
-	}
-
-	private static List<PrologASTNode> makeSubnodes(ListPrologTerm subnodes) {
-		List<PrologASTNode> rightList = new ArrayList<>();
-		for (PrologTerm m : subnodes) {
-			rightList.add(makeASTNode(m));
-		}
-		return rightList;
 	}
 }
