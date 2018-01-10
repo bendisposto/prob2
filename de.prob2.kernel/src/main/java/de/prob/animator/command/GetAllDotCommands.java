d7039ca2a2e1044a51f3d3288b91f207d61a4759
favu100 <Fabian.Vu@hhu.de>
2018-01-10 09:09:03 +0100

    Update GetAllDotCommands

--- de.prob2.kernel/src/main/java/de/prob/animator/command/GetAllDotCommands.java
+++ de.prob2.kernel/src/main/java/de/prob/animator/command/GetAllDotCommands.java
@@ -2,10 +2,12 @@ package de.prob.animator.command;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import de.prob.animator.domainobjects.DotCommandItem;
 import de.prob.parser.ISimplifiedROMap;
 import de.prob.prolog.output.IPrologTermOutput;
+import de.prob.prolog.term.ListPrologTerm;
 import de.prob.prolog.term.PrologTerm;
 import de.prob.statespace.State;
 
@@ -14,12 +16,6 @@ public class GetAllDotCommands extends AbstractCommand {
 	private static final String PROLOG_COMMAND_NAME = "get_dot_commands_in_state";
 	
 	private static final String LIST = "List";
-	
-	private static final String COMMAND_VARIABLE = "Command";
-	
-	private static final String DESCRIPTION_VARIABLE = "Description";
-	
-	private static final String NUMBER_ARGS_VARIABLE = "Number_Args";
 	
 	private List<DotCommandItem> commands = new ArrayList<>();
 	
@@ -39,7 +35,16 @@ public class GetAllDotCommands extends AbstractCommand {
 
 	@Override
 	public void processResult(ISimplifiedROMap<String, PrologTerm> bindings) {
-				
+		ListPrologTerm res = (ListPrologTerm) bindings.get(LIST);
+		commands = res.stream().map(GetAllDotCommands::toCommandItem).collect(Collectors.toList());
+	}
+	
+	private static DotCommandItem toCommandItem(final PrologTerm commandTerm) {
+		final String command = PrologTerm.atomicString(commandTerm.getArgument(1));
+		final String name = PrologTerm.atomicString(commandTerm.getArgument(2));
+		final String description = PrologTerm.atomicString(commandTerm.getArgument(3));
+		final int arity = Integer.parseInt(PrologTerm.atomicString(commandTerm.getArgument(4)));
+		return new DotCommandItem(command, name, description, arity);
 	}
 	
 	public List<DotCommandItem> getCommands() {
