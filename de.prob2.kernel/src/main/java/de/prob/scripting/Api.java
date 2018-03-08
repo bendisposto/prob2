0f04c57a1c4b22a6c5c0592e5b666c11e61174e1
dgelessus <dgelessus@users.noreply.github.com>
2018-03-08 13:53:08 +0100

    Move Api members into a more sensible order

--- de.prob2.kernel/src/main/java/de/prob/scripting/Api.java
+++ de.prob2.kernel/src/main/java/de/prob/scripting/Api.java
@@ -12,7 +12,7 @@ import com.google.inject.Provider;
 
 import de.be4.classicalb.core.parser.ParsingBehaviour;
 import de.be4.classicalb.core.parser.exceptions.BCompoundException;
-import de.be4.classicalb.core.parser.node.*;
+import de.be4.classicalb.core.parser.node.Start;
 import de.be4.classicalb.core.parser.rules.RulesProject;
 
 import de.prob.animator.IAnimator;
@@ -34,10 +34,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Api {
-	@Override
-	public String toString() {
-		return "ProB Connector";
-	}
+	private Logger logger = LoggerFactory.getLogger(Api.class);
+	private final FactoryProvider modelFactoryProvider;
+	private final Provider<IAnimator> animatorProvider;
+	private LinkedHashMap globals = new LinkedHashMap();
 
 	/**
 	 * A {@link FactoryProvider} and
@@ -53,6 +53,52 @@ public class Api {
 		this.modelFactoryProvider = modelFactoryProvider;
 	}
 
+	public Logger getLogger() {
+		return logger;
+	}
+
+	public void setLogger(Logger logger) {
+		this.logger = logger;
+	}
+
+	public LinkedHashMap getGlobals() {
+		return globals;
+	}
+
+	public void setGlobals(LinkedHashMap globals) {
+		this.globals = globals;
+	}
+
+	@Override
+	public String toString() {
+		return "ProB Connector";
+	}
+
+	public CliVersionNumber getVersion() {
+		try {
+			IAnimator animator = animatorProvider.get();
+			GetVersionCommand versionCommand = new GetVersionCommand();
+			animator.execute(versionCommand);
+			animator.kill();
+			return versionCommand.getVersion();
+		} catch (Exception e) {
+			return null;
+		}
+
+	}
+
+	/**
+	 * @return Returns a String representation of the currently available
+	 *         commands for the Api object. Intended to ease use in the Groovy
+	 *         console.
+	 */
+	public String help() {
+		return ("Api Commands: \n\n ClassicalBModel b_load(String PathToFile): load .mch files \n"
+				+ " CSPModel csp_load(String PathToFile): load .csp files \n"
+				+ " toFile(StateSpace s): save StateSpace\n" + " readFile(): reload saved StateSpace\n"
+				+ " shutdown(ProBInstance x): shutdown ProBInstance\n" + " help(): print out available commands");
+	}
+
 	/**
 	 * Shutdown the specified {@link ProBInstance} object.
 	 *
@@ -60,39 +106,6 @@ public class Api {
 	 */
 	public void shutdown(final ProBInstance x) {
 		x.shutdown();
-	}
-
-	public StateSpace eventb_load(final String file, final Map<String, String> prefs)
-			throws IOException, ModelTranslationError {
-		String fileName = file;
-		EventBFactory factory = modelFactoryProvider.getEventBFactory();
-		if (fileName.endsWith(".eventb")) {
-			return factory.loadModelFromEventBFile(file, prefs);
-		}
-
-		ExtractedModel<EventBModel> extracted = factory.extract(fileName);
-		StateSpace s = extracted.load(prefs);
-		return s;
-	}
-
-	public StateSpace eventb_load(final String file) throws IOException, ModelTranslationError {
-		return eventb_load(file, Collections.<String, String>emptyMap());
-	}
-
-	public void eventb_save(final StateSpace s, final String path) throws IOException, ModelTranslationError {
-		EventBModelTranslator translator = new EventBModelTranslator((EventBModel) s.getModel(), s.getMainComponent());
-
-		FileOutputStream fos = new FileOutputStream(path);
-		PrologTermOutput pto = new PrologTermOutput(fos, false);
-
-		pto.openTerm("package");
-		translator.printProlog(pto);
-		pto.closeTerm();
-		pto.fullstop();
-
-		pto.flush();
-		fos.flush();
-		fos.close();
 	}
 
 	/**
@@ -131,6 +144,39 @@ public class Api {
 
 	public StateSpace b_load(final Start ast) throws IOException, ModelTranslationError {
 		return b_load(ast, Collections.<String, String>emptyMap());
+	}
+
+	public StateSpace eventb_load(final String file, final Map<String, String> prefs)
+			throws IOException, ModelTranslationError {
+		String fileName = file;
+		EventBFactory factory = modelFactoryProvider.getEventBFactory();
+		if (fileName.endsWith(".eventb")) {
+			return factory.loadModelFromEventBFile(file, prefs);
+		}
+
+		ExtractedModel<EventBModel> extracted = factory.extract(fileName);
+		StateSpace s = extracted.load(prefs);
+		return s;
+	}
+
+	public StateSpace eventb_load(final String file) throws IOException, ModelTranslationError {
+		return eventb_load(file, Collections.<String, String>emptyMap());
+	}
+
+	public void eventb_save(final StateSpace s, final String path) throws IOException, ModelTranslationError {
+		EventBModelTranslator translator = new EventBModelTranslator((EventBModel) s.getModel(), s.getMainComponent());
+
+		FileOutputStream fos = new FileOutputStream(path);
+		PrologTermOutput pto = new PrologTermOutput(fos, false);
+
+		pto.openTerm("package");
+		translator.printProlog(pto);
+		pto.closeTerm();
+		pto.fullstop();
+
+		pto.flush();
+		fos.flush();
+		fos.close();
 	}
 
 	public StateSpace tla_load(final String file, final Map<String, String> prefs)
@@ -200,50 +246,4 @@ public class Api {
 	public StateSpace csp_load(final String file) throws IOException, ModelTranslationError {
 		return csp_load(file, Collections.<String, String>emptyMap());
 	}
-
-	public CliVersionNumber getVersion() {
-		try {
-			IAnimator animator = animatorProvider.get();
-			GetVersionCommand versionCommand = new GetVersionCommand();
-			animator.execute(versionCommand);
-			animator.kill();
-			return versionCommand.getVersion();
-		} catch (Exception e) {
-			return null;
-		}
-
-	}
-
-	/**
-	 * @return Returns a String representation of the currently available
-	 *         commands for the Api object. Intended to ease use in the Groovy
-	 *         console.
-	 */
-	public String help() {
-		return ("Api Commands: \n\n ClassicalBModel b_load(String PathToFile): load .mch files \n"
-				+ " CSPModel csp_load(String PathToFile): load .csp files \n"
-				+ " toFile(StateSpace s): save StateSpace\n" + " readFile(): reload saved StateSpace\n"
-				+ " shutdown(ProBInstance x): shutdown ProBInstance\n" + " help(): print out available commands");
-	}
-
-	public Logger getLogger() {
-		return logger;
-	}
-
-	public void setLogger(Logger logger) {
-		this.logger = logger;
-	}
-
-	public LinkedHashMap getGlobals() {
-		return globals;
-	}
-
-	public void setGlobals(LinkedHashMap globals) {
-		this.globals = globals;
-	}
-
-	private Logger logger = LoggerFactory.getLogger(Api.class);
-	private final FactoryProvider modelFactoryProvider;
-	private final Provider<IAnimator> animatorProvider;
-	private LinkedHashMap globals = new LinkedHashMap();
 }
