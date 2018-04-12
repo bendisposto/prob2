b68562ca5caeeabb43a027ffb2e316025833f14b
dgelessus <dgelessus@users.noreply.github.com>
2018-04-12 12:54:43 +0200

    Change FileHandler.extractZip to use Path instead of File

--- de.prob2.kernel/src/main/java/de/prob/scripting/FileHandler.java
+++ de.prob2.kernel/src/main/java/de/prob/scripting/FileHandler.java
@@ -1,12 +1,15 @@
 package de.prob.scripting;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
+
+import com.google.common.io.ByteStreams;
 
 /**
  * Provides helper methods for handling files.
@@ -17,39 +20,27 @@ public final class FileHandler {
 	private FileHandler() {
 		throw new AssertionError("Utility class");
 	}
-	
-	private static void createDirectory(String dest, ZipEntry entry) {
-		new File(dest + File.separator + entry.getName()).mkdir();
-	}
 
-	private static void writeFile(final InputStream stream, String dest, ZipEntry entry) throws IOException {
-		final File file = new File(dest + File.separator + entry.getName());
-		if (file.getParentFile() != null) {
-			file.getParentFile().mkdirs();
-		}
-		try (final FileOutputStream output = new FileOutputStream(file)) {
-			int len;
-			byte[] buffer = new byte[4096];
-			while ((len = stream.read(buffer)) > 0) {
-				output.write(buffer, 0, len);
+	public static void extractZip(final Path zip, final Path targetDir) throws IOException {
+		Files.createDirectories(targetDir);
+
+		try (final ZipInputStream inStream = new ZipInputStream(Files.newInputStream(zip))) {
+			ZipEntry entry;
+			while ((entry = inStream.getNextEntry()) != null) {
+				final Path dest = targetDir.resolve(entry.getName());
+				if (entry.isDirectory()) {
+					Files.createDirectories(dest);
+				} else {
+					try (final OutputStream output = Files.newOutputStream(dest)) {
+						ByteStreams.copy(inStream, output);
+					}
+				}
 			}
 		}
 	}
 
 	public static void extractZip(File zip, final String targetDirPath) throws IOException {
-		File destFile = new File(targetDirPath);
-		destFile.mkdir();
-		
-		try (final ZipInputStream inStream = new ZipInputStream(new FileInputStream(zip))) {
-			ZipEntry entry;
-			while ((entry = inStream.getNextEntry()) != null) { 
-				if (entry.isDirectory()) {
-					createDirectory(targetDirPath, entry);
-				} else {
-					writeFile(inStream, targetDirPath, entry);
-				}
-			}
-		}
+		extractZip(zip.toPath(), Paths.get(targetDirPath));
 	}
 
 	public static void extractZip(String pathToZip, String targetDirPath) throws IOException {
