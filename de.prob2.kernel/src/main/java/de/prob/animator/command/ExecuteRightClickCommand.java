7ee804f818ec085abbe5e5fc4a92bc9c7df4a6e3
favu100 <Fabian.Vu@hhu.de>
2018-05-08 08:53:01 +0200

    Move state visualization commands to kernel

---
+++ de.prob2.kernel/src/main/java/de/prob/animator/command/ExecuteRightClickCommand.java
@@ -0,0 +1,48 @@
+package de.prob.animator.command;
+
+import de.prob.animator.command.AbstractCommand;
+import de.prob.parser.ISimplifiedROMap;
+import de.prob.prolog.output.IPrologTermOutput;
+import de.prob.prolog.term.PrologTerm;
+
+public class ExecuteRightClickCommand extends AbstractCommand {
+
+	private static final String PROLOG_COMMAND_NAME = "react_to_item_right_click_option_for_state";
+	private static final String TRANSITION = "TransitionId";
+	private static final String NEW_STATE = "NewStateId";
+	
+	private String stateId;
+	private int row;
+	private int column;
+	private String option;
+	private String transitionID;
+
+	public ExecuteRightClickCommand(String stateId, int row, int column, String option) {
+		this.stateId = stateId;
+		this.row = row-1;
+		this.column = column+1;
+		this.option = option;
+	}
+
+	@Override
+	public void writeCommand(IPrologTermOutput pto) {
+		pto.openTerm(PROLOG_COMMAND_NAME);
+		pto.printAtomOrNumber(stateId);
+		pto.printNumber(row);
+		pto.printNumber(column);
+		pto.printAtom(option);
+		pto.printVariable(TRANSITION);
+		pto.printVariable(NEW_STATE);
+		pto.closeTerm();
+	}
+
+	@Override
+	public void processResult(ISimplifiedROMap<String, PrologTerm> bindings) {
+		transitionID = bindings.get(TRANSITION).getFunctor();
+	}
+
+	public String getTransitionID() {
+		return transitionID;
+	}
+
+}
