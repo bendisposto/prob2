034e9fbda33a445ad34503b6c9d5d215879877f6
dgelessus <dgelessus@users.noreply.github.com>
2018-10-15 13:52:53 +0200

    Update typeCheck Groovy test to new type format

--- de.prob2.kernel/groovyTests/typeCheck.groovy
+++ de.prob2.kernel/groovyTests/typeCheck.groovy
@@ -12,46 +12,46 @@ final typecheck = {String formula ->
 final res1 = typecheck("1 = BOOL")
 assert !res1.ok
 assert !res1.errors.empty
-assert res1.type == "pred"
+assert res1.type == "predicate"
 
 final res2 = typecheck("1") 
 assert res2.ok
 assert res2.errors.empty
-assert res2.type == "integer"
+assert res2.type == "INTEGER"
 
 final res3 = typecheck("TRUE") 
 assert res3.ok
 assert res3.errors.empty
-assert res3.type == "boolean"
+assert res3.type == "BOOL"
 
 final res4 = typecheck("FALSE")
 assert res4.ok
 assert res4.errors.empty
-assert res4.type == "boolean"
+assert res4.type == "BOOL"
 
 final res5 = typecheck("{1,2,3}")
 assert res5.ok
 assert res5.errors.empty
-assert res5.type == "set(integer)"
+assert res5.type == "POW(INTEGER)"
 
 final res6 = typecheck("1 |-> 2")
 assert res6.ok
 assert res6.errors.empty
-assert res6.type == "couple(integer,integer)"
+assert res6.type == "INTEGER*INTEGER"
 
 final res7 = typecheck("PID1") 
 assert res7.ok
 assert res7.errors.empty
-assert res7.type == "global('PID')"
+assert res7.type == "PID"
 
 final res8 = typecheck('"BLAH"')
 assert res8.ok
 assert res8.errors.empty
-assert res8.type == "string"
+assert res8.type == "STRING"
 
 final res9 = typecheck("rec(PID1:1,PID2:2,PID3:3)")
 assert res9.ok
 assert res9.errors.empty
-assert res9.type == "record([field('PID1',integer),field('PID2',integer),field('PID3',integer)])"
+assert res9.type == "struct(PID1:INTEGER, PID2:INTEGER, PID3:INTEGER)"
 
 "it is possible to type check a formula"
